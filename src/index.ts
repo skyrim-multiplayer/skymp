86a9361503435b55bce2f55f6bda66ae8241411c
Leonid Pospelov <pospelovlm@yandex.ru>
2020-06-23 19:39:55 +0300

    Add source-map-support

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,17 @@ const config = env === "development" ? "Debug" : "Release";
 console.log(`Using scamp_native config ${config}`);
 
 const scampNative = require(`../build/${config}/scamp_native.node`);
+
+import * as sourceMapSupport from "source-map-support";
+sourceMapSupport.install();
+
 console.log(scampNative.hello());
 
 (async () => {
   while (1) {
     await new Promise(setImmediate);
+    throw new Error("lox");
   }
 })();
+
+process.on("unhandledRejection", console.error);
