47b98d27b4dcd2eb57c88114bf48a8e940d08e85
Leonid Pospelov <pospelovlm@yandex.ru>
2020-08-02 15:48:57 +0300

    Prepare to merge into skymp5-server

--- README.md
+++
@@ -1,1 +0,0 @@
-skymp5-client
--- index.ts
+++
@@ -1,6 +0,0 @@
-import { SkympClient } from './src/skympClient';
-
-import * as networking from './src/networking';
-import { printConsole, loadGame, findConsoleCommand, TESModPlatform, Game, on, storage } from 'skyrimPlatform';
-
-new SkympClient;
