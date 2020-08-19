f68e91123c5646e38be83282df6d4d0a4a65fcee
Leonid Pospelov <pospelovlm@yandex.ru>
2020-08-19 21:37:19 +0300

    Optimize hot path (getFormEx + dynamic cast)

--- src/platform_se/skyrim_platform/SkyrimPlatformProxy.cpp
+++ src/platform_se/skyrim_platform/SkyrimPlatformProxy.cpp
@@ -1,13 +1,27 @@
 #include "SkyrimPlatformProxy.h"
+#include "NativeValueCasts.h"
 #include "ProxyGetter.h"
+#include <skse64/GameRTTI.h>
+#include <skse64/GameReferences.h>
 #include <unordered_map>
 
 namespace {
+JsValue CreateObject(const char* type, void* form)
+{
+  return form ? NativeValueCasts::NativeObjectToJsObject(
+                  std::make_shared<CallNative::Object>(type, form))
+              : JsValue::Null();
+}
+
 JsValue GetProxyForClass(const std::string& className,
                          const JsValue& skyrimPlatformExports)
 {
   std::shared_ptr<std::unordered_map<std::string, JsValue>> functionsCache(
     new std::unordered_map<std::string, JsValue>);
+
+  bool isGame = !stricmp(className.data(), "Game");
+  bool isObjectReference = !stricmp(className.data(), "ObjectReference");
+  bool isActor = !stricmp(className.data(), "Actor");
 
   auto handler = JsValue::Object();
   handler.SetProperty(
@@ -23,15 +37,58 @@ JsValue GetProxyForClass(const std::string& className,
         std::shared_ptr<std::vector<JsValue>> dynamicCastArgs(
           new std::vector<JsValue>{ origin, JsValue::Undefined(), className });
 
-        if (s == "from") {
-          f = JsValue::Function([keyStr, origin, className, callNativeArgs,
-                                 dynamicCastArgs](
-                                  const JsFunctionArguments& args) -> JsValue {
-            auto& from = args[1];
-            (*dynamicCastArgs)[1] = from;
-            thread_local auto dynamicCast = origin.GetProperty("dynamicCast");
-            return dynamicCast.Call(*dynamicCastArgs);
-          });
+        if (isGame && s == "getFormEx") {
+          f =
+            JsValue::Function([](const JsFunctionArguments& args) -> JsValue {
+              auto f = LookupFormByID((uint32_t)(double)args[1]);
+              if (f)
+                return CreateObject("Form", f);
+              else
+                return JsValue::Null();
+            });
+        } else if (s == "from") {
+          if (isObjectReference) {
+            f = JsValue::Function(
+              [](const JsFunctionArguments& args) -> JsValue {
+                CallNative::ObjectPtr from =
+                  NativeValueCasts::JsObjectToNativeObject(args[1]);
+                if (!from)
+                  return JsValue::Null();
+                auto fromRaw = (TESForm*)from->GetNativeObjectPtr();
+                if (!fromRaw)
+                  return JsValue::Null();
+                TESObjectREFR* resRaw =
+                  DYNAMIC_CAST(fromRaw, TESForm, TESObjectREFR);
+                if (!resRaw)
+                  return JsValue::Null();
+                return CreateObject("ObjectReference", resRaw);
+              });
+          } else if (isActor) {
+            f = JsValue::Function(
+              [](const JsFunctionArguments& args) -> JsValue {
+                CallNative::ObjectPtr from =
+                  NativeValueCasts::JsObjectToNativeObject(args[1]);
+                if (!from)
+                  return JsValue::Null();
+                auto fromRaw = (TESForm*)from->GetNativeObjectPtr();
+                if (!fromRaw)
+                  return JsValue::Null();
+                Actor* resRaw = DYNAMIC_CAST(fromRaw, TESForm, Actor);
+                if (!resRaw)
+                  return JsValue::Null();
+                return CreateObject("Actor", resRaw);
+              });
+          } else {
+            f = JsValue::Function(
+              [keyStr, origin, className, callNativeArgs,
+               dynamicCastArgs](const JsFunctionArguments& args) -> JsValue {
+                auto& from = args[1];
+                (*dynamicCastArgs)[1] = from;
+                thread_local auto dynamicCast =
+                  origin.GetProperty("dynamicCast");
+                return dynamicCast.Call(*dynamicCastArgs);
+              });
+          }
         } else {
           f = JsValue::Function([keyStr, origin, className, callNativeArgs](
                                   const JsFunctionArguments& args) -> JsValue {
