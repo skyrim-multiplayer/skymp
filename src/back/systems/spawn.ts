426d6acae56e94e5d3e0a408eddd86f7850576ba
Leonid Pospelov <pospelovlm@yandex.ru>
2020-10-21 20:46:32 +0300

    Add spawn.ts pseudo-code

--- src/back/systems/spawn.ts
+++ src/back/systems/spawn.ts
@@ -1,5 +1,9 @@
 import { System, Log, SystemContext } from "./system";
 
+const spawnpoints = [
+  { pos: [22659, -8697, -3594], worldOrCell: 0x1a26f, angleZ: 268 },
+];
+
 function randomInteger(min: number, max: number) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
@@ -10,32 +14,28 @@ export class Spawn implements System {
   constructor(private log: Log) {}
 
   async initAsync(ctx: SystemContext): Promise<void> {
-    ctx.gm.on("spawnAllowed", (userId: number) => {
-      /*const formId = 0xff000001 + userId;
-
-      try {
-        ctx.svr.destroyActor(formId);
-      } catch (e) {}
-
-      const spawnpoints = [
-        { pos: [22659, -8697, -3594], worldOrCell: 0x1a26f, angleZ: 268 },
-      ];
-      const idx = randomInteger(0, spawnpoints.length - 1);
-
-      ctx.svr.createActor(
-        formId,
-        spawnpoints[idx].pos,
-        spawnpoints[idx].angleZ,
-        spawnpoints[idx].worldOrCell
-      );
-
-      ctx.svr.setUserActor(userId, formId);
-      ctx.svr.setRaceMenuOpen(formId, true);*/
+    ctx.gm.on("spawnAllowed", (userId: number, userProfileId: number) => {
+      let actorId = ctx.svr.getActorByProfileId(userProfileId);
+      if (actorId) {
+        ctx.svr.setEnabled(actorId, true);
+        ctx.svr.setUserActor(userId, actorId);
+      } else {
+        const idx = randomInteger(0, spawnpoints.length - 1);
+        actorId = ctx.svr.createActor(
+          0,
+          spawnpoints[idx].pos,
+          spawnpoints[idx].angleZ,
+          spawnpoints[idx].worldOrCell,
+          userProfileId
+        );
+        ctx.svr.setUserActor(userId, actorId);
+        ctx.svr.setRaceMenuOpen(actorId, true);
+      }
     });
   }
 
   disconnect(userId: number, ctx: SystemContext): void {
     const actorId = ctx.svr.getUserActor(userId);
-    if (actorId !== 0) ctx.svr.destroyActor(actorId);
+    if (actorId !== 0) ctx.svr.setEnabled(actorId, false);
   }
 }
