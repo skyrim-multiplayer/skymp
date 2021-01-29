bc2e36432d17f78f6d53349416f42456eb0c460e
Leonid Pospelov <pospelovlm@yandex.ru>
2021-01-29 21:48:49 +0300

    Add GitHub workflow for building the project

--- tools/dev_service/config-default.js
+++ tools/dev_service/config-default.js
@@ -1,5 +1,4 @@
 module.exports = {
-  'SkyrimSEFolder': 'C:\\Program Files (x86)\\Steam\\steamapps\\common\\Skyrim Special Edition',
-  'SkyrimMultiplayerBinaryDir': 'C:\\projects\\skyrim-platform\\build',
-  'SkyrimMultiplayerSourceDir': 'C:\\projects\\skyrim-platform'
+  SkyrimSEFolder:
+    "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Skyrim Special Edition",
 };
