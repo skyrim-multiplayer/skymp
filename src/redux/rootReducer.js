c6eebb6bd3607cde9bad30dfdd4a1fcfc9d6bf98
remox112358 <remox112358@gmail.com>
2021-02-08 22:49:10 +0400

    Add watermark interface
    Был добавлен базовый "вотермарк" с отображением ID текущего игрока и количеством онлайна на сервере. Были созданы соответствующие "диспачи". Состояние show по умолчанию true.

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -7,8 +7,10 @@ import { animListReducer } from '../features/animList/reducer'
 import { chatHiveReducer } from '../features/chat_HIVE/reducer'
 import { animListHiveReducer } from '../features/animList_HIVE/reducer'
 import { spawnHiveReducer } from '../features/spawn_HIVE/reducer'
+import { watermarkHiveReducer } from '../features/watermark_HIVE/reducer'
 
 export const rootReducer = combineReducers({
+  watermarkHiveReducer,
   spawnHiveReducer,
   animListHiveReducer,
   chatHiveReducer,
