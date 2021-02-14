9a0928649039702f185aeaf8ab8671d827ce293a
Leonid Pospelov <pospelovlm@yandex.ru>
2021-02-14 23:19:50 +0300

    WIP

--- src/App.js
+++ src/App.js
@@ -1,89 +1,85 @@
-import React from "react"
-import { connect } from "react-redux"
+import React from "react";
+import { connect } from "react-redux";
 
-import Chat from './features/chat'
-import AnimList from './features/animList'
-import ChatHIVE from './features/chat_HIVE'
-import AnimListHIVE from './features/animList_HIVE'
-import SpawnHIVE from './features/spawn_HIVE'
-import WatermarkHIVE from './features/watermark_HIVE'
-import TradeHIVE from './features/trade_HIVE'
-import HudHIVE from './features/hud_HIVE'
+import Chat from "./features/chat";
+import AnimList from "./features/animList";
+import ChatHIVE from "./features/chat_HIVE";
+import AnimListHIVE from "./features/animList_HIVE";
+import SpawnHIVE from "./features/spawn_HIVE";
+import WatermarkHIVE from "./features/watermark_HIVE";
+import TradeHIVE from "./features/trade_HIVE";
+import HudHIVE from "./features/hud_HIVE";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    window.addEventListener('focus', this.onWindowFocus.bind(this))
-    window.addEventListener('blur', this.onWindowFocus.bind(this))
+    window.addEventListener("focus", this.onWindowFocus.bind(this));
+    window.addEventListener("blur", this.onWindowFocus.bind(this));
 
     window.mp = {
       send: (type, data) => {
         try {
           window.skymp.send({
             type,
-            data
-          })
+            data,
+          });
         } catch {
-          console.log(type, data)
+          console.log(type, data);
         }
-      }
-    }
+      },
+    };
 
     try {
-      window.skymp.on("error", console.error)
+      window.skymp.on("error", console.error);
       window.skymp.on("message", (action) => {
-        window.storage.dispatch(action)
-      })
+        window.storage.dispatch(action);
+      });
     } catch {}
 
-    window.isMoveWindow = false
-    window.addEventListener('mousemove', this.onMoveWindow)
-    window.addEventListener('mouseup', this.onMouseUp)
+    window.isMoveWindow = false;
+    window.addEventListener("mousemove", this.onMoveWindow);
+    window.addEventListener("mouseup", this.onMouseUp);
   }
 
   componentWillUnmount() {
-    window.removeEventListener('focus', this.onWindowFocus.bind(this))
-    window.removeEventListener('blur', this.onWindowFocus.bind(this))
-    window.addEventListener('mousemove', this.onMoveWindow)
+    window.removeEventListener("focus", this.onWindowFocus.bind(this));
+    window.removeEventListener("blur", this.onWindowFocus.bind(this));
+    window.addEventListener("mousemove", this.onMoveWindow);
   }
 
   onWindowFocus(e) {
-    const focus = document.hasFocus()
-    this.props.updateBrowserFocus(focus)
+    const focus = document.hasFocus();
+    this.props.updateBrowserFocus(focus);
   }
 
   onMoveWindow(e) {
-    if(window.isMoveWindow && typeof window.moveWindow == 'function') {
+    if (window.isMoveWindow && typeof window.moveWindow == "function") {
       // console.log(e)
-      window.moveWindow(e.clientX, e.clientY)
+      window.moveWindow(e.clientX, e.clientY);
     }
   }
 
   onMouseUp() {
-    if(window.isMoveWindow)
-      window.isMoveWindow = false
-    window.moveWindow = null
+    if (window.isMoveWindow) window.isMoveWindow = false;
+    window.moveWindow = null;
   }
 
   render() {
     return (
-      <div className={`App ${!window.hasOwnProperty('skymp') ? 'bg' : ''}`}>
+      <div className={`App ${!window.hasOwnProperty("skymp") ? "bg" : ""}`}>
         <HudHIVE />
-        <ChatHIVE />
         <AnimList />
         <Chat />
         <AnimListHIVE />
         <SpawnHIVE />
-        <WatermarkHIVE />
         <TradeHIVE />
       </div>
-    )
+    );
   }
 }
-
 
 const mapStateToProps = (state) => {
   return {
--- src/features/chat/reducer/index.js
+++ src/features/chat/reducer/index.js
@@ -1,7 +1,7 @@
 const defaultState = {
-  show: false,
+  show: true,
   list: [],
-  showInput: 'auto',
+  showInput: "auto",
   input: "",
 };
 
@@ -11,39 +11,38 @@ export const chatReducer = (state = defaultState, action) => {
       return {
         ...state,
         show: action.data,
-      }
+      };
     }
-    
+
     case "UPDATE_CHAT_LIST": {
       return {
         ...state,
         list: action.data,
       };
     }
-    
+
     case "ADD_CHAT_MSG": {
-      const list = [...state.list]
-      if(Array.isArray(action.data)) {
-        list.push([...action.data])
+      const list = [...state.list];
+      if (Array.isArray(action.data)) {
+        list.push([...action.data]);
       } else {
-        list.push(action.data)
+        list.push(action.data);
       }
-      while (list.length > 50) 
-        list.shift()
+      while (list.length > 50) list.shift();
 
       return {
         ...state,
         list,
       };
     }
-    
+
     case "UPDATE_CHAT_SHOWINPUT": {
       return {
         ...state,
         showInput: action.data,
-      }
+      };
     }
-    
+
     case "UPDATE_CHAT_INPUT": {
       return {
         ...state,
