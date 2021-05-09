skyrim-multiplayer/skymp

commit c95f14fbda4e33bc6573c7da2b2b39e85efaaf2f

[MINOR] Add IPC message event and sendIpcMessage API

src/platform_se/skyrim_platform/EventsApi.cpp
@@ -286,6 +286,15 @@ struct EventsGlobalState
   std::shared_ptr<Hook> sendPapyrusEvent;
 } g;
 
+struct IpcShare
+{
+  std::recursive_mutex m;
+  std::vector<std::pair<std::string, EventsApi::IpcMessageCallback>>
+    ipcCallbacks;
+} g_ipcShare;
+
+std::atomic<uint32_t> g_chakraThreadId = 0;
+
 namespace {
 void CallCalbacks(const char* eventName, const std::vector<JsValue>& arguments,
                   bool isOnce = false)
@@ -311,6 +320,7 @@ void EventsApi::SendEvent(const char* eventName,
 
 void EventsApi::Clear()
 {
+  g_chakraThreadId = GetCurrentThreadId();
   g = {};
 }
 
@@ -318,87 +328,17 @@ void EventsApi::SendAnimationEventEnter(uint32_t selfId,
                                         std::string& animEventName) noexcept
 {
   g.sendAnimationEvent->Enter(selfId, animEventName);
-
-  /*DWORD owningThread = GetCurrentThreadId();
-  auto f = [&](int) {
-    try {
-      if (g.sendAnimationEvent.inProgressThreads.count(owningThread))
-        throw std::runtime_error("'sendAnimationEvent' is already processing");
-
-      // This should always be done before calling throwing functions
-      g.sendAnimationEvent.inProgressThreads.insert(owningThread);
-
-      for (auto& h : g.sendAnimationEvent.handlers) {
-        auto& perThread = h.perThread[owningThread];
-        PrepareContext(perThread, ClearStorage::Yes);
-
-        perThread.context.SetProperty("selfId", (double)selfId);
-        perThread.context.SetProperty("animEventName", animEventName);
-
-        h.enter.Call({ JsValue::Undefined(), perThread.context });
-
-        animEventName =
-          (std::string)perThread.context.GetProperty("animEventName");
-      }
-    } catch (std::exception& e) {
-      std::string what = e.what();
-      g_taskQueue.AddTask([what] {
-        throw std::runtime_error(what + " (in SendAnimationEventEnter)");
-      });
-    }
-  };
-  g_pool.Push(f).wait();*/
 }
 
 void EventsApi::SendAnimationEventLeave(bool animationSucceeded) noexcept
 {
   g.sendAnimationEvent->Leave(animationSucceeded);
-  /*DWORD owningThread = GetCurrentThreadId();
-  auto f = [&](int) {
-    try {
-      if (!g.sendAnimationEvent.inProgressThreads.count(owningThread))
-        throw std::runtime_error("'sendAnimationEvent' is not processing");
-      g.sendAnimationEvent.inProgressThreads.erase(owningThread);
-
-      for (auto& h : g.sendAnimationEvent.handlers) {
-        auto& perThread = h.perThread.at(owningThread);
-        PrepareContext(perThread, ClearStorage::No);
-
-        perThread.context.SetProperty("animationSucceeded",
-                                      JsValue::Bool(animationSucceeded));
-        h.leave.Call({ JsValue::Undefined(), perThread.context });
-
-        h.perThread.erase(owningThread);
-      }
-    } catch (std::exception& e) {
-      std::string what = e.what();
-      g_taskQueue.AddTask([what] {
-        throw std::runtime_error(what + " (in SendAnimationEventLeave)");
-      });
-    }
-  };
-  g_pool.Push(f).wait();*/
 }
 
 void EventsApi::SendPapyrusEventEnter(uint32_t selfId,
                                       std::string& papyrusEventName) noexcept
 {
   g.sendPapyrusEvent->Enter(selfId, papyrusEventName);
-  /*DWORD owningThread = GetCurrentThreadId();
-  auto f = [&](int) {
-    try {
-      if (!g.sendPapyrusEvent.inProgressThreads.count(owningThread))
-        throw std::runtime_error("'sendPapyrusEvent' is already processing");
-      g.sendPapyrusEvent.inProgressThreads.insert(owningThread);
-
-    } catch (std::exception& e) {
-      std::string what = e.what();
-      g_taskQueue.AddTask([what] {
-        throw std::runtime_error(what + " (in SendPapyrusEventEnter)");
-      });
-    }
-  };
-  g_pool.Push(f).wait();*/
 }
 
 void EventsApi::SendPapyrusEventLeave() noexcept
@@ -447,6 +387,52 @@ JsValue EventsApi::GetHooks()
   return res;
 }
 
+uint32_t EventsApi::IpcSubscribe(const char* systemName,
+                                 IpcMessageCallback callback)
+{
+  // Maybe they decide calling IpcSubscribe from multiple threads...
+  std::lock_guard l(g_ipcShare.m);
+
+  auto it =
+    std::find(g_ipcShare.ipcCallbacks.begin(), g_ipcShare.ipcCallbacks.end(),
+              std::pair<std::string, IpcMessageCallback>{ "", nullptr });
+  if (it == g_ipcShare.ipcCallbacks.end()) {
+    g_ipcShare.ipcCallbacks.push_back({ systemName, callback });
+    return g_ipcShare.ipcCallbacks.size() - 1;
+  }
+
+  it->first = systemName;
+  it->second = callback;
+  return static_cast<uint32_t>(it - g_ipcShare.ipcCallbacks.begin());
+}
+
+void EventsApi::IpcUnsubscribe(uint32_t subscriptionId)
+{
+  std::lock_guard l(g_ipcShare.m);
+  if (g_ipcShare.ipcCallbacks.size() > subscriptionId) {
+    g_ipcShare.ipcCallbacks[subscriptionId] = { "", nullptr };
+  }
+  // TODO: pop_back for empty subscriptions?
+}
+
+void EventsApi::IpcSend(const char* systemName, const uint8_t* data,
+                        uint32_t length)
+{
+  const DWORD currentThreadId = GetCurrentThreadId();
+  if (currentThreadId != g_chakraThreadId) {
+    assert(0 && "IpcSend is only available in Chakra thread");
+    return;
+  }
+
+  auto typedArray = JsValue::Uint8Array(length);
+  memcpy(typedArray.GetTypedArrayData(), data, length);
+
+  auto ipcMessageEvent = JsValue::Object();
+  ipcMessageEvent.SetProperty("sourceSystemName", systemName);
+  ipcMessageEvent.SetProperty("message", typedArray);
+  SendEvent("ipcMessage", { JsValue::Undefined(), ipcMessageEvent });
+}
+
 namespace {
 JsValue AddCallback(const JsFunctionArguments& args, bool isOnce = false)
 {
@@ -479,7 +465,8 @@ JsValue AddCallback(const JsFunctionArguments& args, bool isOnce = false)
                                    "moveAttachDetach",
                                    "objectLoaded",
                                    "waitStop",
-                                   "activate" };
+                                   "activate",
+                                   "ipcMessage" };
 
   if (events.count(eventName) == 0)
     throw InvalidArgumentException("eventName", eventName);
@@ -498,4 +485,35 @@ JsValue EventsApi::On(const JsFunctionArguments& args)
 JsValue EventsApi::Once(const JsFunctionArguments& args)
 {
   return AddCallback(args, true);
+}
+
+JsValue EventsApi::SendIpcMessage(const JsFunctionArguments& args)
+{
+  auto targetSystemName = static_cast<std::string>(args[1]);
+  auto message = args[2].GetArrayBufferData();
+  auto messageLength = args[2].GetArrayBufferLength();
+
+  if (!message || messageLength == 0) {
+    throw std::runtime_error(
+      "sendIpcMessage expects a valid ArrayBuffer instance");
+  }
+
+  std::vector<IpcMessageCallback> callbacks;
+  {
+    std::lock_guard l(g_ipcShare.m);
+    for (auto& [systemName, cb] : g_ipcShare.ipcCallbacks) {
+      if (systemName == targetSystemName) {
+        callbacks.push_back(cb);
+      }
+    }
+  }
+
+  // Want to call callbacks with g_ipcShare.m unlocked
+  for (auto& cb : callbacks) {
+    if (cb) {
+      cb(reinterpret_cast<uint8_t*>(message), messageLength);
+    }
+  }
+
+  return JsValue::Undefined();
 }

src/platform_se/skyrim_platform/EventsApi.h
@@ -7,26 +7,31 @@ class SKSETaskInterface;
 namespace EventsApi {
 JsValue On(const JsFunctionArguments& args);
 JsValue Once(const JsFunctionArguments& args);
+JsValue SendIpcMessage(const JsFunctionArguments& args);
 
 void SendEvent(const char* eventName, const std::vector<JsValue>& arguments);
 void Clear();
 
 // Exceptions will be pushed to g_taskQueue
-
 void SendAnimationEventEnter(uint32_t selfId,
                              std::string& animEventName) noexcept;
 void SendAnimationEventLeave(bool animationSucceeded) noexcept;
-
 void SendPapyrusEventEnter(uint32_t selfId,
                            std::string& papyrusEventName) noexcept;
 void SendPapyrusEventLeave() noexcept;
 
 JsValue GetHooks();
 
+typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length);
+uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback);
+void IpcUnsubscribe(uint32_t subscriptionId);
+void IpcSend(const char* systemName, const uint8_t* data, uint32_t length);
+
 inline void Register(JsValue& exports)
 {
   exports.SetProperty("on", JsValue::Function(On));
   exports.SetProperty("once", JsValue::Function(Once));
   exports.SetProperty("hooks", GetHooks());
+  exports.SetProperty("sendIpcMessage", JsValue::Function(SendIpcMessage));
 }
 }

src/platform_se/skyrim_platform/main.cpp
@@ -294,6 +294,26 @@ void OnPapyrusUpdate(RE::BSScript::IVirtualMachine* vm, RE::VMStackID stackId)
 }
 
 extern "C" {
+__declspec(dllexport) uint32_t
+  SkyrimPlatform_IpcSubscribe_Impl(const char* systemName,
+                                   EventsApi::IpcMessageCallback callback)
+{
+  return EventsApi::IpcSubscribe(systemName, callback);
+}
+
+__declspec(dllexport) void SkyrimPlatform_IpcUnsubscribe_Impl(
+  uint32_t subscriptionId)
+{
+  return EventsApi::IpcUnsubscribe(subscriptionId);
+}
+
+__declspec(dllexport) void SkyrimPlatform_IpcSend_Impl(const char* systemName,
+                                                       const uint8_t* data,
+                                                       uint32_t length)
+{
+  return EventsApi::IpcSend(systemName, data, length);
+}
+
 __declspec(dllexport) bool SKSEPlugin_Query_Impl(
   const SKSE::QueryInterface* skse, SKSE::PluginInfo* info)
 {

src/platform_se/skyrim_platform_entry/main.cpp
@@ -5,8 +5,15 @@
 #include <string>
 #include <vector>
 
+typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length);
+
 typedef bool (*SKSEPlugin_Query_Impl)(void*, void*);
 typedef bool (*SKSEPlugin_Load_Impl)(void*);
+typedef uint32_t (*SkyrimPlatform_IpcSubscribe_Impl)(const char*,
+                                                     IpcMessageCallback);
+typedef void (*SkyrimPlatform_IpcUnsubscribe_Impl)(uint32_t);
+typedef void (*SkyrimPlatform_IpcSend_Impl)(const char*, const uint8_t*,
+                                            uint32_t);
 
 class PlatformImplInterface
 {
@@ -20,6 +27,21 @@ public:
   bool Query(void* skse, void* pluginInfo) { return query(skse, pluginInfo); }
 
   bool Load(void* skse) { return load(skse); }
+
+  uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback)
+  {
+    return ipcSubscribe(systemName, callback);
+  }
+
+  void IpcUnsubscribe(uint32_t subscriptionId)
+  {
+    return ipcUnsubscribe(subscriptionId);
+  }
+
+  void IpcSend(const char* systemName, const uint8_t* data, uint32_t length)
+  {
+    return ipcSend(systemName, data, length);
+  }
 
 private:
   PlatformImplInterface()
@@ -47,13 +69,64 @@ private:
       throw std::runtime_error("Unable to find SKSEPlugin_Load_Impl: Error " +
                                std::to_string(GetLastError()));
     }
+
+    ipcSubscribe = reinterpret_cast<SkyrimPlatform_IpcSubscribe_Impl>(
+      GetProcAddress(skyrimPlatformImpl, "SkyrimPlatform_IpcSubscribe_Impl"));
+    if (!ipcSubscribe) {
+      throw std::runtime_error(
+        "Unable to find SkyrimPlatform_IpcSubscribe_Impl: Error " +
+        std::to_string(GetLastError()));
+    }
+
+    ipcSend = reinterpret_cast<SkyrimPlatform_IpcSend_Impl>(
+      GetProcAddress(skyrimPlatformImpl, "SkyrimPlatform_IpcSend_Impl"));
+    if (!ipcSend) {
+      throw std::runtime_error(
+        "Unable to find SkyrimPlatform_IpcSend_Impl: Error " +
+        std::to_string(GetLastError()));
+    }
+
+    ipcUnsubscribe =
+      reinterpret_cast<SkyrimPlatform_IpcUnsubscribe_Impl>(GetProcAddress(
+        skyrimPlatformImpl, "SkyrimPlatform_IpcUnsubscribe_Impl"));
+    if (!ipcUnsubscribe) {
+      throw std::runtime_error(
+        "Unable to find SkyrimPlatform_IpcUnsubscribe_Impl: Error " +
+        std::to_string(GetLastError()));
+    }
   }
 
   SKSEPlugin_Query_Impl query = nullptr;
   SKSEPlugin_Load_Impl load = nullptr;
+  SkyrimPlatform_IpcSubscribe_Impl ipcSubscribe = nullptr;
+  SkyrimPlatform_IpcSend_Impl ipcSend = nullptr;
+  SkyrimPlatform_IpcUnsubscribe_Impl ipcUnsubscribe = nullptr;
 };
 
 extern "C" {
+
+__declspec(dllexport) uint32_t
+  SkyrimPlatform_IpcSubscribe(const char* systemName,
+                              IpcMessageCallback callback)
+{
+  return PlatformImplInterface::GetSingleton().IpcSubscribe(systemName,
+                                                            callback);
+}
+
+__declspec(dllexport) void SkyrimPlatform_IpcUnsubscribe(
+  uint32_t subscriptionId)
+{
+  return PlatformImplInterface::GetSingleton().IpcUnsubscribe(subscriptionId);
+}
+
+__declspec(dllexport) void SkyrimPlatform_IpcSend(const char* systemName,
+                                                  const uint8_t* data,
+                                                  uint32_t length)
+{
+  return PlatformImplInterface::GetSingleton().IpcSend(systemName, data,
+                                                       length);
+}
+
 __declspec(dllexport) bool SKSEPlugin_Query(void* skse, void* pluginInfo)
 {
   try {

overlay_ports/jsengine/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
 	OUT_SOURCE_PATH SOURCE_PATH
 	REPO skyrim-multiplayer/JsEngine
-	REF 523151bb3cc67254fa29eda695c82f302d2a8189
-	SHA512 d44bebcece66e060282000e1c93e86646287f7ab5b955758236009b1775c7c009038f04fdec38555eabb0cf5836fada6b681b3c88fa524ef309d2391d2b78ad4
+	REF 86fbd1854234c021c429d40dca561f7130434482
+	SHA512 76132d2910d510438cce85e0a557d38829289e4174fde0e757d4dc0042e3ee6c2bee561e90abe7d3a3bf4513d3ec95143efddb308c177b678ae11bae28e8f8c5
 	HEAD_REF master
 )
 

src/platform_se/codegen/index.js
@@ -37,6 +37,7 @@ export declare function getJsMemoryUsage(): number;
 export declare function getPluginSourceCode(pluginName: string): string;
 export declare function writePlugin(pluginName: string, newSources: string): string;
 export declare function getPlatformVersion(): string;
+export declare function sendIpcMessage(targetSystemName: string, message: ArrayBuffer): void;
 export declare let storage: Record<string, unknown>;
 export declare let settings: Record<string, Record<string, unknown>>;
 
@@ -45,6 +46,14 @@ export declare function once(eventName: 'update', callback: () => void): void;
 
 export declare function on(eventName: 'tick', callback: () => void): void;
 export declare function once(eventName: 'tick', callback: () => void): void;
+
+export interface IpcMessageEvent {
+    sourceSystemName: string;
+    message: ArrayBuffer;
+}
+
+export declare function on(eventName: 'ipcMessage', callback: (event: IpcMessageEvent) => void): void;
+export declare function once(eventName: 'ipcMessage', callback: (event: IpcMessageEvent) => void): void;
 
 export interface Face {
 	hairColor: number;

commit e5d0a54bc8dba7cdf969b143a023eb60d93f82ab

[MINOR] Add state parameter to IPC message callback
Breaking changes in IPC API introduced in
c95f14fbda4e33bc6573c7da2b2b39e85efaaf2f
However, tagged with [MINOR] because no version was released since IPC API has been added.

src/platform_se/skyrim_platform/EventsApi.cpp
@@ -11,6 +11,7 @@
 #include <map>
 #include <optional>
 #include <set>
+#include <tuple>
 #include <unordered_map>
 
 #include <RE\ConsoleLog.h>
@@ -286,11 +287,34 @@ struct EventsGlobalState
   std::shared_ptr<Hook> sendPapyrusEvent;
 } g;
 
+class IpcCallbackData
+{
+public:
+  IpcCallbackData() = default;
+  IpcCallbackData(std::string systemName_,
+                  EventsApi::IpcMessageCallback callback_, void* state_)
+    : systemName(systemName_)
+    , callback(callback_)
+    , state(state_)
+  {
+  }
+
+  friend bool operator==(const IpcCallbackData& lhs,
+                         const IpcCallbackData& rhs)
+  {
+    return std::make_tuple(lhs.systemName, lhs.callback, lhs.state) ==
+      std::make_tuple(rhs.systemName, rhs.callback, rhs.state);
+  }
+
+  std::string systemName;
+  EventsApi::IpcMessageCallback callback;
+  void* state = nullptr;
+};
+
 struct IpcShare
 {
   std::recursive_mutex m;
-  std::vector<std::pair<std::string, EventsApi::IpcMessageCallback>>
-    ipcCallbacks;
+  std::vector<IpcCallbackData> ipcCallbacks;
 } g_ipcShare;
 
 std::atomic<uint32_t> g_chakraThreadId = 0;
@@ -388,29 +412,29 @@ JsValue EventsApi::GetHooks()
 }
 
 uint32_t EventsApi::IpcSubscribe(const char* systemName,
-                                 IpcMessageCallback callback)
+                                 IpcMessageCallback callback, void* state)
 {
   // Maybe they decide calling IpcSubscribe from multiple threads...
   std::lock_guard l(g_ipcShare.m);
 
-  auto it =
-    std::find(g_ipcShare.ipcCallbacks.begin(), g_ipcShare.ipcCallbacks.end(),
-              std::pair<std::string, IpcMessageCallback>{ "", nullptr });
-  if (it == g_ipcShare.ipcCallbacks.end()) {
-    g_ipcShare.ipcCallbacks.push_back({ systemName, callback });
-    return g_ipcShare.ipcCallbacks.size() - 1;
-  }
+  IpcCallbackData ipcCallbackData(systemName, callback, state);
 
-  it->first = systemName;
-  it->second = callback;
-  return static_cast<uint32_t>(it - g_ipcShare.ipcCallbacks.begin());
+  auto it = std::find(g_ipcShare.ipcCallbacks.begin(),
+                      g_ipcShare.ipcCallbacks.end(), IpcCallbackData());
+  if (it == g_ipcShare.ipcCallbacks.end()) {
+    g_ipcShare.ipcCallbacks.push_back(ipcCallbackData);
+    return g_ipcShare.ipcCallbacks.size() - 1;
+  } else {
+    *it = ipcCallbackData;
+    return static_cast<uint32_t>(it - g_ipcShare.ipcCallbacks.begin());
+  }
 }
 
 void EventsApi::IpcUnsubscribe(uint32_t subscriptionId)
 {
   std::lock_guard l(g_ipcShare.m);
   if (g_ipcShare.ipcCallbacks.size() > subscriptionId) {
-    g_ipcShare.ipcCallbacks[subscriptionId] = { "", nullptr };
+    g_ipcShare.ipcCallbacks[subscriptionId] = IpcCallbackData();
   }
   // TODO: pop_back for empty subscriptions?
 }
@@ -498,20 +522,21 @@ JsValue EventsApi::SendIpcMessage(const JsFunctionArguments& args)
       "sendIpcMessage expects a valid ArrayBuffer instance");
   }
 
-  std::vector<IpcMessageCallback> callbacks;
+  std::vector<IpcCallbackData> callbacks;
   {
     std::lock_guard l(g_ipcShare.m);
-    for (auto& [systemName, cb] : g_ipcShare.ipcCallbacks) {
-      if (systemName == targetSystemName) {
-        callbacks.push_back(cb);
+    for (auto& callbackData : g_ipcShare.ipcCallbacks) {
+      if (callbackData.systemName == targetSystemName) {
+        callbacks.push_back(callbackData);
       }
     }
   }
 
   // Want to call callbacks with g_ipcShare.m unlocked
-  for (auto& cb : callbacks) {
-    if (cb) {
-      cb(reinterpret_cast<uint8_t*>(message), messageLength);
+  for (auto& callbackData : callbacks) {
+    if (callbackData.callback) {
+      callbackData.callback(reinterpret_cast<uint8_t*>(message), messageLength,
+                            callbackData.state);
     }
   }
 

src/platform_se/skyrim_platform/EventsApi.h
@@ -22,8 +22,10 @@ void SendPapyrusEventLeave() noexcept;
 
 JsValue GetHooks();
 
-typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length);
-uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback);
+typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length,
+                                   void* state);
+uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback,
+                      void* state);
 void IpcUnsubscribe(uint32_t subscriptionId);
 void IpcSend(const char* systemName, const uint8_t* data, uint32_t length);
 

src/platform_se/skyrim_platform/main.cpp
@@ -296,9 +296,10 @@ void OnPapyrusUpdate(RE::BSScript::IVirtualMachine* vm, RE::VMStackID stackId)
 extern "C" {
 __declspec(dllexport) uint32_t
   SkyrimPlatform_IpcSubscribe_Impl(const char* systemName,
-                                   EventsApi::IpcMessageCallback callback)
+                                   EventsApi::IpcMessageCallback callback,
+                                   void* state)
 {
-  return EventsApi::IpcSubscribe(systemName, callback);
+  return EventsApi::IpcSubscribe(systemName, callback, state);
 }
 
 __declspec(dllexport) void SkyrimPlatform_IpcUnsubscribe_Impl(

src/platform_se/skyrim_platform_entry/main.cpp
@@ -5,12 +5,14 @@
 #include <string>
 #include <vector>
 
-typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length);
+typedef void (*IpcMessageCallback)(const uint8_t* data, uint32_t length,
+                                   void* state);
 
 typedef bool (*SKSEPlugin_Query_Impl)(void*, void*);
 typedef bool (*SKSEPlugin_Load_Impl)(void*);
 typedef uint32_t (*SkyrimPlatform_IpcSubscribe_Impl)(const char*,
-                                                     IpcMessageCallback);
+                                                     IpcMessageCallback,
+                                                     void*);
 typedef void (*SkyrimPlatform_IpcUnsubscribe_Impl)(uint32_t);
 typedef void (*SkyrimPlatform_IpcSend_Impl)(const char*, const uint8_t*,
                                             uint32_t);
@@ -28,9 +30,10 @@ public:
 
   bool Load(void* skse) { return load(skse); }
 
-  uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback)
+  uint32_t IpcSubscribe(const char* systemName, IpcMessageCallback callback,
+                        void* state)
   {
-    return ipcSubscribe(systemName, callback);
+    return ipcSubscribe(systemName, callback, state);
   }
 
   void IpcUnsubscribe(uint32_t subscriptionId)
@@ -107,10 +110,10 @@ extern "C" {
 
 __declspec(dllexport) uint32_t
   SkyrimPlatform_IpcSubscribe(const char* systemName,
-                              IpcMessageCallback callback)
+                              IpcMessageCallback callback, void* state)
 {
   return PlatformImplInterface::GetSingleton().IpcSubscribe(systemName,
-                                                            callback);
+                                                            callback, state);
 }
 
 __declspec(dllexport) void SkyrimPlatform_IpcUnsubscribe(