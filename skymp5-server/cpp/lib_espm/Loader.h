ac08d26e94b028cce53a6d8288ad17e79f1d5de9
Leonid Pospelov <pospelovlm@yandex.ru>
2021-07-13 01:47:30 +0300

    Get absolute paths to work in loadOrder

--- skymp5-server/cpp/lib_espm/Loader.h
+++ skymp5-server/cpp/lib_espm/Loader.h
@@ -32,22 +32,22 @@ public:
   using OnProgress = std::function<void(std::string fileName, float readDur,
                                         float parseDur, uintmax_t fileSize)>;
 
-  Loader(const fs::path& dataDir, const std::vector<fs::path>& files_,
+  Loader(const fs::path& dataDir, const std::vector<fs::path>& fileNames,
          OnProgress onProgress = nullptr)
-    : files(files_)
+    : Loader(MakeFilePaths(dataDir, fileNames), onProgress)
   {
-    std::stringstream err;
-    if (!fs::exists(dataDir)) {
-      err << dataDir.string() << " directory doesn't exists";
-      throw LoadError(err);
-    }
+  }
 
-    for (const auto& file : files) {
+  Loader(const std::vector<fs::path>& filePaths_,
+         OnProgress onProgress = nullptr)
+    : filePaths(filePaths_)
+  {
+    for (const auto& p : filePaths) {
       entries.emplace_back();
       auto& entry = entries.back();
 
-      const fs::path p = dataDir / file;
       if (!fs::exists(p)) {
+        std::stringstream err;
         err << p.string() << " doesn't exists";
         throw LoadError(err);
       }
@@ -58,11 +58,12 @@ public:
 
       entry.buffer.reset(new std::vector<char>((size_t)size));
       if (f.read(entry.buffer->data(), size)) {
-        entry.fileName = file;
+        entry.fileName = p.filename().string();
         entry.readDuration = float(clock() - was) / CLOCKS_PER_SEC;
         entry.size = size;
       } else {
-        err << "Couldn't read" << std::endl;
+        std::stringstream err;
+        err << "Couldn't read" << p << std::endl;
         throw LoadError(err);
       }
       const clock_t was1 = clock();
@@ -90,13 +91,30 @@ public:
   std::vector<std::string> GetFileNames() const noexcept
   {
     std::vector<std::string> res;
-    res.reserve(files.size());
-    for (auto& p : files)
-      res.push_back(p.string());
+    res.reserve(filePaths.size());
+    for (auto& p : filePaths) {
+      res.push_back(p.filename().string());
+    }
     return res;
   }
 
 private:
+  std::vector<fs::path> MakeFilePaths(const fs::path& dataDir,
+                                      const std::vector<fs::path>& fileNames)
+  {
+    if (!fs::exists(dataDir)) {
+      std::stringstream err;
+      err << dataDir.string() << " directory doesn't exists";
+      throw LoadError(err);
+    }
+
+    std::vector<fs::path> res;
+    for (auto& fileName : fileNames) {
+      res.push_back(dataDir / fileName);
+    }
+    return res;
+  }
+
   struct Entry
   {
     std::unique_ptr<std::vector<char>> buffer;
@@ -111,6 +129,6 @@ private:
   std::vector<Entry> entries;
   std::unique_ptr<espm::Combiner> combiner;
   std::unique_ptr<espm::CombineBrowser> combineBrowser;
-  const std::vector<fs::path> files;
+  const std::vector<fs::path> filePaths;
 };
 }
