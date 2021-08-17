0bfc7d0dfd261bf0a9edadf1a2823e1e9c22792b
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-08-18 02:45:30 +0300

    move everything from Loader.h to cpp

--- skymp5-server/cpp/lib_espm/Loader.cpp
+++ skymp5-server/cpp/lib_espm/Loader.cpp
@@ -5,6 +5,69 @@
 
 namespace espm {
 
+Loader::Loader(const fs::path& dataDir, const std::vector<fs::path>& fileNames,
+               OnProgress onProgress, BufferType bufferType_)
+  : Loader(MakeFilePaths(dataDir, fileNames), onProgress, bufferType_)
+{
+}
+
+Loader::Loader(const std::vector<fs::path>& filePaths_, OnProgress onProgress,
+               BufferType bufferType_)
+  : filePaths(filePaths_)
+  , bufferType(bufferType_)
+{
+  for (const auto& p : filePaths) {
+    entries.emplace_back();
+    auto& entry = entries.back();
+
+    const clock_t was = clock();
+    entry.buffer = MakeBuffer(p);
+    entry.fileName = p.filename().string();
+    entry.readDuration = float(clock() - was) / CLOCKS_PER_SEC;
+    entry.size = entry.buffer->GetLength();
+
+    const clock_t was1 = clock();
+    entry.browser.reset(
+      new espm::Browser(entry.buffer->GetData(), entry.buffer->GetLength()));
+    entry.parseDuration = float(clock() - was1) / CLOCKS_PER_SEC;
+    if (onProgress) {
+      onProgress(entry.fileName.string(), entry.readDuration,
+                 entry.parseDuration, entry.size);
+    }
+  }
+  combiner = std::make_unique<espm::Combiner>();
+  for (auto& entry : entries) {
+    const auto fileName = entry.fileName.string();
+    combiner->AddSource(entry.browser.get(), fileName.c_str());
+  }
+  combineBrowser = combiner->Combine();
+}
+
+const espm::CombineBrowser& Loader::GetBrowser() const noexcept
+{
+  return *combineBrowser;
+}
+
+std::vector<std::string> Loader::GetFileNames() const noexcept
+{
+  std::vector<std::string> res;
+  res.reserve(filePaths.size());
+  for (auto& p : filePaths) {
+    res.push_back(p.filename().string());
+  }
+  return res;
+}
+
+std::vector<fs::path> Loader::MakeFilePaths(
+  const fs::path& dataDir, const std::vector<fs::path>& fileNames)
+{
+  std::vector<fs::path> res;
+  for (auto& fileName : fileNames) {
+    res.push_back(dataDir / fileName);
+  }
+  return res;
+}
+
 std::unique_ptr<impl::IBuffer> Loader::MakeBuffer(
   const fs::path& filePath) const
 {
--- skymp5-server/cpp/lib_espm/Loader.h
+++ skymp5-server/cpp/lib_espm/Loader.h
@@ -28,69 +28,19 @@ public:
 
   Loader(const fs::path& dataDir, const std::vector<fs::path>& fileNames,
          OnProgress onProgress = nullptr,
-         BufferType bufferType_ = BufferType::MappedBuffer)
-    : Loader(MakeFilePaths(dataDir, fileNames), onProgress, bufferType_)
-  {
-  }
+         BufferType bufferType_ = BufferType::MappedBuffer);
 
   Loader(const std::vector<fs::path>& filePaths_,
          OnProgress onProgress = nullptr,
-         BufferType bufferType_ = BufferType::MappedBuffer)
-    : filePaths(filePaths_)
-    , bufferType(bufferType_)
-  {
-    for (const auto& p : filePaths) {
-      entries.emplace_back();
-      auto& entry = entries.back();
+         BufferType bufferType_ = BufferType::MappedBuffer);
 
-      const clock_t was = clock();
-      entry.buffer = MakeBuffer(p);
-      entry.fileName = p.filename().string();
-      entry.readDuration = float(clock() - was) / CLOCKS_PER_SEC;
-      entry.size = entry.buffer->GetLength();
+  const espm::CombineBrowser& GetBrowser() const noexcept;
 
-      const clock_t was1 = clock();
-      entry.browser.reset(
-        new espm::Browser(entry.buffer->GetData(), entry.buffer->GetLength()));
-      entry.parseDuration = float(clock() - was1) / CLOCKS_PER_SEC;
-      if (onProgress) {
-        onProgress(entry.fileName.string(), entry.readDuration,
-                   entry.parseDuration, entry.size);
-      }
-    }
-    combiner = std::make_unique<espm::Combiner>();
-    for (auto& entry : entries) {
-      const auto fileName = entry.fileName.string();
-      combiner->AddSource(entry.browser.get(), fileName.c_str());
-    }
-    combineBrowser = combiner->Combine();
-  }
-
-  const espm::CombineBrowser& GetBrowser() const noexcept
-  {
-    return *combineBrowser;
-  }
-
-  std::vector<std::string> GetFileNames() const noexcept
-  {
-    std::vector<std::string> res;
-    res.reserve(filePaths.size());
-    for (auto& p : filePaths) {
-      res.push_back(p.filename().string());
-    }
-    return res;
-  }
+  std::vector<std::string> GetFileNames() const noexcept;
 
 private:
   std::vector<fs::path> MakeFilePaths(const fs::path& dataDir,
-                                      const std::vector<fs::path>& fileNames)
-  {
-    std::vector<fs::path> res;
-    for (auto& fileName : fileNames) {
-      res.push_back(dataDir / fileName);
-    }
-    return res;
-  }
+                                      const std::vector<fs::path>& fileNames);
 
   std::unique_ptr<impl::IBuffer> MakeBuffer(const fs::path& filePath) const;
 
