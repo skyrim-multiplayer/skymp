af5c9249ce5e23a6f3623c977dfe700e02e293db
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-08-18 01:18:20 +0300

    system category

--- skymp5-server/cpp/lib_espm/impl/MappedBuffer.cpp
+++ skymp5-server/cpp/lib_espm/impl/MappedBuffer.cpp
@@ -42,12 +42,12 @@ MappedBuffer::MappedBuffer(const fs::path& path)
 #else
   fd_ = open(path.c_str(), O_RDONLY);
   if (fd_ == -1) {
-    throw std::system_error(errno, std::generic_category(),
+    throw std::system_error(errno, std::system_category(),
                             fmt::format("Can't read {}", path.string()));
   }
   const auto mmapResult = mmap(NULL, size_, PROT_READ, MAP_SHARED, fd_, 0);
   if (mmapResult == MAP_FAILED) {
-    throw std::system_error(errno, std::generic_category(),
+    throw std::system_error(errno, std::system_category(),
                             fmt::format("Can't map {}", path.string()));
   }
   data_ = static_cast<char*>(mmapResult);
