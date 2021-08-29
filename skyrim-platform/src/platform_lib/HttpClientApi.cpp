67bb639a2525e4bd193b9b704b86d430653a34c9
Leonid Pospelov <pospelovlm@yandex.ru>
2021-08-29 10:20:03 +0300

    feat(skyrim-platform)!: support POST verb and HTTPS in HttpClient (#135)

---
+++ skyrim-platform/src/platform_lib/HttpClientApi.cpp
@@ -0,0 +1,113 @@
+#include "HttpClientApi.h"
+#include "CreatePromise.h"
+#include "HttpClient.h"
+
+namespace {
+HttpClient g_httpClient;
+
+template <class F>
+inline void IterateKeys(const JsValue& object, F fn)
+{
+  if (object.GetType() != JsValue::Type::Object) {
+    return;
+  }
+
+  auto builtinKeys =
+    JsValue::GlobalObject().GetProperty("Object").GetProperty("keys");
+  auto thisArg = JsValue::Undefined();
+
+  auto keys = builtinKeys.Call({ thisArg, object });
+  int length = static_cast<int>(keys.GetProperty("length"));
+  for (int i = 0; i < length; ++i) {
+    fn(keys.GetProperty(i));
+  }
+}
+
+inline HttpClient::Headers GetHeaders(const JsValue& options)
+{
+  if (options.GetType() != JsValue::Type::Object) {
+    return HttpClient::Headers();
+  }
+
+  auto headers = options.GetProperty("headers");
+  if (headers.GetType() != JsValue::Type::Object) {
+    return HttpClient::Headers();
+  }
+
+  HttpClient::Headers res;
+  IterateKeys(headers, [&](const JsValue& key) {
+    res.push_back({ static_cast<std::string>(key),
+                    static_cast<std::string>(headers.GetProperty(key)) });
+  });
+  return res;
+}
+}
+
+JsValue HttpClientApi::Constructor(const JsFunctionArguments& args)
+{
+  args[0].SetProperty(JsValue::String("host"), args[1]);
+  args[0].SetProperty("get", JsValue::Function(Get));
+  args[0].SetProperty("post", JsValue::Function(Post));
+  return JsValue::Undefined();
+}
+
+JsValue HttpClientApi::Get(const JsFunctionArguments& args)
+{
+  JsValue path = args[1], options = args[2],
+          host = args[0].GetProperty("host");
+
+  JsValue resolverFn = JsValue::Function([=](const JsFunctionArguments& args) {
+    auto resolve = std::make_shared<JsValue>(args[1]);
+    auto pathStr = static_cast<std::string>(path);
+    auto hostStr = static_cast<std::string>(host);
+    g_httpClient.Get(
+      hostStr.data(), pathStr.data(), GetHeaders(options),
+      [=](const HttpClient::HttpResult& res) {
+        auto result = JsValue::Object();
+        result.SetProperty(
+          "body",
+          JsValue::String(std::string{ res.body.begin(), res.body.end() }));
+        result.SetProperty("status", res.status);
+        resolve->Call({ JsValue::Undefined(), result });
+      });
+
+    return JsValue::Undefined();
+  });
+
+  auto promise = CreatePromise(resolverFn);
+
+  return promise;
+}
+
+JsValue HttpClientApi::Post(const JsFunctionArguments& args)
+{
+  JsValue path = args[1], options = args[2],
+          host = args[0].GetProperty("host");
+
+  auto resolverFn = JsValue::Function([=](const JsFunctionArguments& args) {
+    auto resolve = std::make_shared<JsValue>(args[1]);
+    auto pathStr = static_cast<std::string>(path);
+    auto hostStr = static_cast<std::string>(host);
+    auto bodyStr = static_cast<std::string>(options.GetProperty("body"));
+    auto type = static_cast<std::string>(options.GetProperty("contentType"));
+    g_httpClient.Post(
+      hostStr.data(), pathStr.data(), bodyStr.data(), type.data(),
+      GetHeaders(options), [=](const HttpClient::HttpResult& res) {
+        auto result = JsValue::Object();
+        result.SetProperty(
+          "body",
+          JsValue::String(std::string{ res.body.begin(), res.body.end() }));
+        result.SetProperty("status", res.status);
+        resolve->Call({ JsValue::Undefined(), result });
+      });
+
+    return JsValue::Undefined();
+  });
+
+  return CreatePromise(resolverFn);
+}
+
+HttpClient& HttpClientApi::GetHttpClient()
+{
+  return g_httpClient;
+}
--- skyrim-platform/src/platform_lib/ThreadPoolWrapper.h
+++ skyrim-platform/src/platform_lib/ThreadPoolWrapper.h
@@ -1,23 +1,25 @@
 #pragma once
+#include <cstdint>
 #include <ctpl/ctpl_stl.h>
 #include <memory>
 
 class ThreadPoolWrapper
 {
 public:
-  ThreadPoolWrapper(int numThreads_ = 1)
+  ThreadPoolWrapper(int32_t numThreads_ = 1)
     : numThreads(numThreads_)
   {
   }
 
-  auto Push(std::function<void(int)> f)
+  auto Push(std::function<void(int32_t)> f)
   {
-    if (!pool)
-      pool.reset(new ctpl::thread_pool(numThreads));
+    if (!pool) {
+      pool = std::make_unique<ctpl::thread_pool>(numThreads);
+    }
     return pool->push(f);
   }
 
 private:
   std::unique_ptr<ctpl::thread_pool> pool;
-  const int numThreads;
+  const int32_t numThreads;
 };
