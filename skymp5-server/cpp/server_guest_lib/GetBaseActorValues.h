44dfed5d4e6bedb80daaef9c157d6fe7dec26c46
Egor <ejs.defolt@gmail.com>
2021-09-15 18:17:26 +0500

    fixed clang-fromatting once more

--- skymp5-server/cpp/server_guest_lib/GetBaseActorValues.h
+++ skymp5-server/cpp/server_guest_lib/GetBaseActorValues.h
@@ -10,15 +10,15 @@ struct BaseActorValues
 
 BaseActorValues GetBaseActorValues(const uint32_t& baseId, const uint32_t& raceIdOverride)
 {
-	BaseActorValues baseActorValues;
-	baseActorValues.health = 100;
-	baseActorValues.stamina = 100;
-	baseActorValues.magicka = 100;
-	baseActorValues.healRate = 100;
-	baseActorValues.staminaRate = 100;
-	baseActorValues.magickaRate = 100;
-	baseActorValues.healRateMult = 100;
-	baseActorValues.magickaRateMult = 100;
-	baseActorValues.staminaRateMult = 100;
-	return baseActorValues;
+  BaseActorValues baseActorValues;
+  baseActorValues.health = 100;
+  baseActorValues.stamina = 100;
+  baseActorValues.magicka = 100;
+  baseActorValues.healRate = 100;
+  baseActorValues.staminaRate = 100;
+  baseActorValues.magickaRate = 100;
+  baseActorValues.healRateMult = 100;
+  baseActorValues.magickaRateMult = 100;
+  baseActorValues.staminaRateMult = 100;
+  return baseActorValues;
 }
