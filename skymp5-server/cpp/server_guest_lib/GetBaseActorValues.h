4814c8f6435dcdc604a61638041bf2546a395188
Egor <55379903+d0gge@users.noreply.github.com>
2021-09-15 22:45:12 +0500

    feat(skymp5-server): add basic GetBaseActorValues function (#205)

---
+++ skymp5-server/cpp/server_guest_lib/GetBaseActorValues.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <cstdint>
+
+struct BaseActorValues
+{
+  float health = 100;
+  float stamina = 100;
+  float magicka = 100;
+  float healRate = 100;
+  float staminarRate = 100;
+  float magickaRate = 100;
+  float healRateMult = 100;
+  float staminaRateMult = 100;
+  float magickaRateMult = 100;
+};
+
+inline BaseActorValues GetBaseActorValues(uint32_t baseId,
+                                          uint32_t raceIdOverride)
+{
+  BaseActorValues baseActorValues;
+  return baseActorValues;
+}
