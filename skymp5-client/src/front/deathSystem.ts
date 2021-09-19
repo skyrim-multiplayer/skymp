f9b1cfa3e3b2bea3d16e6555a2cbdbb1fe83e0c5
ZikkeyLS <hisaha21@yandex.ru>
2021-09-19 12:06:04 +0300

    Format fix

--- skymp5-client/src/front/deathSystem.ts
+++ skymp5-client/src/front/deathSystem.ts
@@ -3,27 +3,27 @@ import { hooks, Game, printConsole, Actor } from "skyrimPlatform";
 let gAllowGetUp = true;
 
 export const update = (): void => {
-    gAllowGetUp =
-        (Game.getPlayer() as Actor).getActorValuePercentage("health") >= 0.05;
+  gAllowGetUp =
+    (Game.getPlayer() as Actor).getActorValuePercentage("health") >= 0.05;
 };
 
 hooks.sendAnimationEvent.add({
-    enter(ctx) {
-        if (ctx.animEventName.toLowerCase().includes("killmove")) {
-            ctx.animEventName = "";
-        }
+  enter(ctx) {
+    if (ctx.animEventName.toLowerCase().includes("killmove")) {
+      ctx.animEventName = "";
+    }
 
-        if (ctx.selfId !== 0x14) return;
-        if (!gAllowGetUp && ctx.animEventName === "GetUpBegin") {
-            ctx.animEventName = "";
-            printConsole("block GetUpBegin");
-        }
-    },
-    leave() {
-        return;
-    },
+    if (ctx.selfId !== 0x14) return;
+    if (!gAllowGetUp && ctx.animEventName === "GetUpBegin") {
+      ctx.animEventName = "";
+      printConsole("block GetUpBegin");
+    }
+  },
+  leave() {
+    return;
+  },
 });
 
 export const MakePlayerImmortal = (): void => {
-    Game.getPlayer()?.startDeferredKill();
+  Game.getPlayer()?.startDeferredKill();
 };
