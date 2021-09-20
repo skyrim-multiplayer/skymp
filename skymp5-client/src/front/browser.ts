c86ac5a42e667c994b0f68e987624ef7ae427c94
alekcey0211 <34068026+alekcey0211@users.noreply.github.com>
2021-09-21 00:14:23 +0300

    refactoring

--- skymp5-client/src/front/browser.ts
+++ skymp5-client/src/front/browser.ts
@@ -13,99 +13,101 @@ import { DXScanCodes } from '../lib/dx-scan-codes';
 import { EventEmitter } from '../lib/event-emitter';
 import { badMenus } from '../lib/ui-menu';
 
-const badMenus: UiMenu[] = [
-	UiMenu.Barter,
-	UiMenu.Book,
-	UiMenu.Container,
-	UiMenu.Crafting,
-	UiMenu.Gift,
-	UiMenu.Inventory,
-	UiMenu.Journal,
-	UiMenu.Lockpicking,
-	UiMenu.Loading,
-	UiMenu.Map,
-	UiMenu.RaceSex,
-	UiMenu.Stats,
-	UiMenu.Tween,
-	UiMenu.Console,
-	UiMenu.Main,
-];
-
-const getMultiBindigString = (...codes: DXScanCodes[]) => codes.sort((a, b) => a - b).join('-');
-
 export const main = (): void => {
   const emitter = new EventEmitter();
 
-  let noBadMenuOpen = true;
-  let lastBadMenuCheck = 0;
+	const badMenus: UiMenu[] = [
+		UiMenu.Barter,
+		UiMenu.Book,
+		UiMenu.Container,
+		UiMenu.Crafting,
+		UiMenu.Gift,
+		UiMenu.Inventory,
+		UiMenu.Journal,
+		UiMenu.Lockpicking,
+		UiMenu.Loading,
+		UiMenu.Map,
+		UiMenu.RaceSex,
+		UiMenu.Stats,
+		UiMenu.Tween,
+		UiMenu.Console,
+		UiMenu.Main,
+	];
 
-  let visible = false;
-  const browserSetVisible = (state: boolean) => {
-    visible = state;
-    browser.setVisible(state);
-  };
-  browser.setVisible(false);
-  once("update", () => {
-    visible = true;
-    browser.setVisible(true);
-  });
+	let browserVisibleState = false;
+	browser.setVisible(false);
+	const setBrowserVisible = (state: boolean) => {
+		browserVisibleState = state;
+		browser.setVisible(state);
+	};
+	once('update', () => {
+		browserVisibleState = true;
+		browser.setVisible(true);
+	});
 
-  let focused = false;
-  const browserSetFocused = (state: boolean) => {
-    focused = state;
-    browser.setFocused(state);
-  };
+	let browserFocusedState = false;
+	const setBrowserFocused = (state: boolean) => {
+		browserFocusedState = state;
+		browser.setFocused(state);
+	};
 
-  const inputChangeEvent = 'event:input-change';
-  const keyState: { num: number } = { num: 0 };
+	let badMenuOpen = true;
 
-  on('update', () => {
-    const numKeys = Input.getNumKeysPressed();
+	let lastBadMenuCheck = 0;
+	on('update', () => {
+		if (Date.now() - lastBadMenuCheck > 200) {
+			lastBadMenuCheck = Date.now();
+			badMenuOpen = badMenus.findIndex((menu) => Ui.isMenuOpen(menu)) !== -1;
+			browser.setVisible(browserVisibleState && !badMenuOpen);
+		}
+	});
 
-    if (keyState.num !== numKeys) {
-      keyState.num = numKeys;
-      const keyCodes = Array(numKeys)
-        .fill(null)
-        .map((_, i) => Input.getNthKeyPressed(i));
-      emitter.emit(inputChangeEvent, keyCodes);
-    }
+	const onInputChangeEventName = 'event:input-change';
 
-    if (Date.now() - lastBadMenuCheck > 200) {
-      lastBadMenuCheck = Date.now();
-      noBadMenuOpen = badMenus.findIndex((menu) => Ui.isMenuOpen(menu)) === -1;
-    }
+	const singleBindings: Record<number, () => void> = {
+		[DxScanCodes.F2]: () => setBrowserVisible(!browserVisibleState),
+		[DxScanCodes.F6]: () => setBrowserFocused(!browserFocusedState),
+		[DxScanCodes.Escape]: () => (browserFocusedState ? setBrowserFocused(false) : undefined)
+	};
 
-    browser.setVisible(visible && noBadMenuOpen)
-  });
+	const getMultiBindigString = (...codes: DxScanCodes[]) => codes.sort((a, b) => a - b).join('-');
+	const multiBindings: Record<string, () => void> = {
+		[getMultiBindigString(DxScanCodes.LeftShift, DxScanCodes.Tab)]: () => {
+		  // example
+		},
+	};
 
-  const singleBindings: Record<number, () => void> = {
-    [DXScanCodes.F2]: () => browserSetVisible(!visible),
-    [DXScanCodes.F6]: () => browserSetFocused(!focused),
-    [DXScanCodes.Escape]: () => (focused ? browserSetFocused(false) : undefined),
-  };
+	let lastNumKeys = 0;
+	on('update', () => {
+		const numKeys = Input.getNumKeysPressed();
 
-  const multiBindings: Record<string, () => void> = {
-    [getMultiBindigString(DXScanCodes.LeftShift, DXScanCodes.Tab)]: () => {
-      // example use multi keys binding
-    },
-  };
+		if (lastNumKeys !== numKeys) {
+			lastNumKeys = numKeys;
 
-  emitter.subscribe(inputChangeEvent, (data) => {
-    if (!Array.isArray(data)) return;
+			const keyCodes = Array(numKeys)
+				.fill(null)
+				.map((_, i) => Input.getNthKeyPressed(i));
 
-    const keycodes: number[] = data;
-    if (keycodes.length === 0) return;
+			emitter.emit(onInputChangeEventName, keyCodes);
+		}
+	});
 
-    const multi: string = keycodes.join('-');
+	emitter.subscribe(onInputChangeEventName, (data) => {
+		if (!Array.isArray(data)) return;
 
-    if (multiBindings[multi]) {
-      multiBindings[multi]();
-      return;
-    }
+		const keycodes: number[] = data;
+		if (keycodes.length === 0) return;
 
-    const single: number = keycodes[0];
-    if (singleBindings[single]) singleBindings[single]();
-  });
+		const multi: string = keycodes.join('-');
+
+		if (multiBindings[multi]) {
+			multiBindings[multi]();
+			return;
+		}
+
+		const single: number = keycodes[0];
+		if (singleBindings[single]) singleBindings[single]();
+	});
 
   const cfg = {
     ip: settings["skymp5-client"]["server-ip"],
