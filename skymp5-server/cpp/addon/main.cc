8f59c4fc226217c0e44193017141042d82348a31
Korisss <vaginpawel2005@outlook.com>
2021-09-20 20:50:44 +0300

    test clang-format

--- skymp5-server/cpp/addon/main.cc
+++ skymp5-server/cpp/addon/main.cc
@@ -986,41 +986,42 @@ void ScampServer::RegisterChakraApi(std::shared_ptr<JsEngine> chakraEngine)
     }));
 
   mp.SetProperty("readDataFile",
-    JsValue::Function([this](const JsFunctionArguments& args) {
-      std::string path = args[1];
-      if (path.find("..") != std::string::npos) {
-        throw std::runtime_error(
-          "readDataFile doesn't support paths containing '..'");
-      }
-      auto dataDir = GetDataDirSafe(serverSettings);
-      auto filePath = std::filesystem::path(dataDir) / path;
+                 JsValue::Function([this](const JsFunctionArguments& args) {
+                   std::string path = args[1];
+                   if (path.find("..") != std::string::npos) {
+                     throw std::runtime_error(
+                       "readDataFile doesn't support paths containing '..'");
+                   }
+                   auto dataDir = GetDataDirSafe(serverSettings);
+                   auto filePath = std::filesystem::path(dataDir) / path;
 
-      std::ifstream t(filePath);
-      std::stringstream buffer;
-      buffer << t.rdbuf();
-      return buffer.str();
-  }));
+                   std::ifstream t(filePath);
+                   std::stringstream buffer;
+                   buffer << t.rdbuf();
+                   return buffer.str();
+                 }));
 
-  mp.SetProperty("writeDataFile", 
-    JsValue::Function([this](const JsFunctionArguments& args) {
-      std::string path = args[1];
+  mp.SetProperty("writeDataFile",
+                 JsValue::Function([this](const JsFunctionArguments& args) {
+                   std::string path = args[1];
 
-      if (path.find("..") != std::string::npos) {
-        throw std::runtime_error("writeDataFile doesn't support paths containing '..'");
-      }
+                   if (path.find("..") != std::string::npos) {
+                     throw std::runtime_error(
+                       "writeDataFile doesn't support paths containing '..'");
+                   }
 
-      auto dataDir = GetDataDirSafe(serverSettings);
-      auto filePath = std::filesystem::path(dataDir) / path;
+                   auto dataDir = GetDataDirSafe(serverSettings);
+                   auto filePath = std::filesystem::path(dataDir) / path;
 
-      std::string stringToWrite = args[2];
-      std::ofstream dataFile(filePath);
+                   std::string stringToWrite = args[2];
+                   std::ofstream dataFile(filePath);
 
-      dataFile << stringToWrite;
+                   dataFile << stringToWrite;
 
-      dataFile.close();
+                   dataFile.close();
 
-      return JsValue::Undefined();
-  }));
+                   return JsValue::Undefined();
+                 }));
 
   auto update = [this] {
     partOne->NotifyGamemodeApiStateChanged(gamemodeApiState);
