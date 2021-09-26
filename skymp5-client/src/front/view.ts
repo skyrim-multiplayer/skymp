cbb77af6e3ceb514aebd57b361e9c8fd621f33bc
Zikkey <68398139+ZikkeyLS@users.noreply.github.com>
2021-09-26 18:34:13 +0300

    feat(skyrim5-client): set in-game ActorValues to the values specified in `createActor` packet on the client (#240)
    Closes #187

--- skymp5-client/src/front/view.ts
+++ skymp5-client/src/front/view.ts
@@ -326,13 +326,13 @@ export class FormView implements View<FormModel> {
         );
         if (model.look && model.look.name)
           refr.setDisplayName("" + model.look.name, true);
+
+        const actor = Actor.from(refr);
+        if (actor) {
+          deathSystem.makeActorImmortal(actor);
+        }
       }
       this.refrId = (refr as ObjectReference).getFormID();
-
-      const actor = Actor.from(refr);
-      if (actor) {
-        deathSystem.makeActorImmortal(actor);
-      }
     }
 
     if (!this.ready) return;
