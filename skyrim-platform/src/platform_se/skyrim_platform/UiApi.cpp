8430dbc07724f6e1787a2c20364b16514a88c907
russo-2025 <russo-2025@mail.ru>
2021-09-27 12:50:50 +0300

    cleanup

--- skyrim-platform/src/platform_se/skyrim_platform/UiApi.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/UiApi.cpp
@@ -2,50 +2,37 @@
 #include "EventsApi.h"
 #include "GameEventSinks.h"
 #include <RE/UI.h>
+#include <RE/MenuOpenCloseEvent.h>
 
 extern TaskQueue g_taskQueue;
 
-namespace RE {
-    //need to update CommonLibSSE (missing in the current version)
-    //https://github.com/Ryan-rsm-McKenzie/CommonLibSSE/blob/01374c521e6ab485e5aca4de83a2d2fdc4c9c3c1/include/RE/M/MenuOpenCloseEvent.h
-    class MenuOpenCloseEvent
+namespace UiApi {
+    void onMenuOpenClose(const char* menuName, bool opening)
     {
-    public:
-        // members
-        BSFixedString menuName;  // 00
-        bool          opening;   // 08
-        std::uint8_t  pad09;     // 09
-        std::uint16_t pad0A;     // 0A
-        std::uint32_t pad0C;     // 0C
-    };
-    static_assert(sizeof(MenuOpenCloseEvent) == 0x10);
-    //>>>
-}
-
-void onMenuOpenClose(const char * menuName, bool opening) {
-    g_taskQueue.AddTask([=] {
+      g_taskQueue.AddTask([=] {
         auto obj = JsValue::Object();
 
         obj.SetProperty("name", JsValue::String(menuName));
-        obj.SetProperty("type", JsValue::String( opening ? "open" : "close"));
+        obj.SetProperty("type", JsValue::String(opening ? "open" : "close"));
 
         EventsApi::SendEvent("menuOpenClose", { JsValue::Undefined(), obj });
-    });
-}
+      });
+    }
 
-class MyEventSink : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
-{
-public:
-    ~MyEventSink() {};
-    RE::BSEventNotifyControl ProcessEvent(const RE::MenuOpenCloseEvent* e, RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_eventSource) override {
+    class MyEventSink : public RE::BSTEventSink<RE::MenuOpenCloseEvent>
+    {
+    public:
+      ~MyEventSink(){};
+      RE::BSEventNotifyControl ProcessEvent(
+        const RE::MenuOpenCloseEvent* e,
+        RE::BSTEventSource<RE::MenuOpenCloseEvent>* a_eventSource) override
+      {
         onMenuOpenClose(e->menuName.c_str(), e->opening);
 
         return RE::BSEventNotifyControl::kContinue;
+      };
     };
-};
 
-namespace UiApi
-{
     void Register(JsValue& exports)
     {
         auto ui = RE::UI::GetSingleton();
