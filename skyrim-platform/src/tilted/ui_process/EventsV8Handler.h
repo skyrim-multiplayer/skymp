fbb63c1a9f81bd1cefff3aced86eebdf56398cba
Leonid Pospelov <pospelovlm@yandex.ru>
2021-09-27 20:57:31 +0300

    Fix clang-format again

--- skyrim-platform/src/tilted/ui_process/EventsV8Handler.h
+++ skyrim-platform/src/tilted/ui_process/EventsV8Handler.h
@@ -1,32 +1,34 @@
 #pragma once
 
-#include <include/cef_v8.h>
 #include <Stl.hpp>
+#include <include/cef_v8.h>
 
-namespace CEFUtils
+namespace CEFUtils {
+using TCallbacks =
+  Map<std::string, std::pair<CefRefPtr<CefV8Context>, CefRefPtr<CefV8Value>>>;
+
+struct EventsV8Handler final : CefV8Handler
 {
-    using TCallbacks = Map<std::string, std::pair<CefRefPtr<CefV8Context>, CefRefPtr<CefV8Value>>>;
+  explicit EventsV8Handler(CefRefPtr<CefV8Context> apContext) noexcept;
+  ~EventsV8Handler() = default;
 
-    struct EventsV8Handler final : CefV8Handler
-    {
-        explicit EventsV8Handler(CefRefPtr<CefV8Context> apContext) noexcept;
-        ~EventsV8Handler() = default;
+  EventsV8Handler(EventsV8Handler&&) = delete;
+  EventsV8Handler(const EventsV8Handler&) = delete;
+  EventsV8Handler& operator=(EventsV8Handler&&) = delete;
+  EventsV8Handler& operator=(const EventsV8Handler&) = delete;
 
-        EventsV8Handler(EventsV8Handler&&) = delete;
-        EventsV8Handler(const EventsV8Handler&) = delete;
-        EventsV8Handler& operator=(EventsV8Handler&&) = delete;
-        EventsV8Handler& operator=(const EventsV8Handler&) = delete;
+  bool Execute(const CefString& acName, CefRefPtr<CefV8Value> apObject,
+               const CefV8ValueList& acArguments,
+               CefRefPtr<CefV8Value>& aReturnValue,
+               CefString& aException) override;
 
-        bool Execute(const CefString& acName, CefRefPtr<CefV8Value> apObject, const CefV8ValueList& acArguments, CefRefPtr<CefV8Value>& aReturnValue, CefString& aException) override;
+  const TCallbacks& GetCallbacks() const noexcept;
 
-        const TCallbacks& GetCallbacks() const noexcept;
+  IMPLEMENT_REFCOUNTING(EventsV8Handler);
 
-        IMPLEMENT_REFCOUNTING(EventsV8Handler);
+private:
+  TCallbacks m_callbacks;
 
-    private:
-
-        TCallbacks m_callbacks;
-
-        CefRefPtr<CefV8Context> m_pContext;
-    };
+  CefRefPtr<CefV8Context> m_pContext;
+};
 }
