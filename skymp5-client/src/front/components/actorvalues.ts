eae25905bfa4426881db10fe4479ade599bed3eb
alekcey0211 <alekcey_ezh@mail.ru>
2021-10-04 12:30:44 +0300

    more more

--- skymp5-client/src/front/components/actorvalues.ts
+++ skymp5-client/src/front/components/actorvalues.ts
@@ -1,27 +1,25 @@
-import { Actor } from "skyrimPlatform";
+import {
+    Actor,
+} from "skyrimPlatform";
 
 import * as structures from "../../lib/structures/actorvalues";
 export type ActorValues = structures.ActorValues;
 
 export const getActorValues = (ac: Actor): ActorValues => {
-  if (!ac) return { health: 0, stamina: 0, magicka: 0 };
-  let healthPercentage = ac.isDead() ? 0 : ac.getActorValuePercentage("health");
-  const staminaPercentage = ac.getActorValuePercentage("stamina");
-  const magickaPercentage = ac.getActorValuePercentage("magicka");
+    if (!ac) return { health: 0, stamina: 0, magicka: 0 };
+    let healthPercentage = (ac.isDead()) ? 0 : ac.getActorValuePercentage("health");
+    const staminaPercentage = ac.getActorValuePercentage("stamina");
+    const magickaPercentage = ac.getActorValuePercentage("magicka");
 
-  const resultActorValue: ActorValues = {
-    health: healthPercentage,
-    stamina: staminaPercentage,
-    magicka: magickaPercentage,
-  };
-  return resultActorValue;
-};
+    const resultActorValue: ActorValues = {
+        health: healthPercentage,
+        stamina: staminaPercentage,
+        magicka: magickaPercentage,
+    };
+    return resultActorValue;
+}
 
-export const setActorValuePercentage = (
-  ac: Actor,
-  avName: string,
-  percentage: number
-) => {
+export const setActorValuePercentage = (ac: Actor, avName: string, percentage: number) => {
   const currentPercentage = ac.getActorValuePercentage(avName);
   if (currentPercentage === percentage) return;
 
