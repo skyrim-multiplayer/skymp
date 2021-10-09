0e42c5d289ba26acf5e3d91ff3089dde3a3a4f35
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-10-09 20:09:04 +0300

    formatting

--- skymp5-server/cpp/lib_espm/espm.h
+++ skymp5-server/cpp/lib_espm/espm.h
@@ -661,7 +661,7 @@ public:
     uint32_t race = 0;
     uint16_t healthOffset = 0;
     uint16_t magickaOffset = 0;
-    uint16_t staminaOffset = 0; 
+    uint16_t staminaOffset = 0;
   };
 
   Data GetData(CompressedFieldsCache& compressedFieldsCache) const noexcept;
