bddb57a7fe5ee8c88002ca72db0568ab94e1b8bd
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-10-09 19:11:26 +0300

    move tostring/fromstring to cpp

--- skymp5-server/cpp/mp_common/MovementData.h
+++ skymp5-server/cpp/mp_common/MovementData.h
@@ -5,6 +5,7 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <tuple>
 
 enum class RunMode
 {
@@ -14,42 +15,9 @@ enum class RunMode
   Sprinting,
 };
 
-const inline std::string g_standing = "Standing";
-const inline std::string g_walking = "Walking";
-const inline std::string g_running = "Running";
-const inline std::string g_sprinting = "Sprinting";
+const std::string& ToString(RunMode runMode);
 
-// XXX: move to cpp
-inline const std::string& ToString(RunMode runMode)
-{
-  switch (runMode) {
-    case RunMode::Standing:
-      return g_standing;
-    case RunMode::Walking:
-      return g_walking;
-    case RunMode::Running:
-      return g_running;
-    case RunMode::Sprinting:
-      return g_sprinting;
-    default:
-      throw std::runtime_error("unhandled case for RunMode");
-  }
-}
-
-inline RunMode RunModeFromString(std::string_view str)
-{
-  if (str == g_standing) {
-    return RunMode::Standing;
-  } else if (str == g_walking) {
-    return RunMode::Walking;
-  } else if (str == g_running) {
-    return RunMode::Running;
-  } else if (str == g_sprinting) {
-    return RunMode::Sprinting;
-  } else {
-    throw std::runtime_error("cannot parse RunMode from " + std::string{str});
-  }
-}
+RunMode RunModeFromString(std::string_view str);
 
 // XXX: rename to MovementMessage?
 struct MovementData
