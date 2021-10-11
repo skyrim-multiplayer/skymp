5400abfb867c1e62a2ba78f7d9fcc893dace7527
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-10-11 18:29:36 +0300

    insert changes back

--- skymp5-server/cpp/server_guest_lib/ActionListener.cpp
+++ skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -505,7 +505,8 @@ void ActionListener::OnChangeValues(const RawMessageData& rawMsgData,
 }
 
 namespace {
-float CalculateDamage(MpActor& actor, const HitData& hitData)
+float CalculateDamage(MpActor& actor, const HitData& hitData,
+                      espm::CompressedFieldsCache& compressedFieldCache)
 {
   // TODO(#200): Implement damage calculation logic
   if (!actor.GetParent()) {
@@ -520,7 +521,16 @@ float CalculateDamage(MpActor& actor, const HitData& hitData)
   const auto& browser = actor.GetParent()->GetEspm().GetBrowser();
 
   if (hitData.source == 0x1f4) {
-    return 5.f;
+    auto raceId = actor.GetLook()->raceId;
+    const auto lookUpRace = browser.LookupById(raceId);
+    if (!lookUpRace.rec || lookUpRace.rec->GetType() != "RACE") {
+      throw std::runtime_error(
+        fmt::format("Unable to get unarmed damage from {0:x}", raceId));
+    }
+
+    const auto raceData =
+      espm::Convert<espm::RACE>(lookUpRace.rec)->GetData(compressedFieldCache);
+    return raceData.unarmedDamage;
   }
 
   const auto lookUpWeapon = browser.LookupById(hitData.source);
@@ -556,7 +566,8 @@ void ActionListener::OnHit(const RawMessageData& rawMsgData,
     hitData.target = actor->GetFormId();
   }
 
-  const auto damage = CalculateDamage(*actor, hitData);
+  auto& espmCache = partOne.worldState.GetEspmCache();
+  const auto damage = CalculateDamage(*actor, hitData, espmCache);
 
   // TODO(#276): Send a packet
 }
