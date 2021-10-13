3f981a82d119bd41162ccbad289381757205dbb7
Leonid Pospelov <pospelovlm@yandex.ru>
2021-10-13 11:49:44 +0300

    Update GameEventSinks.cpp

--- skyrim-platform/src/platform_se/skyrim_platform/GameEventSinks.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/GameEventSinks.cpp
@@ -434,7 +434,11 @@ RE::BSEventNotifyControl GameEventSinks::ProcessEvent(
 
       auto causeLocal = RE::TESForm::LookupByID(causeId);
       causeLocal = causeLocal == causeRefr ? causeLocal : nullptr;
-      obj.SetProperty("agressor", CreateObject("ObjectReference", causeLocal));
+
+      // [MAJOR RELEASE] Remove "agressor"
+      auto aggressor = CreateObject("ObjectReference", causeLocal);
+      obj.SetProperty("agressor", aggressor);
+      obj.SetProperty("aggressor", aggressor);
 
       obj.SetProperty("source",
                       CreateObject("Form", RE::TESForm::LookupByID(sourceId)));
