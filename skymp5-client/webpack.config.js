a829d610af20e6e6240a77e96030d1299e653131
kkEngine <78795688+kkEngine@users.noreply.github.com>
2021-10-14 17:23:18 +0300

    - WIP using webpack and it's config

--- skymp5-client/webpack.config.js
+++ skymp5-client/webpack.config.js
@@ -1,16 +1,20 @@
 //webpack.config.js
 const path = require('path');
-const WebpackSystemRegister = require('webpack-system-register');
+//const WebpackSystemRegister = require('webpack-system-register');
 
 module.exports = {
     mode: "development",
     //mode: "production",
-    entry: {
-        main: "./src/index.ts",
-    },
-    plugins: [
-        new WebpackSystemRegister({}),
+    // entry: {
+    //     main: "./src/index.ts",
+    // },
+    entry: [
+        //"./node_modules/core-js/shim",
+        "./src/index.ts"
     ],
+    // plugins: [
+    //     new WebpackSystemRegister({}),
+    // ],
     devtool: "inline-source-map",
     output: {
         libraryTarget: "system",
@@ -18,6 +22,10 @@ module.exports = {
         filename: "skymp5-client.js", // <--- Will be compiled to this single file
     },
     resolve: {
+        modules: [
+            path.resolve(__dirname, '../build/dist/client/Data/Platform/Modules'),
+            "node_modules"
+        ],
         extensions: [".ts", ".tsx", ".js"],
     },
     optimization: {
@@ -25,18 +33,24 @@ module.exports = {
     },
     module: {
         rules: [
+            // {
+            //     test: /\.tsx?$/,
+            //     include: [
+            //         path.resolve(__dirname, '../build/dist/client/Data/Platform/Modules')
+            //     ],
+            //     use: "ts-loader"
+            // },
             {
                 test: /\.tsx?$/,
                 // include: [
-                //     // path.resolve(__dirname, 'src'),
+                //     path.resolve(__dirname, 'src'),
                 //     path.resolve(__dirname, '../build/dist/client/Data/Platform/Modules')
                 // ],
                 use: "ts-loader"
-
             }
         ]
     },
-    externals: {
-        "skyrimPlatform": path.resolve(__dirname, "../build/dist/client/Data/Platform/Modules")
-    }
+    // externals: {
+    //     "skyrimPlatform": path.resolve(__dirname, "../build/dist/client/Data/Platform/Modules/skyrimPlatform.ts")
+    // }
 };
