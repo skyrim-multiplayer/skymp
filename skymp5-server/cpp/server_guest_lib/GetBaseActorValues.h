3edfa8005c7e28046c6ba60b2bed5d8bbd818cce
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-10-14 21:37:54 +0300

    Update GetBaseActorValues.h

--- skymp5-server/cpp/server_guest_lib/GetBaseActorValues.h
+++ skymp5-server/cpp/server_guest_lib/GetBaseActorValues.h
@@ -8,14 +8,14 @@
 struct BaseActorValues
 {
   float health = 100.f;
-  float stamina = 100.f;
   float magicka = 100.f;
+  float stamina = 100.f;
   float healRate = 0.7f;
-  float staminaRate = 5.0f;
   float magickaRate = 3.0f;
+  float staminaRate = 5.0f;
   float healRateMult = 100.f;
-  float staminaRateMult = 100.f;
   float magickaRateMult = 100.f;
+  float staminaRateMult = 100.f;
 
   using PropertiesVisitor =
     std::function<void(const char* propName, const char* jsonValue)>;
