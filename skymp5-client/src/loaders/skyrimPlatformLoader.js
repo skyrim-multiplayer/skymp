d0e86ee991cf86ec32188b79a6167a82d70f3b38
Leonid Pospelov <pospelovlm@yandex.ru>
2021-10-19 19:22:07 +0300

    fixes

--- skymp5-client/src/loaders/skyrimPlatformLoader.js
+++
@@ -1,3 +0,0 @@
-module.exports = function (source) {
-  return `module.exports = addNativeExports("skyrimPlatform", exports);`;
-}
--- skymp5-client/webpack.config.js
+++ skymp5-client/webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   module: {
     rules: [{
       test: /.*skyrimPlatform.ts$/,
-      use: path.resolve('src/loaders/skyrimPlatformLoader.js'),
+      use: path.resolve('src/skyrimPlatformLoader.js'),
     }, {
       test: /\.tsx?$/,
       use: "ts-loader"
