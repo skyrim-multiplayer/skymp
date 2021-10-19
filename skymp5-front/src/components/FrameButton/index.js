82fa48c9aa71fd905564d22a47906f4b7bebe19e
q1000treadz <42095596+q1000treadz@users.noreply.github.com>
2021-10-19 16:42:52 +0300

    close multiple issues

--- skymp5-front/src/components/FrameButton/index.js
+++ skymp5-front/src/components/FrameButton/index.js
@@ -30,18 +30,18 @@ const ButtonItem = props => {
 const FrameButton = props => {
     let fwidth = props.width || 384;
     let fheight = props.height || 64;
-    let idDisabled = props.disabled != undefined ? props.disabled : true;
+    let isDisabled = props.disabled != undefined ? props.disabled : true;
     return (
         <div
-            className={`skymp-button ${idDisabled ? 'disabled' : 'active'}`}
+            className={`skymp-button ${isDisabled ? 'disabled' : 'active'}`}
             onClick={(e) => {
                 if (!props.disabled)
                     props.onClick ? props.onClick(e) : console.log(e)
             }}
             style={{ height: `${fheight}px`, width: `${fwidth}px` }}>
-            <ButtonItem name={`button start${idDisabled ? ' disabled' : ''}`} height={fheight} />
-            <ButtonItem name={`button middle${idDisabled ? ' disabled' : ''}`} width={fwidth - 64 * 2} height={fheight} text={props.text} />
-            <ButtonItem name={`button end${idDisabled ? ' disabled' : ''}`} height={fheight} />
+            <ButtonItem name={`button start${isDisabled ? ' disabled' : ''}`} height={fheight} />
+            <ButtonItem name={`button middle${isDisabled ? ' disabled' : ''}`} width={fwidth - 64 * 2} height={fheight} text={props.text} />
+            <ButtonItem name={`button end${isDisabled ? ' disabled' : ''}`} height={fheight} />
         </div>
     )
 }
