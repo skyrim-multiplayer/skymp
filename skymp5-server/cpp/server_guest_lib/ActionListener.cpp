033cf0eadb09cd73fb97580ea44db43152aa1c39
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-10-20 23:49:58 +0300

    dead characters cannot hit

--- skymp5-server/cpp/server_guest_lib/ActionListener.cpp
+++ skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -592,6 +592,12 @@ void ActionListener::OnHit(const RawMessageData& rawMsgData_,
     throw std::runtime_error("Unable to change values without Actor attached");
   }
 
+  if (aggressor->IsDead()) {
+    spdlog::debug(fmt::format("{:x} actor is dead and can't attack",
+                              aggressor->GetFormId()));
+    return;
+  }
+
   HitData hitData = hitData_;
 
   if (hitData.aggressor == 0x14) {
