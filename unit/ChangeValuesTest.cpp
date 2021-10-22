f48b9dab95835d5133e86d00a5859545f0c5cc9a
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-10-22 19:41:16 +0300

    test fix

--- unit/ChangeValuesTest.cpp
+++ unit/ChangeValuesTest.cpp
@@ -94,7 +94,7 @@ TEST_CASE("OnChangeValues call is cropping percentage values",
   IActionListener::RawMessageData msgData;
   msgData.userId = 0;
 
-  ac.SetPercentages(0.0f, 0.0f, 0.0f);
+  ac.SetPercentages(0.1f, 0.0f, 0.0f);
   auto past = std::chrono::steady_clock::now() - 1s;
   ac.SetLastAttributesPercentagesUpdate(past);
   p.GetActionListener().OnChangeValues(msgData, 1.0f, 1.0f, 1.0f);
@@ -113,7 +113,7 @@ TEST_CASE("OnChangeValues call is cropping percentage values",
   auto changeForm = ac.GetChangeForm();
 
   REQUIRE_THAT(changeForm.healthPercentage,
-               Catch::Matchers::WithinAbs(expectedHealth, 0.000001f));
+               Catch::Matchers::WithinAbs(expectedHealth + 0.1f, 0.000001f));
   REQUIRE_THAT(changeForm.magickaPercentage,
                Catch::Matchers::WithinAbs(expectedMagicka, 0.000001f));
   REQUIRE_THAT(changeForm.staminaPercentage,
