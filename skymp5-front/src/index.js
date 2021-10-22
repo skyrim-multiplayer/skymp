2af0ddd791b3fe9d37357f6cb52ef0d3e9ad814f
Vlad Denisov <vlad.a.denisov@gmail.com>
2021-10-22 21:26:09 +0300

    fix: rerender now should also recalculate size

--- skymp5-front/src/index.js
+++ skymp5-front/src/index.js
@@ -18,25 +18,70 @@ window.skyrimPlatform.widgets = new Widgets([
   {
     caption: "Test?",
     elements: [
-      { type: "button", tags: ['BUTTON_STYLE_FRAME'], text: "Sample text", click: () => { console.log("Yes") } },
-      { type: "button", tags: ['BUTTON_STYLE_FRAME_LEFT'], text: "Sample text 2", click: () => {
+      {
+        type: "button",
+        tags: ["BUTTON_STYLE_FRAME"],
+        text: "Sample text",
+        click: () => {
+          console.log("Yes");
+        },
+      },
+      {
+        type: "button",
+        tags: ["BUTTON_STYLE_FRAME_LEFT"],
+        text: "Sample text 2",
+        click: () => {
           window.skyrimPlatform.widgets.set([
             {
-              caption: "Test?",
+              type: "form",
+              id: 1,
+              caption: "login",
               elements: [
-                { type: "button", tags: ['BUTTON_STYLE_FRAME'], text: "Sample text", click: () => { console.log("Yes") } },
-                { type: "button", isDisabled: true, text: "Sample text 2", click: () => { console.log("Yes") } },
-                { type: "button", tags: ['BUTTON_STYLE_FRAME_RIGHT'], text: "Sample text 3", click: () => { console.log("No") } },
-                {type: "inputText"}
+                { type: "button", tags: ["BUTTON_STYLE_GITHUB"] },
+                {
+                  type: "button",
+                  tags: ["BUTTON_STYLE_PATREON", "ELEMENT_SAME_LINE"],
+                },
+                { type: "icon", text: "email", tags: ["ICON_STYLE_MAIL"] },
+                {
+                  type: "inputText",
+                  tags: ["ELEMENT_SAME_LINE"],
+                  placeholder: "dude33@gmail.com",
+                },
+                { type: "icon", text: "password", tags: ["ICON_STYLE_KEY"] },
+                {
+                  type: "inputPass",
+                  tags: ["ELEMENT_SAME_LINE"],
+                  placeholder: "password, you know",
+                },
+                { type: "checkBox", text: "remember me" },
+                {
+                  type: "button",
+                  text: "register now",
+                  tags: ["ELEMENT_SAME_LINE", "BUTTON_STYLE_FRAME"],
+                },
+                {
+                  type: "button",
+                  text: "travel to skyrim",
+                  tags: ["BUTTON_STYLE_FRAME"],
+                },
               ],
             },
-          ])
-        }},
-      { type: "button", tags: ['BUTTON_STYLE_FRAME_RIGHT'], text: "Sample text 3", click: () => { console.log("No") } },
-      {type: "inputText"}
+          ]);
+        },
+      },
+      {
+        type: "button",
+        tags: ["BUTTON_STYLE_FRAME_RIGHT"],
+        text: "Sample text 3",
+        click: () => {
+          console.log("No");
+        },
+      },
+      { type: "inputText" },
     ],
   },
-])
+]); 
 
 const widget = window.skyrimPlatform.widgets.get()[0]
 if (widget) {
