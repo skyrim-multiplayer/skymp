e1fbb8a1ad612c8acd6d858e500ae255cfd0c4e2
Leonid Pospelov <pospelovlm@yandex.ru>
2021-11-01 21:17:00 +0300

    Update TES5DamageFormula.cpp

--- skymp5-server/cpp/server_guest_lib/formulas/TES5DamageFormula.cpp
+++ skymp5-server/cpp/server_guest_lib/formulas/TES5DamageFormula.cpp
@@ -56,7 +56,7 @@ float TES5DamageFormulaImpl::GetBaseWeaponDamage() const
 
 float TES5DamageFormulaImpl::CalcWeaponRating() const
 {
-  // TODO(#xyz): take other components into account
+  // TODO(#457): take other components into account
   return GetBaseWeaponDamage();
 }
 
@@ -68,7 +68,7 @@ float TES5DamageFormulaImpl::CalcArmorRatingComponent(
         espm::ARMO::kType) {
     auto armorData =
       espm::GetData<espm::ARMO>(opponentEquipmentEntry.baseId, espmProvider);
-    // TODO(#xyz): take other components into account
+    // TODO(#458): take other components into account
     return static_cast<float>(armorData.baseRatingX100) / 100;
   }
   return 0;
@@ -76,9 +76,6 @@ float TES5DamageFormulaImpl::CalcArmorRatingComponent(
 
 float TES5DamageFormulaImpl::CalcOpponentArmorRating() const
 {
-  // TODO(#xyz): OpponentArmorRating is 1 if your character is successfully
-  // sneaking and has the Master Sneak perk (C) UESP Wiki
-  // ^ at least for Oblivion
   float combinedArmorRating = 0;
   for (const auto& entry : target.GetEquipment().inv.entries) {
     combinedArmorRating += CalcArmorRatingComponent(entry);
@@ -98,7 +95,7 @@ float TES5DamageFormulaImpl::CalculateDamage() const
     espm::GetData<espm::GMST>(espm::GMST::kFArmorRating, espmProvider).value;
   float minReceivedDamage = incomingDamage * (1 - 0.01 * maxArmorRating);
 
-  // TODO(#xyz): take other components into account
+  // TODO(#461): take other components into account
   float damage =
     std::max<float>(minReceivedDamage,
                     incomingDamage / (CalcOpponentArmorRating() * 0.12 + 1));
@@ -106,7 +103,7 @@ float TES5DamageFormulaImpl::CalculateDamage() const
     damage *= 2;
   }
   if (hitData.isHitBlocked) {
-    // TODO(#xyz): implement correct block formula
+    // TODO(#460): implement correct block formula
     damage *= 0.1;
   }
   return damage;
