2364fe70efea21dce6d228a477c833133f15f970
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-11-18 01:35:39 +0300

    .

---
+++ skymp5-server/cpp/server_guest_lib/sweet_pie/GameRules.cpp
@@ -0,0 +1,15 @@
+#include "GameRules.h"
+
+namespace sweetpie {
+struct Rules::RulesImpl
+{
+};
+}
+
+namespace sweetpie {
+namespace gamemode {
+struct Lobby::Impl
+{
+};
+}
+}
---
+++ skymp5-server/cpp/server_guest_lib/sweet_pie/GameRules.h
@@ -0,0 +1,30 @@
+#include <map>
+#include <memory>
+
+namespace sweetpie {
+class Rules
+{
+public:
+  virtual void AddPlayer(uint64_t playerID);
+  virtual void RemovePlayer(uint64_t playerID);
+  virtual void BeforeUpdate();
+  void Update();
+  virtual void BeforeMove();
+  void OnMove();
+
+private:
+  struct RulesImpl;
+  std::shared_ptr<RulesImpl> rulesImpl;
+};
+}
+
+namespace sweetpie {
+namespace gamemode {
+class TeamPlay : public Rules
+{
+private:
+  struct Impl;
+  std::shared_ptr<Impl> pImpl;
+};
+}
+}
