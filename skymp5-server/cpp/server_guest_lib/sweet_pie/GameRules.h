2ff1a658c2b48f3a9bcaf62ccfc200eb67925d6a
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-11-19 22:17:33 +0300

    Update GameRules.h

--- skymp5-server/cpp/server_guest_lib/sweet_pie/GameRules.h
+++ skymp5-server/cpp/server_guest_lib/sweet_pie/GameRules.h
@@ -1,10 +1,42 @@
 #include "NiPoint3.h"
+#include <chrono>
 #include <functional>
 #include <map>
 #include <memory>
 #include <set>
 
 namespace sweetpie {
+class PlaySpace
+{
+public:
+  void AddPlayer(Player player);
+  void RemovePlayer(Player player);
+
+  void AddTeam(Team team);
+  void RemoveTeam(Team team);
+
+  void AddPlayerToTeam(Player player, Team team);
+  void RemovePlayerFromTeam(Player player, Team team);
+
+private:
+  sweetpie::Data data;
+  uint16_t numberOfTeams = 0;
+  std::set<std::pair<uint16_t, Team&>> teams;
+  uint16_t maxNumberOfPlayers = 0;
+  std::set<std::pair<uint16_t, Player&>> players;
+};
+
+struct Team : public Data
+{
+  std::set<std::pair<uint16_t, Player&>> players;
+  uint16_t maxTeamSize = 0;
+};
+
+struct Player : public Data
+{
+  std::pair<uint16_t, Team&> team;
+};
+
 class Rules
 {
 public:
@@ -34,94 +66,28 @@ private:
 }
 
 namespace sweetpie {
-
 struct Data
 {
-  enum InvalidId : uint64_t
+  enum InvalidId : uint16_t
   {
-    InvalidId = (uint64_t)~0
+    InvalidID = (uint16_t)~0
   };
-  uint64_t id = InvalidId;
-  int64_t score = 0;
-  std::set<std::shared_ptr<sweetpie::Data>> subordinates;
-  sweetpie::geometry::SimpleFugure space;
+
+  uint16_t id = InvalidID;
+  float score = 0;
+  Space bounds;
 };
 }
 
 namespace sweetpie {
-namespace geometry {
-enum class Figure
-{
-  Null,
-  Circle,
-  Ellipse,
-  Rectangle
-};
-class SimpleFugure
+class Space
 {
 public:
-  SimpleFugure() = delete;
-  virtual bool IsInside(NiPoint3 pos) { return true; }
-  const NiPoint3& GetPosition() { return position; }
-
-protected:
-  NiPoint3 position;
-  float sqrNorm = 0;
-  float norm = 0;
-  Figure type = Figure::Null;
-};
-
-class Circle : public sweetpie::geometry::SimpleFugure
-{
-public:
-  Circle(NiPoint3 position, float radius);
-  const float& GetRadius() const;
-  bool IsInside(NiPoint3 pos) override;
+  Space(const NiPoint3& position,
+        std::function<bool(const NiPoint3&, const NiPoint3&)> f);
+  const std::function<bool(const NiPoint3&, const NiPoint3&)> IsInside;
 
 private:
-  float radius = 0;
+  std::shared_ptr<const NiPoint3&> position;
 };
-
-class Ellipse : public sweetpie::geometry::Circle
-{
-  friend sweetpie::geometry::Circle;
-
-public:
-  Ellipse(NiPoint3 position1, float radius1, NiPoint3 position2,
-          float radius2);
-  const float& GetSecondRadius() const;
-  const NiPoint3& GetSecondPosition() const;
-  bool IsInside(NiPoint3 pos) override;
-
-private:
-  float secondRadius = 0;
-  NiPoint3 secondPosition;
-};
-class Rectangle : public sweetpie::geometry::SimpleFugure
-{
-public:
-  Rectangle(NiPoint3 position, NiPoint3 size, float roatation);
-
-private:
-  NiPoint3 size;
-  float roatation;
-};
-class Polyhedron : public sweetpie::geometry::SimpleFugure
-{
-private:
-};
-
-// represents Line x*A + y*B + C = 0
-class Line
-{
-public:
-  float A = 0.f;
-  float B = 1.f;
-  float C = 0.f;
-
-private:
-  float normalization = 1.f;
-};
-
-}
 }
