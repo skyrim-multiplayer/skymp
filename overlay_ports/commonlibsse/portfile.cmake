9c7d386ef17b7191327cb0c92677e190230918c8
etherform <44289657+etherform@users.noreply.github.com>
2021-11-26 12:37:37 +0300

    update overlay ports

--- overlay_ports/commonlibsse/portfile.cmake
+++ overlay_ports/commonlibsse/portfile.cmake
@@ -1,19 +1,16 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ryan-rsm-McKenzie/CommonLibSSE
-    REF ec96be63ebaa82349225456005b773c75364f2b6
-    SHA512 f7c4f9760ce5a9671e4cdc28b230fe3901504af8335aeb0544482e41dc7e5eec6127281edbb58c082ad91ec9e26f6fdb63c7a8d931027433dc0fcca5a53b621b
+    REF 84738878bc33a445edb834b008cc882380455243
+    SHA512 8e048db41b77fc06d5d4ebbe24145db409729cef62f26c5abb30664b00b881b375085d36c604a68d18346de6b2187c81046aa606f2c858ac901d8c84420a773b
     HEAD_REF master
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-
-vcpkg_configure_cmake(
-    SOURCE_PATH ${SOURCE_PATH}
-)
+vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH "lib/cmake/commonlibsse")
 vcpkg_copy_pdbs()
 
-file(INSTALL "${SOURCE_PATH}/include" DESTINATION "${CURRENT_PACKAGES_DIR}")
+#file(INSTALL "${SOURCE_PATH}/include" DESTINATION "${CURRENT_PACKAGES_DIR}")
 file(INSTALL "${SOURCE_PATH}/LICENSE" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
