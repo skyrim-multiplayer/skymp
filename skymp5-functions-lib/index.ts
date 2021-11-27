2074bf6a748da578b78a8d5bad65f6a65517335b
Leonid Pospelov <pospelovlm@yandex.ru>
2021-11-27 22:39:14 +0300

    working on gamemode

--- skymp5-functions-lib/index.ts
+++ skymp5-functions-lib/index.ts
@@ -2,8 +2,12 @@ import { BrowserProperty } from './src/props/browserProperty';
 import { DialogProperty } from './src/props/dialogProperty';
 import { EvalProperty } from './src/props/evalProperty';
 import { Ctx } from './src/types/ctx';
-import { Mp } from './src/types/mp';
+import { LocationalData, Mp } from './src/types/mp';
 import { FunctionInfo } from './src/utils/functionInfo';
+
+DialogProperty.init();
+BrowserProperty.init();
+EvalProperty.init();
 
 declare const mp: Mp;
 
@@ -16,31 +20,69 @@ const neutralPortal = '42f70:SweetPie.esp';
 const redPortal = '42e96:SweetPie.esp';
 const bluePortal = '42fc1:SweetPie.esp';
 
-const whiterunSpawnPoint = {
-  pos: [22659, -8697, -3594],
-  cellOrWorldDesc: '1a26f:Skyrim.esm',
-  rot: [0, 0, 268],
+type SpawnPoint = LocationalData;
+
+interface SweetPieMap {
+  // TODO: custom blue/red spawnpoints
+  mainSpawnPoint: SpawnPoint;
+
+  safePlace: SpawnPoint;
+  safePlaceGoOutDoors: string[];
+  safePlaceComeInDoors: string[];
+
+  leaveMapDoors: string[];
+}
+
+class SweetPieRound {
+  constructor(public readonly map: SweetPieMap) {}
+
+  launchTimer = 60;
+  maxPlayers = 20;
+  players = new Set<number>();
+  state: 'running' | 'warmup' = 'warmup';
+}
+
+const whiterun: SweetPieMap = {
+  mainSpawnPoint: {
+    pos: [22659, -8697, -3594],
+    cellOrWorldDesc: '1a26f:Skyrim.esm',
+    rot: [0, 0, 268],
+  },
+  safePlace: {
+    pos: [-108, -809, 69.25],
+    cellOrWorldDesc: '1605e:Skyrim.esm',
+    rot: [0, 0, 176],
+  },
+  safePlaceGoOutDoors: ['16072:Skyrim.esm'],
+  safePlaceComeInDoors: ['1a6f4:Skyrim.esm'],
+  leaveMapDoors: ['1b1f3:Skyrim.esm'],
 };
 
-const whiterunTavernSpawnPoint = {
-  pos: [-108, -809, 69.25],
-  cellOrWorldDesc: '1605e:Skyrim.esm',
-  rot: [0, 0, 176],
+const rounds = new Array<SweetPieRound>();
+rounds.push(new SweetPieRound(whiterun));
+
+const findRoundForPlayer = (player: number): SweetPieRound | undefined => {
+  return rounds.find((x) => x.state !== 'running');
 };
 
-const hallSpawnPoint = {
-  pos: [18511, 10256, 610.6392],
-  cellOrWorldDesc: '42b5f:SweetPie.esp',
-  rot: [0, 0, 347],
+const getPlayerCurrentRound = (player: number): SweetPieRound | undefined => {
+  return rounds.find((x) => x.players.has(player));
 };
 
-const whiterunEscapeDoors = ['1b1f3:Skyrim.esm'];
-const whiterunTavernDoorExterior = ['1a6f4:Skyrim.esm'];
-const whiterunTavernDoorInterior = ['16072:Skyrim.esm'];
+const joinRound = (round: SweetPieRound, player: number) => {
+  mp.set(player, 'locationalData', round.map.safePlace);
+  round.players.add(player);
+};
 
-DialogProperty.init();
-BrowserProperty.init();
-EvalProperty.init();
+const leaveRound = (round: SweetPieRound | undefined, player: number) => {
+  const hallSpawnPoint = {
+    pos: [18511, 10256, 610.6392],
+    cellOrWorldDesc: '42b5f:SweetPie.esp',
+    rot: [0, 0, 347],
+  };
+  mp.set(player, 'locationalData', hallSpawnPoint);
+  round?.players.delete(player);
+};
 
 const enum DialogId {
   NoAction,
@@ -60,12 +102,12 @@ mp.onActivate = (target: number, caster: number) => {
     return false;
   }
 
+
   const lookupRes = mp.lookupEspmRecordById(target);
   const hasTeleport = lookupRes.record?.fields.findIndex((field) => field.type === 'XTEL') !== -1;
   if (hasTeleport) {
-    if (whiterunTavernDoorInterior.indexOf(targetDesc) !== -1) {
-      return true;
-    } else if (whiterunTavernDoorExterior.indexOf(targetDesc) !== -1) {
+    const round = getPlayerCurrentRound(caster);
+    if (round && round.map.safePlaceComeInDoors.indexOf(targetDesc) !== -1) {
       DialogProperty.showMessageBox(
         caster,
         DialogId.NoAction,
@@ -74,12 +116,14 @@ mp.onActivate = (target: number, caster: number) => {
         ['well']
       );
       return false;
-    } else if (whiterunEscapeDoors.indexOf(targetDesc) !== -1) {
+    } else if (!round || round.map.leaveMapDoors.indexOf(targetDesc) !== -1) {
       DialogProperty.showMessageBox(caster, DialogId.LeaveMatch, 'run out', 'do you want to run out of town?', [
         'yes',
         'no',
       ]);
       return false;
+    } else if (round.map.safePlaceGoOutDoors.indexOf(targetDesc) !== -1) {
+      return true;
     } else {
       EvalProperty.eval(caster, (ctx) => {
         ctx.sp.Debug.notification('Interiors are not available during combat');
@@ -103,66 +147,26 @@ DialogProperty.setDialogResponseHandler((response) => {
       break;
     case DialogId.JoinDeathMatch:
       if (response.buttonIndex === 0) {
-        mp.set(response.actorId, 'locationalData', whiterunTavernSpawnPoint);
+        const round = findRoundForPlayer(response.actorId);
+        if (round) {
+          joinRound(round, response.actorId);
+        } else {
+          DialogProperty.showMessageBox(
+            response.actorId,
+            DialogId.NoAction,
+            'no free maps',
+            'sorry, there are no free slots for new players',
+            ['well']
+          );
+        }
       }
       break;
     case DialogId.LeaveMatch:
       if (response.buttonIndex === 0) {
-        mp.set(response.actorId, 'locationalData', hallSpawnPoint);
+        const round = findRoundForPlayer(response.actorId);
+        leaveRound(round, response.actorId);
       }
       break;
   }
   return true;
 });
-
-/*type FrontConstructorValue = {} | null;
-type FrontConstructorState = { frontConstructorPrev?: FrontConstructorValue };
-
-const updateOwner = (ctx: Ctx<FrontConstructorState, FrontConstructorValue>) => {
-  if (!ctx.value) return;
-
-  // skymp5-client doesn't update references until new value is received
-  // so just compare references
-  if (ctx.state.frontConstructorPrev === ctx.value) return;
-  ctx.state.frontConstructorPrev = ctx.value;
-
-  const stringified = JSON.stringify(ctx.value);
-  let src = "";
-  src += 'window.tmp = ' + stringified + ';';
-  src += 'window.skyrimPlatform.widgets.set(window.tmp);';
-  src += "delete window.tmp;";
-  ctx.sp.browser.executeJavaScript(src);
-};
-
-mp.makeProperty('frontConstructor', {
-  isVisibleByOwner: true,
-  isVisibleByNeighbors: false,
-  updateOwner: new FunctionInfo(updateOwner).getText(),
-  updateNeighbor: '',
-});
-
-const showDialog = (actorId: number, caption: string, text: string, buttons: string[]): void => {
-  const widget = {
-    type: 'form',
-    id: 1,
-    caption: caption,
-    elements: [{ type: 'text', text: text, tags: [''] }],
-  };
-  for (const buttonText of buttons) {
-    widget.elements.push({ type: 'button', text: buttonText, tags: ['BUTTON_STYLE_FRAME'] });
-  }
-  mp.set(actorId, 'frontConstructor', {});
-};
-
-const activateQuitGamePortal = (caster: number) => {
-  showDialog(caster, "Quit game", "Do you want to quit SweetPie?", ["Yes", "No"])
-};
-
-mp.onActivate = (target: number, caster: number) => {
-  const targetDesc = mp.getDescFromId(target);
-  if (targetDesc === quitGamePortal) {
-    activateQuitGamePortal(caster);
-    return false;
-  }
-  return true;
-};*/
