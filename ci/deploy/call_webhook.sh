6ffc022ff5d71d683bd6417e6da17803104647ce
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-12-05 04:32:34 +0300

    out of interest

--- ci/deploy/call_webhook.sh
+++ ci/deploy/call_webhook.sh
@@ -13,5 +13,5 @@ if [[ "$1" = "" ]]; then
   exit 1
 fi
 
-curl "$DEPLOY_STATUS_WEBHOOK" -H 'content-type: application/json' \
+curl -v "$DEPLOY_STATUS_WEBHOOK" -H 'content-type: application/json' \
     --data "`echo "$1" | jq --raw-input '{content: .}'`"
