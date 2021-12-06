2ab454d875c9abc356fd3d0dd81fd1c1a6f98929
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-12-06 04:46:53 +0300

    code cleanup

--- ci/deploy/gather-indev/cli.ts
+++ ci/deploy/gather-indev/cli.ts
@@ -1,37 +1,17 @@
 import { Octokit } from '@octokit/rest';
 import * as pull_manager from './pull_manager';
-// import * as pull_merger from './pull_merger';
 
 const octokit = new Octokit();
 
 
 (async function() {
 
-// const { data: pr } = await octokit.rest.pulls.get({
-//   owner: 'skyrim-multiplayer',
-//   repo: 'skymp',
-//   pull_number: 123,
-// });
-
-// // console.log(pullRequest);
-// console.log(pr.number, pr.labels);
-
-// const { data: prList } = await octokit.pulls.list({
-//   owner: 'skyrim-multiplayer',
-//   repo: 'skymp',
-//   state: 'open',
-// });
-
-// for (const pr of prList) {
-//   pr.labels
-// }
-
-// await pull_merger.mergePull({headLabel: 'kek', number: 123,});
-
 const pulls = await pull_manager.getPullsWithLabel(octokit);
 
+// numbers list for merge_pulls.sh
 console.log(pulls.map((pull) => pull.number).join(' '));
 
+// description for Discord message
 console.log('Included PRs:');
 for (const pull of pulls) {
   console.log(`<[${pull.title}](https://github.com/skyrim-multiplayer/skymp/pull/${pull.number})> from ${pull.headLabel} by **${pull.authorLogin}**`);
