1e2bdc8dc31b57be37ce71675c4e383b161219be
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2021-12-11 12:35:52 +0300

    Add effect reading validation

--- skymp5-server/cpp/lib_espm/espm.cpp
+++ skymp5-server/cpp/lib_espm/espm.cpp
@@ -1075,20 +1075,30 @@ espm::Effects::Data espm::Effects::GetData(
 
   Data result;
   uint32_t effectIndex = 0;
+  bool orderFlag = true;
+  bool isValid = true;
 
   espm::RecordHeaderAccess::IterateFields(
     parent,
     [&](const char* type, uint32_t size, const char* data) {
       if (!memcmp(type, "EFID", 4)) {
+        isValid = orderFlag == true;
         result.effects.emplace_back();
         result.effects[effectIndex].effectId =
           *reinterpret_cast<const uint32_t*>(data);
+        orderFlag = false;
       } else if (!memcmp(type, "EFIT", 4)) {
+        isValid = orderFlag == false;
         Effect& eff = result.effects[effectIndex];
         eff.magnitude = *reinterpret_cast<const float*>(data);
         eff.areaOfEffect = *reinterpret_cast<const uint32_t*>(data + 4);
         eff.duration = *reinterpret_cast<const uint32_t*>(data + 8);
         effectIndex++;
+        orderFlag = true;
+      }
+      if (!isValid) {
+        auto name = parent->GetEditorId(compressedFieldsCache);
+        throw std::runtime_error("Bad effect array. " + *name);
       }
     },
     compressedFieldsCache);
