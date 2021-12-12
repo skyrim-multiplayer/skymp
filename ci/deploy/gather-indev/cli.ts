608decdddcd3404120c66da53173b95691697c5a
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2021-12-12 22:48:01 +0300

    indent function

--- ci/deploy/gather-indev/cli.ts
+++ ci/deploy/gather-indev/cli.ts
@@ -3,29 +3,26 @@ import * as pull_manager from './pull_manager';
 
 const octokit = new Octokit();
 
+(async function () {
+  const pulls = await pull_manager.getPullsWithLabel(octokit);
 
-(async function() {
+  // numbers list for merge_pulls.sh
+  console.log(pulls.map((pull) => pull.number).join(' '));
 
-const pulls = await pull_manager.getPullsWithLabel(octokit);
+  // description for Discord message
+  console.log('Included PRs:');
+  let hasPulls = false;
+  for (const pull of pulls) {
+    console.log(
+      `[${pull.title}](<https://github.com/skyrim-multiplayer/skymp/pull/${pull.number}>)`
+      + `from ${pull.headLabel} by **${pull.authorLogin}**`
+    );
+    hasPulls = true;
+  }
 
-// numbers list for merge_pulls.sh
-console.log(pulls.map((pull) => pull.number).join(' '));
-
-// description for Discord message
-console.log('Included PRs:');
-let hasPulls = false;
-for (const pull of pulls) {
-  console.log(
-    `[${pull.title}](<https://github.com/skyrim-multiplayer/skymp/pull/${pull.number}>)`
-    + `from ${pull.headLabel} by **${pull.authorLogin}**`
-  );
-  hasPulls = true;
-}
-
-if (!hasPulls) {
-  console.log('None. Will build clean base branch.')
-}
-
+  if (!hasPulls) {
+    console.log('None. Will build clean base branch.')
+  }
 })().catch((err) => {
   console.error(err)
   process.exit(1);
