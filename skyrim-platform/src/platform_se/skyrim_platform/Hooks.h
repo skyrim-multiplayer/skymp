c9a21d98ca3f2a2c01e13b9b1187d7cb816e0bb6
etherform <44289657+etherform@users.noreply.github.com>
2021-12-15 15:43:14 +0300

    small fix

--- skyrim-platform/src/platform_se/skyrim_platform/Hooks.h
+++
@@ -1,7 +0,0 @@
-namespace Hooks {
-  void Hooks::InstallDeviceConnectHook()
-{
-	REL::Relocation<std::uintptr_t> hook{ Offset::InputManager::ProcessEvent, 0x7E };
-	REL::safe_fill(hook.address(), REL::NOP, 0x6);
-}
-}
