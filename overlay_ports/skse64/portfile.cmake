1e5ba40cdc4fbc748ab1773909f34ac867f80c67
etherform <44289657+etherform@users.noreply.github.com>
2021-12-22 20:48:12 +0300

    revert skse changes

--- overlay_ports/skse64/portfile.cmake
+++ overlay_ports/skse64/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_download_distfile(ARCHIVE
-    URLS "https://skse.silverlock.org/beta/skse64_2_01_03.7z"
-    FILENAME "skse64_2_01_03.7z"
-    SHA512 844947f2990c4d10d2b035f7a3d80dcc462a8dac61fa84e3e07262d510d799bf0910e424b61cbb4c9e34033856203a9200562333b0fc4a1f9dd5ca3136330e59
+    URLS "https://skse.silverlock.org/beta/skse64_2_00_19.7z"
+    FILENAME "skse64_2_00_19.7z"
+    SHA512 fb0c72605ea59030647979e4f69029ddfc0e7e63b06390d6595ace9da7e43383306feceada19fdf575137baeb5cc930c8c602df6b70a154ca2cd3020c20807af
 )
 
 vcpkg_extract_source_archive_ex(
@@ -19,16 +19,16 @@ vcpkg_build_cmake()
 vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
-file(GLOB skse64_include "${SOURCE_PATH}/skse64_2_01_03/src/skse64/skse64/*.h")
+file(GLOB skse64_include "${SOURCE_PATH}/skse64_2_00_19/src/skse64/skse64/*.h")
 file(INSTALL ${skse64_include} DESTINATION "${CURRENT_PACKAGES_DIR}/include/skse64")
 
-file(GLOB skse64_common_include "${SOURCE_PATH}/skse64_2_01_03/src/skse64/skse64_common/*.h")
+file(GLOB skse64_common_include "${SOURCE_PATH}/skse64_2_00_19/src/skse64/skse64_common/*.h")
 file(INSTALL ${skse64_common_include} DESTINATION "${CURRENT_PACKAGES_DIR}/include/skse64_common")
 
-file(GLOB common_include "${SOURCE_PATH}/skse64_2_01_03/src/common/*.h")
+file(GLOB common_include "${SOURCE_PATH}/skse64_2_00_19/src/common/*.h")
 file(INSTALL ${common_include} DESTINATION "${CURRENT_PACKAGES_DIR}/include/common")
 
-file(GLOB xbyak_include "${SOURCE_PATH}/skse64_2_01_03/src/skse64/xbyak/*.h")
+file(GLOB xbyak_include "${SOURCE_PATH}/skse64_2_00_19/src/skse64/xbyak/*.h")
 file(INSTALL ${xbyak_include} DESTINATION "${CURRENT_PACKAGES_DIR}/include/xbyak")
 
-file(INSTALL ${SOURCE_PATH}/skse64_2_01_03/src/skse64/skse64_license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/skse64_2_00_19/src/skse64/skse64_license.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
