3503eac23244dbd85b59a4a4768d2f9ec97c792d
Leonid Pospelov <pospelovlm@yandex.ru>
2021-12-23 22:07:02 +0300

    feat(skymp5-server): implement Actor.GetSitState (#667)

--- skymp5-server/cpp/server_guest_lib/PapyrusActor.h
+++ skymp5-server/cpp/server_guest_lib/PapyrusActor.h
@@ -11,6 +11,7 @@ public:
   DEFINE_METHOD_SPSNIPPET(DrawWeapon);
   DEFINE_METHOD_SPSNIPPET(UnequipAll);
   DEFINE_METHOD_SPSNIPPET(PlayIdle);
+  DEFINE_METHOD_SPSNIPPET(GetSitState);
 
   VarValue IsWeaponDrawn(VarValue self,
                          const std::vector<VarValue>& arguments);
@@ -24,6 +25,7 @@ public:
     AddMethod(vm, "DrawWeapon", &PapyrusActor::DrawWeapon);
     AddMethod(vm, "UnequipAll", &PapyrusActor::UnequipAll);
     AddMethod(vm, "PlayIdle", &PapyrusActor::PlayIdle);
+    AddMethod(vm, "GetSitState", &PapyrusActor::GetSitState);
   }
 
   std::shared_ptr<IPapyrusCompatibilityPolicy> compatibilityPolicy;
