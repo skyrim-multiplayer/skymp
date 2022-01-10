6bbd820c420697130c539f463f6419c410c89551
Dinakrab <64138121+Dinakrab@users.noreply.github.com>
2022-01-10 20:44:42 +0300

    Update HealthRestorationTest.cpp

--- unit/HealthRestorationTest.cpp
+++ unit/HealthRestorationTest.cpp
@@ -13,22 +13,10 @@ TEST_CASE("Potions restore healt", "[Restoration]")
   auto& ac = p.worldState.GetFormAt<MpActor>(0xff000000);
   IActionListener::RawMessageData rawMsgData;
   rawMsgData.userId = 0;
-  ac.SetEquipment(R"(
-    {
-      "inv": {
-        "entries": [
-          {
-            "baseId": 256739,
-            "count": 1,
-            "worn": true
-          }
-        ]
-      }
-    }
-  )");
-  // 256739 = 0x3EAE3 - restores 100 hp
+  ac.AddItem(0x3EAE3, 1);
+  // 0x3EAE3 restores 100 hp
   ac.SetPercentages(0.1f, 0.f, 0.f);
-  p.GetActionListener().OnEquip(rawMsgData, 256739);
+  p.GetActionListener().OnEquip(rawMsgData, 0x3EAE3);
 
   REQUIRE(ac.GetChangeForm().healthPercentage == 1.0f);
 
