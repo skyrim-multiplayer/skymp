e2d73dd66b4a4dde1bf9b067932ea07fc1759493
etherform <44289657+etherform@users.noreply.github.com>
2022-02-01 17:10:37 +0300

    add story event handler (wip)

---
+++ skyrim-platform/src/platform_se/skyrim_platform/EventHandlerStory.cpp
@@ -0,0 +1,1 @@
+#include "EventHandlerStory.h"
---
+++ skyrim-platform/src/platform_se/skyrim_platform/EventHandlerStory.h
@@ -0,0 +1,110 @@
+#pragma once
+
+using EventResult = RE::BSEventNotifyControl;
+
+class EventHandlerStory final
+  : public RE::BSTEventSink<RE::ActorKill::Event>
+  , public RE::BSTEventSink<RE::BooksRead::Event>
+  , public RE::BSTEventSink<RE::CriticalHit::Event>
+  , public RE::BSTEventSink<RE::DisarmedEvent::Event>
+  , public RE::BSTEventSink<RE::DragonSoulsGained::Event>
+  , public RE::BSTEventSink<RE::TESHarvestedEvent::ItemHarvested>
+  , public RE::BSTEventSink<RE::LevelIncrease::Event>
+  , public RE::BSTEventSink<RE::LocationDiscovery::Event>
+  , public RE::BSTEventSink<RE::ShoutAttack::Event>
+  , public RE::BSTEventSink<RE::SkillIncrease::Event>
+  , public RE::BSTEventSink<RE::SoulsTrapped::Event>
+  , public RE::BSTEventSink<RE::SpellsLearned::Event>
+{
+public:
+  [[nodiscard]] static EventHandlerScript* GetSingleton()
+  {
+    static EventHandlerScript singleton;
+    return &singleton;
+  }
+
+  static void RegisterSinks()
+  {
+    add_sink<RE::ActorKill>();
+    add_sink<RE::BooksRead>();
+    add_sink<RE::CriticalHit>();
+    add_sink<RE::DisarmedEvent>();
+    add_sink<RE::DragonSoulsGained>();
+
+    if (const auto event = RE::TESHarvestedEvent::GetEventSource(); event) {
+      event->AddEventSink<RE::TESHarvestedEvent::ItemHarvested>(
+        GetSingleton());
+      logger::info("Registered {} handler"sv,
+                   typeid(RE::TESHarvestedEvent::ItemHarvested).name());
+    }
+
+    add_sink<RE::LevelIncrease>();
+    add_sink<RE::LocationDiscovery>();
+    add_sink<RE::ShoutAttack>();
+    add_sink<RE::SkillIncrease>();
+    add_sink<RE::SoulsTrapped>();
+    add_sink<RE::SpellsLearned>();
+  }
+
+  EventResult ProcessEvent(const RE::ActorKill::Event* a_event,
+                           RE::BSTEventSource<RE::ActorKill::Event>*) override;
+
+  EventResult ProcessEvent(const RE::BooksRead::Event* a_event,
+                           RE::BSTEventSource<RE::BooksRead::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::CriticalHit::Event* a_event,
+    RE::BSTEventSource<RE::CriticalHit::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::DisarmedEvent::Event* a_event,
+    RE::BSTEventSource<RE::DisarmedEvent::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::DragonSoulsGained::Event* a_event,
+    RE::BSTEventSource<RE::DragonSoulsGained::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::TESHarvestedEvent::ItemHarvested* a_event,
+    RE::BSTEventSource<RE::TESHarvestedEvent::ItemHarvested>*) override;
+
+  EventResult ProcessEvent(
+    const RE::LevelIncrease::Event* a_event,
+    RE::BSTEventSource<RE::LevelIncrease::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::LocationDiscovery::Event* a_event,
+    RE::BSTEventSource<RE::LocationDiscovery::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::ShoutAttack::Event* a_event,
+    RE::BSTEventSource<RE::ShoutAttack::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::SkillIncrease::Event* a_event,
+    RE::BSTEventSource<RE::SkillIncrease::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::SoulsTrapped::Event* a_event,
+    RE::BSTEventSource<RE::SoulsTrapped::Event>*) override;
+
+  EventResult ProcessEvent(
+    const RE::SpellsLearned::Event* a_event,
+    RE::BSTEventSource<RE::SpellsLearned::Event>*) override;
+
+private:
+  EventHandlerStory() = default;
+  EventHandlerStory(const EventHandlerStory&) = delete;
+  EventHandlerStory(EventHandlerStory&&) = delete;
+
+  ~EventHandlerStory() = default;
+
+  template <class T>
+  static void add_sink()
+  {
+    if (const auto holder = T::GetEventSource()) {
+      holder->AddEventSink<T::Event>(GetSingleton());
+      logger::info("Registered {} handler"sv, typeid(T::Event).name());
+    }
+  }
+};
