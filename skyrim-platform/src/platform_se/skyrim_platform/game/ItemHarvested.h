c577048e44f62e47762489439c9382ec14be1026
etherform <44289657+etherform@users.noreply.github.com>
2022-02-02 10:53:59 +0300

    Update ItemHarvested.h

--- skyrim-platform/src/platform_se/skyrim_platform/game/ItemHarvested.h
+++ skyrim-platform/src/platform_se/skyrim_platform/game/ItemHarvested.h
@@ -1,25 +1,25 @@
 #pragma once
 
-#include "RE/B/BSTEvent.h"
-
 namespace RE {
-struct TESHarvestedEvent
+
+struct ItemHarvested
 {
 public:
-  struct ItemHarvested
+  struct Event
   {
   public:
     // members
     RE::TESBoundObject* produceItem; // 00
     RE::Actor* harvester;            // 08
   };
-  static_assert(sizeof(ItemHarvested) == 0x10);
+  static_assert(sizeof(Event) == 0x10);
 
-  static RE::BSTEventSource<TESHarvestedEvent::ItemHarvested>* GetEventSource()
+  static RE::BSTEventSource<ItemHarvested::Event>* GetEventSource()
   {
-    using func_t = decltype(&TESHarvestedEvent::GetEventSource);
+    using func_t = decltype(&ItemHarvested::GetEventSource);
     REL::Relocation<func_t> func{ REL::ID(14875) };
     return func();
   }
 };
+
 }
