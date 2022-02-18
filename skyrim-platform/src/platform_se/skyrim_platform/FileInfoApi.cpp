108a6a3fe68190e3ab9c8d34e29556a14893f423
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2022-02-19 01:11:31 +0300

    validation from that pr

--- skyrim-platform/src/platform_se/skyrim_platform/FileInfoApi.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/FileInfoApi.cpp
@@ -1,20 +1,18 @@
 #include "FileInfoApi.h"
 
 #include "FileInfo.h"
+#include "InvalidArgumentException.h"
 
 namespace FileInfoApi {
 
 JsValue FileInfo(const JsFunctionArguments& args)
 {
-  auto path = static_cast<std::string>(args[1]);
-  for (char c : path) {
-    if (!(('0' <= c && c <= '9') || ('A' <= c && c <= 'Z') ||
-          ('a' <= c && c <= 'z') || c == '.' || c == '-' || c == '-')) {
-      throw std::runtime_error("FileInfo: forbidden characters in path");
-    }
+  auto filename = static_cast<std::string>(args[1]);
+  if (!ValidateFilename(filename, /*allowDots*/ false)) {
+    throw InvalidArgumentException("filename", filename);
   }
 
-  const auto cppResult = ::FileInfo("Data/" + path);
+  const auto cppResult = ::FileInfo("Data/" + filename);
   auto jsResult = JsValue::Object();
   jsResult.SetProperty("crc32", static_cast<int>(cppResult.crc32));
   jsResult.SetProperty("size", static_cast<int>(cppResult.size));
