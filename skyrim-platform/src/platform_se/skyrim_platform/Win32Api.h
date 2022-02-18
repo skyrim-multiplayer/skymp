fb78f0ed6042efacae2fffadd1efe624903ff2b8
Roman Nikonov <11034375+nic11@users.noreply.github.com>
2022-02-19 01:41:27 +0300

    remove from Win32Api

--- skyrim-platform/src/platform_se/skyrim_platform/Win32Api.h
+++ skyrim-platform/src/platform_se/skyrim_platform/Win32Api.h
@@ -7,14 +7,11 @@ JsValue LoadUrl(const JsFunctionArguments& args);
 
 JsValue ExitProcess(const JsFunctionArguments& args);
 
-JsValue FileInfo(const JsFunctionArguments& args);
-
 inline void Register(JsValue& exports)
 {
   auto win32 = JsValue::Object();
   win32.SetProperty("loadUrl", JsValue::Function(LoadUrl));
   win32.SetProperty("exitProcess", JsValue::Function(ExitProcess));
-  win32.SetProperty("fileInfo", JsValue::Function(FileInfo));
   exports.SetProperty("win32", win32);
 }
 
