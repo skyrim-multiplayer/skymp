f84b0f4e4d2d62f3a84f9b0c6dc3caa9bd53b402
d0gge <ejs.defolt@gmail.com>
2022-03-04 10:37:55 +0500

    formatting fix

--- skymp5-server/cpp/server_guest_lib/formulas/TES5DamageFormula.cpp
+++ skymp5-server/cpp/server_guest_lib/formulas/TES5DamageFormula.cpp
@@ -137,8 +137,7 @@ float TES5DamageFormulaImpl::CalculateDamage() const
     // TODO(#460): implement correct block formula
     damage *= 0.1;
   }
-  if (hitData.isSneakAttack)
-  {
+  if (hitData.isSneakAttack) {
     damage *= 2;
   }
   return damage;
