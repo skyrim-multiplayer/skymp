78bc788a533cbb910362d537388147f12ef21a43
d0gge <ejs.defolt@gmail.com>
2022-04-27 21:47:39 +0500

    taking count into account on dropping

--- unit/DropItemTest.cpp
+++ unit/DropItemTest.cpp
@@ -11,7 +11,8 @@ TEST_CASE("Dropping an item", "[DropItemTest]")
 {
   auto& partOne = GetPartOne();
   // an iron dagger
-  constexpr uint32_t baseId = 0x0001397E;
+  constexpr uint32_t ironDagger = 0x0001397E;
+  constexpr uint32_t ironArrow = 0x0001397D;
   DoConnect(partOne, 0);
 
   partOne.CreateActor(0xff000000, { 1, 2, 3 }, 0, 0x3c);
@@ -21,21 +22,29 @@ TEST_CASE("Dropping an item", "[DropItemTest]")
 
   ac.RemoveAllItems();
   REQUIRE(ac.GetInventory().GetTotalItemCount() == 0);
-  ac.AddItem(baseId, 1);
+  ac.AddItem(ironDagger, 1);
   REQUIRE(ac.GetInventory().GetTotalItemCount() == 1);
   partOne.Messages().clear();
   REQUIRE(partOne.Messages().size() == 0);
   DoMessage(partOne, 0,
             nlohmann::json{ { "t", MsgType::DropItem },
-                            { "baseId", baseId },
+                            { "ironDagger", ironDagger },
                             { "count", 1 } });
   // 1 message from here and another 1 is comming from actionListener
   REQUIRE(partOne.Messages().size() == 2);
-  REQUIRE(ac.GetInventory().GetItemCount(baseId) == 0);
+  REQUIRE(ac.GetInventory().GetItemCount(ironDagger) == 0);
   MpObjectReference& refr =
     partOne.worldState.GetFormAt<MpObjectReference>(0xff000001);
-  REQUIRE(refr.GetBaseId() == baseId);
+  REQUIRE(refr.GetBaseId() == ironDagger);
   REQUIRE(refr.GetPos().x == 1.f);
   REQUIRE(refr.GetPos().y == 2.f);
   REQUIRE(refr.GetPos().z == 3.f);
+  partOne.Messages().clear();
+  REQUIRE(partOne.Messages().size() == 0);
+  DoMessage(partOne, 0,
+            nlohmann::json{ { "t", MsgType::DropItem },
+                            { "ironDagger", ironArrow },
+                            { "count", 5 } });
+  REQUIRE(partOne.Messages().size() == 2);
+  REQUIRE(ac.GetInventory().GetItemCount(ironArrow) == 10);
 }
