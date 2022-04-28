4e9cc728dcea10073027fb2856c4876da82d5903
Korisss <vaginpawel2005@yandex.ru>
2022-04-28 21:07:42 +0300

    feat: add localization provider

---
+++ skymp5-server/cpp/localization_provider/LocalizationProvider.h
@@ -0,0 +1,36 @@
+#include <filesystem>
+#include <map>
+#include <string>
+#include <vector>
+
+class LocalizationProvider
+{
+  std::map<uint32_t, std::string> Parse(
+    const std::filesystem::directory_entry& file);
+
+  std::map<uint32_t, std::string> ParseILDLStrings(
+    std::vector<char> buffer, std::vector<DirectoryEntry> entries);
+
+  std::map<uint32_t, std::string> ParseStrings(
+    std::vector<char> buffer, std::vector<DirectoryEntry> entries);
+
+  std::vector<DirectoryEntry> ParseDirectoryEntries(std::vector<char> buffer);
+
+public:
+  LocalizationProvider(std::string language);
+  std::map<std::string, std::map<uint32_t, std::string>>
+    localization; // localization[filename][stringId]
+  std::string Get(std::string file, uint32_t stringId);
+};
+
+class DirectoryEntry
+{
+public:
+  uint32_t stringId;
+  uint32_t offset;
+  uint32_t length;
+  std::string str;
+
+  DirectoryEntry();
+  DirectoryEntry(uint32_t stringId, uint32_t stringOffset);
+};
