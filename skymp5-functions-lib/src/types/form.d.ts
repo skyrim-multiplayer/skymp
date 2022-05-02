6e2dcde378e4fd7019d368917dd67cf6f6021a87
Korisss <vaginpawel2005@yandex.ru>
2022-05-02 16:40:44 +0300

    add typescript definitions

---
+++ skymp5-functions-lib/src/types/form.d.ts
@@ -0,0 +1,15 @@
+export class Form {
+  constructor(formId: number) {}
+
+  GetName(): string;
+  GetGoldValue(): number;
+  GetWeight(): number;
+  GetKeywords(): number[];
+  GetNthKeyword(): number;
+  GetNumKeywords(): number;
+  HasKeyword(keyword: number): boolean;
+  GetType(): string;
+  GetEditorId(): string;
+  GetSignature(): string;
+  EqualSignature(signature: string): boolean;
+}
