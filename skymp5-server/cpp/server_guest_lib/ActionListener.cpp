aaccaf9b5a94d8fdf1d9f660eeb96b52267ad1c0
d0gge <ejs.defolt@gmail.com>
2022-05-28 21:46:14 +0500

    minor fix

--- skymp5-server/cpp/server_guest_lib/ActionListener.cpp
+++ skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -654,7 +654,7 @@ void ActionListener::OnHit(const RawMessageData& rawMsgData_,
   }
 
   std::unordered_set<uint32_t> greenZones = { 0x0760AADA };
-  uint32_t aggressorCell = aggressor->GetCellOrWorld();
+  uint32_t aggressorCell = aggressor->GetCellOrWorld().ToFormId;
   auto it = greenZones.find(aggressorCell);
   if (it != greenZones.end()) {
     return;
