1dd571f0efc63a9d3bb104c9a21bdc3714cfb18b
d0gge <ejs.defolt@gmail.com>
2022-06-11 13:47:29 +0500

    changed as required

--- skymp5-server/cpp/server_guest_lib/PapyrusActor.cpp
+++ skymp5-server/cpp/server_guest_lib/PapyrusActor.cpp
@@ -1,13 +1,12 @@
 #include "PapyrusActor.h"
 
-#include "EspmGameObject.h"
 #include "Inventory.h"
 #include "MpActor.h"
-#include "MpForm.h"
 #include "MpFormGameObject.h"
 #include "MsgType.h"
 
 #include "CIString.h"
+#include "SpSnippetFunctionGen.h"
 
 namespace {
 espm::ActorValue ConvertToAV(CIString actorValueName)
@@ -85,75 +84,13 @@ VarValue PapyrusActor::EquipItem(VarValue self,
                                  const std::vector<VarValue>& arguments)
 {
   if (auto actor = GetFormPtr<MpActor>(self)) {
-    auto& form = GetRecordPtr(arguments[0]);
-    if (!form.rec) {
-      throw std::runtime_error(
-        fmt::format("Unable to find a record of a form"));
+    if (arguments.size() < 1) {
+      throw std::runtime_error("EquipItem requires at least one argument");
     }
-    if (!actor->GetParent()->HasEspm()) {
-      throw std::runtime_error(fmt::format(
-        "No espm attached to the actor with id {:x}", actor->GetBaseId()));
-    }
-    auto& loader = actor->GetParent()->GetEspm();
-    auto& cache = actor->GetParent()->GetEspmCache();
-
-    const Equipment eq = actor->GetEquipment();
-
-    actor->OnEquip(form.rec->GetId());
-    Equipment newEq;
-    newEq.numChanges = eq.numChanges + 1;
-    for (auto& entry : eq.inv.entries) {
-      bool isEquipped = entry.extra.worn != Inventory::Worn::None;
-      bool isWeap = espm::GetRecordType(entry.baseId, actor->GetParent()) ==
-        espm::WEAP::kType;
-      if (entry.baseId == form.rec->GetId()) {
-        if (isWeap) {
-          continue;
-        }
-        Inventory::Entry modifiedEntry = entry;
-        modifiedEntry.extra.worn = Inventory::Worn::Right;
-        newEq.inv.AddItems({ modifiedEntry });
-        continue;
-      }
-      if (isEquipped && isWeap) {
-        continue;
-      }
-      newEq.inv.AddItems({ entry });
-    }
-    const Inventory inv = actor->GetInventory();
-    Inventory::Entry bestEntry;
-    int16_t bestDamage = -1;
-    for (auto& entry : inv.entries) {
-      if (entry.baseId) {
-        auto lookupRes = loader.GetBrowser().LookupById(entry.baseId);
-        if (auto weap = espm::Convert<espm::WEAP>(lookupRes.rec)) {
-          if (!bestEntry.count ||
-              weap->GetData(cache).weapData->damage > bestDamage) {
-            bestEntry = entry;
-            bestDamage = weap->GetData(cache).weapData->damage;
-          }
-        }
-      }
-    }
-    if (bestEntry.count > 0) {
-      bestEntry.extra.worn = Inventory::Worn::Right;
-      newEq.inv.AddItems({ bestEntry });
-    }
-    actor->SetEquipment(newEq.ToJson().dump());
-    for (auto listener : actor->GetListeners()) {
-      auto actor = dynamic_cast<MpActor*>(listener);
-      if (!actor) {
-        continue;
-      }
-      std::string s;
-      s += Networking::MinPacketId;
-      s += nlohmann::json{
-        { "t", MsgType::UpdateEquipment },
-        { "idx", actor->GetIdx() },
-        { "data", newEq.ToJson() }
-      }.dump();
-      actor->SendToUser(s.data(), s.size(), true);
-    }
+    SpSnippet(GetName(), "EquipItem",
+              SpSnippetFunctionGen::SerializeArguments(arguments).data(),
+              actor->GetFormId())
+      .Execute(actor);
   }
   return VarValue::None();
 }
