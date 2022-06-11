58cab51a3ee50601e137e9022cbfe8f4545a2723
Roman Nikonov <code@nic11.xyz>
2022-06-12 01:45:33 +0200

    no to escapeJs, separate FunctionInfo

---
+++ skymp5-client/src/lib/functionInfo.ts
@@ -0,0 +1,20 @@
+export class FunctionInfo<F extends { toString: () => string }> {
+  public constructor(private f: F) {}
+
+  get text(): string {
+    return `try{${this.getTextWithoutErrorHandling()}}catch(e){` +
+        `ctx.sp.printConsole('[CTX ERROR]', e, '\\n', ${this.f})}`
+  }
+
+  getText(args?: Record<string, unknown>): string {
+    if (!args) {
+      return this.text;
+    }
+    return `(function(){const {${Object.keys(args).join(',')}} = ${JSON.stringify(args)};${this.text}})()`;
+  }
+
+  private getTextWithoutErrorHandling(): string {
+    const funcString = this.f.toString().substring(0, this.f.toString().length - 1);
+    return funcString.replace(new RegExp('^.+?{', 'm'), '').trim();
+  }
+}
