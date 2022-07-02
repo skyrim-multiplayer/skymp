f179eb067513ee645927d7cfa5d7e873a524e359
Roman Nikonov <code@nic11.xyz>
2022-07-02 17:33:15 +0200

    console fix & refactor?

--- skymp5-client/src/features/browser.ts
+++ skymp5-client/src/features/browser.ts
@@ -58,9 +58,7 @@ export const main = (): void => {
   browser.setVisible(false);
   once("update", () => browser.setVisible(true));
 
-  const openedMenus: string[] = [];
-
-  const badMenuOpen = () => !!openedMenus.length;
+  const badMenusOpen = new Set<string>();
 
   on("menuOpen", (e: MenuOpenEvent) => {
     if (e.name === Menu.Cursor) {
@@ -68,7 +66,7 @@ export const main = (): void => {
     }
     if (IsBadMenu(e.name)) {
       browser.setVisible(false);
-      openedMenus.push(e.name);
+      badMenusOpen.add(e.name);
     } else if (e.name === Menu.HUD) {
       browser.setVisible(true);
     }
@@ -78,11 +76,10 @@ export const main = (): void => {
     if (e.name === Menu.Cursor) {
       isCursorMenuOpened = false;
     }
-    const i = openedMenus.indexOf(e.name);
-    if (i !== -1) {
-      openedMenus.splice(i, 1);
-
-      if (openedMenus.length === 0) browser.setVisible(true);
+    if (badMenusOpen.delete(e.name)) {
+      if (badMenusOpen.size === 0) {
+        browser.setVisible(true);
+      }
     }
 
     if (e.name === Menu.HUD) browser.setVisible(false);
@@ -92,7 +89,11 @@ export const main = (): void => {
     [[DxScanCode.F1], () => FormView.isDisplayingNicknames = !FormView.isDisplayingNicknames],
     [[DxScanCode.F2], () => browser.setVisible(!browser.isVisible())],
     [[DxScanCode.F6], () => browser.setFocused(!browser.isFocused())],
-    [[DxScanCode.Enter], () => browser.setFocused(true)],
+    [[DxScanCode.Enter], () => {
+      if (badMenusOpen.size !== 0) {
+        browser.setFocused(true);
+      }
+    }],
     [
       [DxScanCode.Escape],
       () => browser.isFocused() && browser.setFocused(false),
