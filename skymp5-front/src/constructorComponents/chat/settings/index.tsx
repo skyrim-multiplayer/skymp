317e4b6912a2dda0e6412b2675f5ec1db8656dfe
Vlad Denisov <vlad.a.denisov@gmail.com>
2022-08-28 23:27:18 +0300

    fix: font in settings

--- skymp5-front/src/constructorComponents/chat/settings/index.tsx
+++ skymp5-front/src/constructorComponents/chat/settings/index.tsx
@@ -15,9 +15,9 @@ const Settings = (props: {
   return (
     <div className='chat-settings'>
       <div className='content'>
-        <SkyrimSlider text={'Размер Шрифта'} name={'fontSize'} min={14} max={22} setValue={(value) => props.setFontSize(value)} sliderValue={props.fontSize} marks={[14, 15, 16, 17, 18, 19, 20, 21, 22]}/>
-        <CheckBox text={'Звуки Дайсов'} initialValue={!props.isSoundsDisabled} setChecked={(value) => props.setDisableSounds(!value)} disabled={false} />
-        <CheckBox text={'Кнопка Отправки'} initialValue={props.showSendButton} setChecked={props.setShowSendButton} disabled={false} />
+        <SkyrimSlider text={'размер шрифта'} name={'fontSize'} min={14} max={22} setValue={(value) => props.setFontSize(value)} sliderValue={props.fontSize} marks={[14, 15, 16, 17, 18, 19, 20, 21, 22]}/>
+        <CheckBox text={'звуки дайсов'} initialValue={!props.isSoundsDisabled} setChecked={(value) => props.setDisableSounds(!value)} disabled={false} />
+        <CheckBox text={'кнопка отправки'} initialValue={props.showSendButton} setChecked={props.setShowSendButton} disabled={false} />
       </div>
       <SkyrimFrame width={512} height={360} header={false} name={'Settings'}/>
     </div>
