0ac527e66af3b1e2f47a4ef63d2a54926a3f0fa7
Ruslan <ruslan56781@mail.ru>
2022-09-14 15:35:08 +0300

    Fix Error

--- skymp5-server/cpp/papyrus_vm_lib/Reader.cpp
+++ skymp5-server/cpp/papyrus_vm_lib/Reader.cpp
@@ -269,7 +269,7 @@ VarValue Reader::FillVariableData()
       Read32_bit();
       break;
     default:
-      throw std::runtime_error("Error");
+      throw std::runtime_error("Wrong Type  ");
   }
 
   return Data;
