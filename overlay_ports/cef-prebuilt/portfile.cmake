2cfbda2b13f23f1262ea744f0192330ed84b1a02
Roman Nikonov <code@nic11.xyz>
2022-11-17 00:05:29 +0300

    internal: update CEF to 107.1.9 (#1216)

--- overlay_ports/cef-prebuilt/portfile.cmake
+++ overlay_ports/cef-prebuilt/portfile.cmake
@@ -1,9 +1,9 @@
-set(CEF_URL "https://cef-builds.spotifycdn.com/cef_binary_105.3.25%2Bg0ca6a9e%2Bchromium-105.0.5195.54_windows64.tar.bz2")
+set(CEF_URL "https://cef-builds.spotifycdn.com/cef_binary_107.1.9+g1f0a21a+chromium-107.0.5304.110_windows64.tar.bz2")
 
 vcpkg_download_distfile(ARCHIVE
     URLS ${CEF_URL}
     FILENAME "cef.tar.bz2"
-    SHA512 a4cc027671b85bb881b04d654e40c9d3027ae5ef833dc2cefcd80733bf9491219aa4d9ff3f9634d6c06d8b5c92dcc2e1d38cb460edc6874ada5423e8e4be249b 
+    SHA512 81e4ce641cbf44f35bc09ff471d1893b9ce037ce27f399af8898be429b745a8e0f5fa8cfa203965f55b74d335dec94f7be4b9a73b0f0fbe71b5fbab0f8116e49
 )
 
 vcpkg_extract_source_archive_ex(
