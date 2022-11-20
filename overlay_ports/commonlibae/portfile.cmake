94808f1cbb490420cb10659ffb30c1b352f6d13b
Roman Nikonov <code@nic11.xyz>
2022-11-20 13:08:39 +0100

    update sha

--- overlay_ports/commonlibae/portfile.cmake
+++ overlay_ports/commonlibae/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO Ryan-rsm-McKenzie/CommonLibSSE
     REF 0e9d380b90950eb3ece1e5b95e3b6a379ee03f8e
-    SHA512 0
+    SHA512 80afa2c9444f4bbb873e8de4a4af8b38b19af2ac3b60d0d68ca25d02b1b3f4bdac83ebe30ced2385c8fc90bb0e0dcf64f9fc05f896951a0dd60a799bc8d53a35
     HEAD_REF master
     PATCHES
       patches/objectrefr-make_moverefr_public.patch
