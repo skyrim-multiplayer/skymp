449c029d15b4f4c32479c9c7aa5891fb3a73db9e
Egor <ejs.defolt@gmail.com>
2022-12-18 21:18:45 +0500

    fix: fix animation system test

--- unit/AnimationSystemTest.cpp
+++ unit/AnimationSystemTest.cpp
@@ -19,11 +19,11 @@ TEST_CASE("Animations system processes animation events correctly",
   data.animEventName = "attackStart";
   REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 1.f);
   system.Process(&actor, data);
-  // attackStart consumes 5 points of stamina
-  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.95f);
-  data.animEventName = "jumpStart";
-  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.95f);
+  // attackStart consumes 7 points of stamina
+  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.93f);
+  data.animEventName = "jumpStandingStart";
+  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.93f);
   system.Process(&actor, data);
-  // jumpStart consumes 5 points of stamina
-  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.9f);
+  // jumpStart consumes 10 points of stamina
+  REQUIRE(actor.GetChangeForm().actorValues.staminaPercentage == 0.83f);
 }
