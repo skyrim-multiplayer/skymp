c798576aec33fe540fdee7ed1330bb5f4f82fde2
Roman Nikonov <code@nic11.xyz>
2022-12-18 17:24:04 +0100

    already had \n

--- unit/DistContentsTest.cpp
+++ unit/DistContentsTest.cpp
@@ -113,8 +113,8 @@ TEST_CASE("Distribution folder must contain all requested files",
     std::stringstream err;
     err << "Unexpected contents of '" << DIST_DIR << "', see diff"
         << std::endl;
-    err << ss.str() << std::endl;
-    err << "Update unit/DistContentsExpected.json if it was expected to change"
+    err << ss.str()
+        << "Update unit/DistContentsExpected.json if it was expected to change"
         << std::endl;
     err << std::endl;
     throw std::runtime_error(err.str());
