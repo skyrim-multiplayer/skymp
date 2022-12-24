9c66c699e30812d0be237dcc59b06eb2cd8f0ac4
Ruslan <ruslan56781@mail.ru>
2022-12-24 15:11:31 +0300

    papyrus methods GetActorValue

---
+++ PapyrusActor.cpp
@@ -0,0 +1,111 @@
+#include "PapyrusActor.h"
+
+#include "MpActor.h"
+#include "MpFormGameObject.h"
+#include "CIString.h"
+#include "SpSnippetFunctionGen.h"
+
+namespace {
+espm::ActorValue ConvertToAV(CIString actorValueName)
+{
+  if (!actorValueName.compare("health")) {
+    return espm::ActorValue::Health;
+  }
+  if (!actorValueName.compare("stamina")) {
+    return espm::ActorValue::Stamina;
+  }
+  if (!actorValueName.compare("magicka")) {
+    return espm::ActorValue::Magicka;
+  }
+  return espm::ActorValue::None;
+}
+}
+
+VarValue PapyrusActor::IsWeaponDrawn(VarValue self,
+                                     const std::vector<VarValue>& arguments)
+{
+  if (auto actor = GetFormPtr<MpActor>(self)) {
+    return VarValue(actor->IsWeaponDrawn());
+  }
+  return VarValue(false);
+}
+
+VarValue PapyrusActor::RestoreActorValue(
+  VarValue self, const std::vector<VarValue>& arguments)
+{
+  espm::ActorValue attributeName =
+    ConvertToAV(static_cast<const char*>(arguments[0]));
+  float modifire = static_cast<double>(arguments[1]);
+  if (auto actor = GetFormPtr<MpActor>(self)) {
+    actor->RestoreActorValue(attributeName, modifire);
+  }
+  return VarValue();
+}
+
+VarValue PapyrusActor::DamageActorValue(VarValue self,
+                                        const std::vector<VarValue>& arguments)
+{
+  espm::ActorValue attributeName =
+    ConvertToAV(static_cast<const char*>(arguments[0]));
+  float modifire = static_cast<double>(arguments[1]);
+  if (auto actor = GetFormPtr<MpActor>(self)) {
+    actor->DamageActorValue(attributeName, modifire);
+  }
+  return VarValue();
+}
+
+VarValue PapyrusActor::SetAlpha(VarValue self,
+                                const std::vector<VarValue>& arguments)
+{
+  if (auto selfRefr = GetFormPtr<MpActor>(self)) {
+    if (arguments.size() < 1) {
+      throw std::runtime_error("SetAlpha requires at least one argument");
+    }
+    // TODO: Make normal sync for this. For now using workaround to inform
+    // neigbours by sending papyrus functions to them.
+    auto funcName = "SetAlpha";
+    auto serializedArgs = SpSnippetFunctionGen::SerializeArguments(arguments);
+    for (auto listener : selfRefr->GetListeners()) {
+      auto targetRefr = dynamic_cast<MpActor*>(listener);
+      if (targetRefr) {
+        SpSnippet(GetName(), funcName, serializedArgs.data(),
+                  selfRefr->GetFormId())
+          .Execute(targetRefr);
+      }
+    }
+  }
+  return VarValue::None();
+}
+
+  VarValue PapyrusActor::GetActorValue(VarValue self,
+                                     const std::vector<VarValue>& arguments)
+{
+
+    if (arguments.size() < 1)
+    throw std::runtime_error(
+      "Papyrus Actor.GetActorValue: wrong argument count");
+
+
+  if (actor = GetFormPtr<MpActor>(self)) {
+      esmp::ActorValue attrID =
+        ConvertToAV(static_cast<const char*>(arguments[0]));
+
+      return VarValue(actor->GetActorValue(attrID));
+  }
+  return VarValue();
+}
+
+VarValue PapyrusActor::EquipItem(VarValue self,
+                                 const std::vector<VarValue>& arguments)
+{
+  if (auto actor = GetFormPtr<MpActor>(self)) {
+    if (arguments.size() < 1) {
+      throw std::runtime_error("EquipItem requires at least one argument");
+    }
+    SpSnippet(GetName(), "EquipItem",
+              SpSnippetFunctionGen::SerializeArguments(arguments).data(),
+              actor->GetFormId())
+      .Execute(actor);
+  }
+  return VarValue::None();
+}
