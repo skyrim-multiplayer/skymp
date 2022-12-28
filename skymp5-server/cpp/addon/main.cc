skyrim-multiplayer/skymp

commit 81da95f8e034569ff192d5fd6f17d04ac7b9ccbd

feat(skymp5-server): implement animation system and actor values setting (#1212)

skymp5-server/cpp/addon/main.cc
@@ -1355,9 +1355,9 @@ void ScampServer::RegisterChakraApi(std::shared_ptr<JsEngine> chakraEngine)
         if (auto actor = dynamic_cast<MpActor*>(&refr)) {
           auto chForm = actor->GetChangeForm();
           res = JsValue::Object();
-          res.SetProperty("health", chForm.healthPercentage);
-          res.SetProperty("magicka", chForm.magickaPercentage);
-          res.SetProperty("stamina", chForm.staminaPercentage);
+          res.SetProperty("health", chForm.actorValues.healthPercentage);
+          res.SetProperty("magicka", chForm.actorValues.magickaPercentage);
+          res.SetProperty("stamina", chForm.actorValues.staminaPercentage);
         }
       } else if (propertyName == "profileId") {
         if (auto actor = dynamic_cast<MpActor*>(&refr)) {
@@ -1441,9 +1441,11 @@ void ScampServer::RegisterChakraApi(std::shared_ptr<JsEngine> chakraEngine)
         }
       } else if (propertyName == "percentages") {
         if (auto actor = dynamic_cast<MpActor*>(&refr)) {
-          actor->NetSetPercentages(newValue["health"].get<float>(),
-                                   newValue["magicka"].get<float>(),
-                                   newValue["stamina"].get<float>());
+          ActorValues actorValues;
+          actorValues.healthPercentage = newValue["health"].get<float>();
+          actorValues.magickaPercentage = newValue["magicka"].get<float>();
+          actorValues.staminaPercentage = newValue["stamina"].get<float>();
+          actor->NetSetPercentages(actorValues);
         }
       } else {
 

skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -1,4 +1,5 @@
 #include "ActionListener.h"
+#include "AnimationSystem.h"
 #include "ConsoleCommands.h"
 #include "CropRegeneration.h"
 #include "DummyMessageOutput.h"
@@ -126,15 +127,7 @@ void ActionListener::OnUpdateAnimation(const RawMessageData& rawMsgData,
   if (!actor) {
     return;
   }
-
-  constexpr const char* kBlockStartAnimationName = "blockStart";
-  constexpr const char* kBlockStopAnimationName = "blockStop";
-  if (!strcmp(animationData.animEventName, kBlockStartAnimationName)) {
-    actor->SetIsBlockActive(true);
-  }
-  if (!strcmp(animationData.animEventName, kBlockStopAnimationName)) {
-    actor->SetIsBlockActive(false);
-  }
+  partOne.animationSystem.Process(actor, animationData);
   SendToNeighbours(idx, rawMsgData);
 }
 
@@ -509,9 +502,7 @@ void ActionListener::OnCustomEvent(const RawMessageData& rawMsgData,
 }
 
 void ActionListener::OnChangeValues(const RawMessageData& rawMsgData,
-                                    const float healthPercentage,
-                                    const float magickaPercentage,
-                                    const float staminaPercentage)
+                                    const ActorValues& newActorValues)
 {
   MpActor* actor = partOne.serverState.ActorByUser(rawMsgData.userId);
   if (!actor) {
@@ -522,38 +513,35 @@ void ActionListener::OnChangeValues(const RawMessageData& rawMsgData,
   float timeAfterRegeneration = CropPeriodAfterLastRegen(
     actor->GetDurationOfAttributesPercentagesUpdate(now).count());
 
-  MpChangeForm changeForm = actor->GetChangeForm();
-  float health = healthPercentage;
-  float magicka = magickaPercentage;
-  float stamina = staminaPercentage;
+  ActorValues currentActorValues = actor->GetChangeForm().actorValues;
+  float health = newActorValues.healthPercentage;
+  float magicka = newActorValues.magickaPercentage;
+  float stamina = newActorValues.staminaPercentage;
 
-  if (healthPercentage != changeForm.healthPercentage) {
-    health = CropHealthRegeneration(health, timeAfterRegeneration, actor);
+  if (newActorValues.healthPercentage != currentActorValues.healthPercentage) {
+    currentActorValues.healthPercentage =
+      CropHealthRegeneration(health, timeAfterRegeneration, actor);
   }
-  if (magickaPercentage != changeForm.magickaPercentage) {
-    magicka = CropMagickaRegeneration(magicka, timeAfterRegeneration, actor);
+  if (newActorValues.magickaPercentage !=
+      currentActorValues.magickaPercentage) {
+    currentActorValues.magickaPercentage =
+      CropMagickaRegeneration(magicka, timeAfterRegeneration, actor);
   }
-  if (staminaPercentage != changeForm.staminaPercentage) {
-    stamina = CropStaminaRegeneration(stamina, timeAfterRegeneration, actor);
-  }
-
-  if (IsNearlyEqual(health, healthPercentage) == false ||
-      IsNearlyEqual(magicka, magickaPercentage) == false ||
-      IsNearlyEqual(stamina, staminaPercentage) == false) {
-    std::string s;
-    s += Networking::MinPacketId;
-    s += nlohmann::json{
-      { "t", MsgType::ChangeValues },
-      { "data",
-        { { "health", health },
-          { "magicka", magicka },
-          { "stamina", stamina } } }
-    }.dump();
-    actor->SendToUser(s.data(), s.size(), true);
+  if (newActorValues.staminaPercentage !=
+      currentActorValues.staminaPercentage) {
+    currentActorValues.staminaPercentage =
+      CropStaminaRegeneration(stamina, timeAfterRegeneration, actor);
   }
 
-  actor->SetPercentages(health, magicka, stamina);
-  actor->SetLastAttributesPercentagesUpdate(now);
+  if (!IsNearlyEqual(currentActorValues.healthPercentage,
+                     newActorValues.healthPercentage) ||
+      !IsNearlyEqual(currentActorValues.magickaPercentage,
+                     newActorValues.magickaPercentage) ||
+      !IsNearlyEqual(currentActorValues.staminaPercentage,
+                     newActorValues.staminaPercentage)) {
+    actor->NetSendChangeValues(currentActorValues);
+  }
+  actor->SetPercentages(currentActorValues);
 }
 
 namespace {
@@ -747,32 +735,32 @@ void ActionListener::OnHit(const RawMessageData& rawMsgData_,
     return;
   }
 
-  MpChangeForm targetForm = targetActor.GetChangeForm();
+  ActorValues currentActorValues = targetActor.GetChangeForm().actorValues;
 
-  float healthPercentage = targetForm.healthPercentage;
-  float magickaPercentage = targetForm.magickaPercentage;
-  float staminaPercentage = targetForm.staminaPercentage;
+  float healthPercentage = currentActorValues.healthPercentage;
+  float magickaPercentage = currentActorValues.magickaPercentage;
+  float staminaPercentage = currentActorValues.staminaPercentage;
 
   hitData.isHitBlocked = targetActor.IsBlockActive()
     ? ShouldBeBlocked(*aggressor, targetActor)
     : false;
   float damage = partOne.CalculateDamage(*aggressor, targetActor, hitData);
   damage = damage < 0.f ? 0.f : damage;
-  float currentHealthPercentage =
+  currentActorValues.healthPercentage =
     CalculateCurrentHealthPercentage(targetActor, damage, healthPercentage);
 
-  currentHealthPercentage =
-    currentHealthPercentage < 0.f ? 0.f : currentHealthPercentage;
+  currentActorValues.healthPercentage =
+    currentActorValues.healthPercentage < 0.f
+    ? 0.f
+    : currentActorValues.healthPercentage;
 
-  auto now = std::chrono::steady_clock::now();
-  targetActor.NetSetPercentages(currentHealthPercentage, magickaPercentage,
-                                staminaPercentage, now, aggressor);
-  targetActor.SetLastHitTime(now);
+  targetActor.NetSetPercentages(currentActorValues, aggressor);
+  targetActor.SetLastHitTime();
 
   spdlog::debug("Target {0:x} is hitted by {1} damage. Current health "
                 "percentage: {2}. Last "
                 "health percentage: {3}. (Last: {3} => Current: {2})",
-                hitData.target, damage, currentHealthPercentage,
+                hitData.target, damage, currentActorValues.healthPercentage,
                 healthPercentage);
 }
 

skymp5-server/cpp/server_guest_lib/ActorValues.h
@@ -1,0 +1,26 @@
+#pragma once
+#include <tuple>
+
+struct ActorValues
+{
+  float healthPercentage = 1.f;
+  float magickaPercentage = 1.f;
+  float staminaPercentage = 1.f;
+  float health = 100.f;
+  float magicka = 100.f;
+  float stamina = 100.f;
+  float healRate = 0.7f;
+  float magickaRate = 3.0f;
+  float staminaRate = 5.0f;
+  float healRateMult = 100.f;
+  float magickaRateMult = 100.f;
+  float staminaRateMult = 100.f;
+
+  auto ToTuple() const
+  {
+    return std::make_tuple(healthPercentage, magickaPercentage,
+                           staminaPercentage, health, magicka, stamina,
+                           healRate, magickaRate, staminaRate, healRateMult,
+                           magickaRate, staminaRate);
+  }
+};

skymp5-server/cpp/server_guest_lib/AnimationSystem.cpp
@@ -1,0 +1,122 @@
+#include "AnimationSystem.h"
+#include "AnimationData.h"
+#include "MpActor.h"
+#include "espm.h"
+
+AnimationSystem::AnimationSystem()
+{
+  InitAnimationCallbacks();
+}
+
+void AnimationSystem::Process(MpActor* actor, const AnimationData& animData)
+{
+  auto it = animationCallbacks.find(animData.animEventName);
+  if (it == animationCallbacks.end()) {
+    return;
+  }
+  it->second(actor);
+}
+
+std::chrono::steady_clock::time_point
+AnimationSystem::GetLastAttackReleaseAnimationTime() const
+{
+  return lastAttackReleaseAnimationTime;
+}
+
+void AnimationSystem::SetLastAttackReleaseAnimationTime(
+  std::chrono::steady_clock::time_point timePoint)
+{
+  lastAttackReleaseAnimationTime = timePoint;
+}
+
+using namespace std::chrono_literals;
+
+void AnimationSystem::InitAnimationCallbacks()
+{
+  animationCallbacks = {
+    {
+      "blockStart",
+      [](MpActor* actor) {
+        constexpr float newRate = 0.f;
+        actor->SetIsBlockActive(true);
+        actor->SetActorValue(espm::ActorValue::StaminaRate, newRate);
+      },
+    },
+    {
+      "blockStop",
+      [&](MpActor* actor) {
+        actor->SetIsBlockActive(false);
+        actor->SetActorValue(espm::ActorValue::StaminaRate,
+                             actor->GetBaseValues().staminaRate);
+      },
+    },
+    {
+      "attackStart",
+      [](MpActor* actor) {
+        constexpr float modifier = 7.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "AttackStartH2HRight",
+      [](MpActor* actor) {
+        constexpr float modifier = 4.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "AttackStartH2HLeft",
+      [](MpActor* actor) {
+        constexpr float modifier = 4.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "JumpStandingStart",
+      [](MpActor* actor) {
+        constexpr float modifier = 10.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "JumpDirectionalStart",
+      [](MpActor* actor) {
+        constexpr float modifier = 15.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "bowAttackStart",
+      [this](MpActor* actor) { SetLastAttackReleaseAnimationTime(); },
+    },
+    {
+      "attackRelease",
+      [&](MpActor* actor) {
+        std::chrono::duration<float> elapsedTime =
+          std::chrono::steady_clock::now() -
+          GetLastAttackReleaseAnimationTime();
+        if (elapsedTime > 2s) {
+          constexpr float modifier = 20.f;
+          actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+        } else {
+          constexpr float modifier = 60.f;
+          actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+        }
+      },
+    },
+    {
+      "crossbowAttackStart",
+      [](MpActor* actor) {
+        constexpr float modifier = 10.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
+      "SneakSprintStartRoll",
+      [](MpActor* actor) {
+        constexpr float modifier = 15.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+  };
+}

skymp5-server/cpp/server_guest_lib/AnimationSystem.h
@@ -1,0 +1,30 @@
+#pragma once
+#include <chrono>
+#include <functional>
+#include <string>
+#include <unordered_map>
+
+class MpActor;
+struct AnimationData;
+
+class AnimationSystem
+{
+public:
+  AnimationSystem();
+  void Process(MpActor* actor, const AnimationData& animData);
+
+private:
+  using AnimationCallback = std::function<void(MpActor*)>;
+  using AnimationCallbacks =
+    std::unordered_map<std::string, AnimationCallback>;
+
+  void InitAnimationCallbacks();
+  std::chrono::steady_clock::time_point GetLastAttackReleaseAnimationTime()
+    const;
+  void SetLastAttackReleaseAnimationTime(
+    std::chrono::steady_clock::time_point timePoint =
+      std::chrono::steady_clock::now());
+
+  AnimationCallbacks animationCallbacks;
+  std::chrono::steady_clock::time_point lastAttackReleaseAnimationTime;
+};

skymp5-server/cpp/server_guest_lib/PartOne.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "AnimationSystem.h"
 #include "GamemodeApi.h"
 #include "ISaveStorage.h"
 #include "MpActor.h"
@@ -77,6 +78,7 @@ public:
 
   WorldState worldState;
   ServerState serverState;
+  AnimationSystem animationSystem;
 
   Networking::ISendTarget& GetSendTarget() const;
 

skymp5-server/cpp/server_guest_lib/ActionListener.h
@@ -8,6 +8,7 @@
 
 class ServerState;
 class WorldState;
+struct ActorValues;
 
 class ActionListener
 {
@@ -77,9 +78,7 @@ public:
                              const char* eventName, simdjson::dom::element& e);
 
   virtual void OnChangeValues(const RawMessageData& rawMsgData,
-                              const float healthPercentage,
-                              const float magickaPercentage,
-                              const float staminaPercentage);
+                              const ActorValues& actorValues);
 
   virtual void OnHit(const RawMessageData& rawMsgData, const HitData& hitData);
 

skymp5-server/cpp/server_guest_lib/CropRegeneration.cpp
@@ -47,25 +47,27 @@ float CropHealthRegeneration(float newAttributeValue,
   BaseActorValues baseValues = GetValues(actor);
   return CropRegeneration(newAttributeValue, secondsAfterLastRegen,
                           baseValues.healRate, baseValues.healRateMult,
-                          actor->GetChangeForm().healthPercentage);
+                          actor->GetChangeForm().actorValues.healthPercentage);
 }
 
 float CropMagickaRegeneration(float newAttributeValue,
                               float secondsAfterLastRegen, MpActor* actor)
 {
   BaseActorValues baseValues = GetValues(actor);
-  return CropRegeneration(newAttributeValue, secondsAfterLastRegen,
-                          baseValues.magickaRate, baseValues.magickaRateMult,
-                          actor->GetChangeForm().magickaPercentage);
+  return CropRegeneration(
+    newAttributeValue, secondsAfterLastRegen, baseValues.magickaRate,
+    baseValues.magickaRateMult,
+    actor->GetChangeForm().actorValues.magickaPercentage);
 }
 
 float CropStaminaRegeneration(float newAttributeValue,
                               float secondsAfterLastRegen, MpActor* actor)
 {
   BaseActorValues baseValues = GetValues(actor);
-  return CropRegeneration(newAttributeValue, secondsAfterLastRegen,
-                          baseValues.staminaRate, baseValues.staminaRateMult,
-                          actor->GetChangeForm().staminaPercentage);
+  return CropRegeneration(
+    newAttributeValue, secondsAfterLastRegen,
+    actor->GetChangeForm().actorValues.staminaRate, baseValues.staminaRateMult,
+    actor->GetChangeForm().actorValues.staminaPercentage);
 }
 
 float CropPeriodAfterLastRegen(float secondsAfterLastRegen,

commit 925fd3785fb74fcb084691876d0a3363ff86358e

fix(skymp5-server): fix bugs introduced in #1212

skymp5-server/cpp/addon/main.cc
@@ -1441,7 +1441,7 @@ void ScampServer::RegisterChakraApi(std::shared_ptr<JsEngine> chakraEngine)
         }
       } else if (propertyName == "percentages") {
         if (auto actor = dynamic_cast<MpActor*>(&refr)) {
-          ActorValues actorValues;
+          ActorValues actorValues = actor->GetChangeForm().actorValues;
           actorValues.healthPercentage = newValue["health"].get<float>();
           actorValues.magickaPercentage = newValue["magicka"].get<float>();
           actorValues.staminaPercentage = newValue["stamina"].get<float>();

skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -127,7 +127,23 @@ void ActionListener::OnUpdateAnimation(const RawMessageData& rawMsgData,
   if (!actor) {
     return;
   }
-  partOne.animationSystem.Process(actor, animationData);
+
+  WorldState* espmProvider = actor->GetParent();
+  if (!espmProvider) {
+    return;
+  }
+
+  if (!partOne.animationSystem) {
+    std::vector<std::string> espmFiles = espmProvider->espmFiles;
+
+    std::set<std::string> s;
+    s = { espmFiles.begin(), espmFiles.end() };
+    bool isSweetpie = s.count("SweetPie.esp") != 0;
+
+    partOne.animationSystem = std::make_unique<AnimationSystem>(isSweetpie);
+  }
+  partOne.animationSystem->Process(actor, animationData);
+
   SendToNeighbours(idx, rawMsgData);
 }
 

skymp5-server/cpp/server_guest_lib/ActorValues.h
@@ -3,9 +3,12 @@
 
 struct ActorValues
 {
+  // Percentages are expected to be always on top of struct
+  // See unit/ChangeValuesTest.cpp
   float healthPercentage = 1.f;
   float magickaPercentage = 1.f;
   float staminaPercentage = 1.f;
+
   float health = 100.f;
   float magicka = 100.f;
   float stamina = 100.f;
@@ -21,6 +24,6 @@ struct ActorValues
     return std::make_tuple(healthPercentage, magickaPercentage,
                            staminaPercentage, health, magicka, stamina,
                            healRate, magickaRate, staminaRate, healRateMult,
-                           magickaRate, staminaRate);
+                           magickaRateMult, staminaRateMult);
   }
 };

skymp5-server/cpp/server_guest_lib/AnimationSystem.cpp
@@ -3,9 +3,9 @@
 #include "MpActor.h"
 #include "espm.h"
 
-AnimationSystem::AnimationSystem()
+AnimationSystem::AnimationSystem(bool isSweetpie)
 {
-  InitAnimationCallbacks();
+  InitAnimationCallbacks(isSweetpie);
 }
 
 void AnimationSystem::Process(MpActor* actor, const AnimationData& animData)
@@ -17,39 +17,52 @@ void AnimationSystem::Process(MpActor* actor, const AnimationData& animData)
   it->second(actor);
 }
 
-std::chrono::steady_clock::time_point
-AnimationSystem::GetLastAttackReleaseAnimationTime() const
+void AnimationSystem::ClearInfo(MpActor* actor)
 {
-  return lastAttackReleaseAnimationTime;
+  lastAttackReleaseAnimationTimePoints.erase(actor->GetFormId());
+}
+
+std::chrono::steady_clock::time_point
+AnimationSystem::GetLastAttackReleaseAnimationTime(MpActor* actor) const
+{
+  auto it = lastAttackReleaseAnimationTimePoints.find(actor->GetFormId());
+  if (it == lastAttackReleaseAnimationTimePoints.end()) {
+    return std::chrono::steady_clock::time_point();
+  }
+  return it->second;
 }
 
 void AnimationSystem::SetLastAttackReleaseAnimationTime(
-  std::chrono::steady_clock::time_point timePoint)
+  MpActor* actor, std::chrono::steady_clock::time_point timePoint)
 {
-  lastAttackReleaseAnimationTime = timePoint;
+  lastAttackReleaseAnimationTimePoints[actor->GetFormId()] = timePoint;
 }
 
-using namespace std::chrono_literals;
-
-void AnimationSystem::InitAnimationCallbacks()
+void AnimationSystem::InitAnimationCallbacks(bool isSweetpie)
 {
   animationCallbacks = {
     {
       "blockStart",
-      [](MpActor* actor) {
+      [isSweetpie](MpActor* actor) {
         constexpr float newRate = 0.f;
         actor->SetIsBlockActive(true);
-        actor->SetActorValue(espm::ActorValue::StaminaRate, newRate);
+        if (isSweetpie) {
+          actor->SetActorValue(espm::ActorValue::StaminaRate, newRate);
+        }
       },
     },
     {
       "blockStop",
-      [&](MpActor* actor) {
+      [isSweetpie](MpActor* actor) {
         actor->SetIsBlockActive(false);
-        actor->SetActorValue(espm::ActorValue::StaminaRate,
-                             actor->GetBaseValues().staminaRate);
+        if (isSweetpie) {
+          actor->SetActorValue(espm::ActorValue::StaminaRate,
+                               actor->GetBaseValues().staminaRate);
+        }
       },
-    },
+    }
+  };
+  const AnimationCallbacks additionalCallbacks = {
     {
       "attackStart",
       [](MpActor* actor) {
@@ -87,15 +100,15 @@ void AnimationSystem::InitAnimationCallbacks()
     },
     {
       "bowAttackStart",
-      [this](MpActor* actor) { SetLastAttackReleaseAnimationTime(); },
+      [this](MpActor* actor) { SetLastAttackReleaseAnimationTime(actor); },
     },
     {
       "attackRelease",
       [&](MpActor* actor) {
         std::chrono::duration<float> elapsedTime =
           std::chrono::steady_clock::now() -
-          GetLastAttackReleaseAnimationTime();
-        if (elapsedTime > 2s) {
+          GetLastAttackReleaseAnimationTime(actor);
+        if (elapsedTime > std::chrono::seconds(2)) {
           constexpr float modifier = 20.f;
           actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
         } else {
@@ -119,4 +132,9 @@ void AnimationSystem::InitAnimationCallbacks()
       },
     },
   };
+
+  if (isSweetpie) {
+    animationCallbacks.insert(additionalCallbacks.begin(),
+                              additionalCallbacks.end());
+  }
 }

skymp5-server/cpp/server_guest_lib/AnimationSystem.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "CIString.h"
 #include <chrono>
 #include <functional>
 #include <string>
@@ -10,21 +11,24 @@ struct AnimationData;
 class AnimationSystem
 {
 public:
-  AnimationSystem();
+  AnimationSystem(bool isSweetpie);
   void Process(MpActor* actor, const AnimationData& animData);
+  void ClearInfo(MpActor* actor);
 
 private:
   using AnimationCallback = std::function<void(MpActor*)>;
-  using AnimationCallbacks =
-    std::unordered_map<std::string, AnimationCallback>;
+  using AnimationCallbacks = std::unordered_map<CIString, AnimationCallback>;
+  using AnimationTimePoints =
+    std::unordered_map<uint32_t, std::chrono::steady_clock::time_point>;
 
-  void InitAnimationCallbacks();
-  std::chrono::steady_clock::time_point GetLastAttackReleaseAnimationTime()
-    const;
+  void InitAnimationCallbacks(bool isSweetpie);
+  std::chrono::steady_clock::time_point GetLastAttackReleaseAnimationTime(
+    MpActor* actor) const;
   void SetLastAttackReleaseAnimationTime(
+    MpActor* actor,
     std::chrono::steady_clock::time_point timePoint =
       std::chrono::steady_clock::now());
 
   AnimationCallbacks animationCallbacks;
-  std::chrono::steady_clock::time_point lastAttackReleaseAnimationTime;
+  AnimationTimePoints lastAttackReleaseAnimationTimePoints;
 };

skymp5-server/cpp/server_guest_lib/PartOne.cpp
@@ -37,8 +37,8 @@ struct PartOne::Impl
   simdjson::dom::parser parser;
   espm::Loader* espm = nullptr;
 
-  std::function<void(Networking::ISendTarget*sendTarget,
-                     MpObjectReference*emitter, MpObjectReference*listener)>
+  std::function<void(Networking::ISendTarget* sendTarget,
+                     MpObjectReference* emitter, MpObjectReference* listener)>
     onSubscribe, onUnsubscribe;
 
   espm::CompressedFieldsCache compressedFieldsCache;
@@ -321,6 +321,11 @@ void PartOne::HandlePacket(void* partOneInstance, Networking::UserId userId,
       return this_->AddUser(userId, UserType::User);
     case Networking::PacketType::ServerSideUserDisconnect: {
       ScopedTask t([userId, this_] {
+        if (auto actor = this_->serverState.ActorByUser(userId)) {
+          if (this_->animationSystem) {
+            this_->animationSystem->ClearInfo(actor);
+          }
+        }
         this_->serverState.Disconnect(userId);
         this_->serverState.disconnectingUserId = Networking::InvalidUserId;
       });
@@ -398,11 +403,11 @@ FormCallbacks PartOne::CreateFormCallbacks()
 
   FormCallbacks::SubscribeCallback
     subscribe =
-      [this](MpObjectReference*emitter, MpObjectReference*listener) {
+      [this](MpObjectReference* emitter, MpObjectReference* listener) {
         return pImpl->onSubscribe(pImpl->sendTarget, emitter, listener);
       },
-    unsubscribe = [this](MpObjectReference*emitter,
-                         MpObjectReference*listener) {
+    unsubscribe = [this](MpObjectReference* emitter,
+                         MpObjectReference* listener) {
       return pImpl->onUnsubscribe(pImpl->sendTarget, emitter, listener);
     };
 

skymp5-server/cpp/server_guest_lib/PartOne.h
@@ -78,7 +78,7 @@ public:
 
   WorldState worldState;
   ServerState serverState;
-  AnimationSystem animationSystem;
+  std::shared_ptr<AnimationSystem> animationSystem;
 
   Networking::ISendTarget& GetSendTarget() const;
 