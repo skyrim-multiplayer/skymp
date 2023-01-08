7b112892d8de11eeeb041bd2e73f16526782667f
Roman Nikonov <code@nic11.xyz>
2023-01-08 17:25:01 +0100

    upd refs

--- misc/build-docker-image.sh
+++ misc/build-docker-image.sh
@@ -16,7 +16,7 @@ set -x
 VCPKG_URL="`git -C vcpkg remote get-url origin`"
 VCPKG_COMMIT="`git -C vcpkg rev-parse HEAD`"
 
-sed -i -r -e 's/(skymp-(vcpkg-deps|runtime-base):)[-0-9a-zA-Z]+/\1'"$VERSION_TAG"'/g' ci/github_env_linux ci/deploy/remote/branchctl.sh
+sed -i -r -e 's/(skymp-(vcpkg-deps|runtime-base):)[-0-9a-zA-Z]+/\1'"$VERSION_TAG"'/g' misc/github_env_linux misc/deploy/remote/branchctl.sh
 
 podman build . --target=skymp-vcpkg-deps --tag=skymp/skymp-vcpkg-deps:"$VERSION_TAG" --cpu-quota=$((100000*10)) --build-arg VCPKG_URL="$VCPKG_URL" --build-arg VCPKG_COMMIT="$VCPKG_COMMIT"
 podman build . --target=skymp-runtime-base --tag=skymp/skymp-runtime-base:"$VERSION_TAG" --cpu-quota=$((100000*10)) --build-arg VCPKG_URL="$VCPKG_URL" --build-arg VCPKG_COMMIT="$VCPKG_COMMIT"
--- misc/deploy/gather_server_branch.sh
+++ misc/deploy/gather_server_branch.sh
@@ -3,13 +3,13 @@
 set -e
 set -x
 
-data="`cd ci/deploy/gather-server-branch && node_modules/.bin/ts-node list_included_pulls.ts`"
+data="`cd misc/deploy/gather-server-branch && node_modules/.bin/ts-node list_included_pulls.ts`"
 
 msg="Gathering $DEPLOY_BRANCH..."$'\n'
 msg+="`echo "$data" | tail -n +2`"  # start from second line
-./ci/deploy/call_webhook.sh "$msg"
+./misc/deploy/call_webhook.sh "$msg"
 
 # Take first line and pass it to script. It treats this as a list of PR numbers
-echo "$data" | head -n 1 | xargs ./ci/deploy/merge_pulls.sh
+echo "$data" | head -n 1 | xargs ./misc/deploy/merge_pulls.sh
 
 git submodule update --init --recursive
