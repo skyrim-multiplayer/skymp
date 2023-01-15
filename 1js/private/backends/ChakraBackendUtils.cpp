fff371c01b167f0b1984218cb5ed69940a603be3
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-01-15 15:13:32 +0600

    fix

--- 1js/private/backends/ChakraBackendUtils.cpp
+++ 1js/private/backends/ChakraBackendUtils.cpp
@@ -21,26 +21,6 @@ void* ChakraBackendUtils::NativeFunctionImpl(void* callee, bool isConstructorCal
     }
   }
 
-void* ChakraBackendUtils::NativeFunctionImpl(void* callee, bool isConstructorCall,
-                                  void** arguments,
-                                  unsigned short argumentsCount,
-                                  void* callbackState)
-  {
-    try {
-      JsFunctionArgumentsImpl args(arguments, argumentsCount);
-
-      auto f = reinterpret_cast<FunctionT*>(callbackState);
-      return (*f)(args).value;
-    } catch (std::exception& e) {
-      JsValueRef whatStr, err;
-      if (JsCreateString(e.what(), strlen(e.what()), &whatStr) == JsNoError &&
-          JsCreateError(whatStr, &err) == JsNoError) {
-        JsSetException(err);
-      }
-      return JS_INVALID_REFERENCE;
-    }
-  }
-
   std::string ChakraBackendUtils::ConvertJsExceptionToString(JsValueRef exception)
   {
     try {
--- 1js/private/backends/ChakraBackendUtils.h
+++ 1js/private/backends/ChakraBackendUtils.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <stdexcept>
 #include <ChakraCore.h>
 #include "private/JsFunctionArgumentsImpl.h"
