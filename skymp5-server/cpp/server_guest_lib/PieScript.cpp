f62e7bf10b0a8a5bb9e20677cfee81ea8c364a02
Egor Gavrilov <ejs.defolt@gmail.com>
2023-02-03 20:19:04 +0300

    fix: change capture in lambda function

--- skymp5-server/cpp/server_guest_lib/PieScript.cpp
+++ skymp5-server/cpp/server_guest_lib/PieScript.cpp
@@ -453,7 +453,7 @@ void PieScript::Play(MpActor& actor, WorldState& worldState,
                              it->second.GetManacost());
       actor.AddItem(it->second.GetBaseId(), 1);
       actor.RemoveItem(it->first, 1, nullptr);
-      worldState.SetTimer(20 * 3).Then([&](Viet::Void) {
+      worldState.SetTimer(20 * 3).Then([it, &actor](Viet::Void) {
         actor.RemoveItem(it->second.GetBaseId(), 1, nullptr);
         actor.AddItem(it->first, 1);
       });
