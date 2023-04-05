71f22cdd181481d6f6a5a7d84dac7490605548c5
Egor <ejs.defolt@gmail.com>
2023-04-06 00:01:31 +0500

    feat: add animation callback for left hand attacks

--- skymp5-server/cpp/server_guest_lib/AnimationSystem.cpp
+++ skymp5-server/cpp/server_guest_lib/AnimationSystem.cpp
@@ -58,6 +58,13 @@ void AnimationSystem::InitAnimationCallbacks()
       },
     },
     {
+      "attackStartLeftHand",
+      [](MpActor* actor) {
+        constexpr float modifier = 7.f;
+        actor->DamageActorValue(espm::ActorValue::Stamina, modifier);
+      },
+    },
+    {
       "AttackStartH2HRight",
       [](MpActor* actor) {
         constexpr float modifier = 4.f;
@@ -91,7 +98,7 @@ void AnimationSystem::InitAnimationCallbacks()
     },
     {
       "attackRelease",
-      [&](MpActor* actor) {
+      [this](MpActor* actor) {
         std::chrono::duration<float> elapsedTime =
           std::chrono::steady_clock::now() -
           GetLastAttackReleaseAnimationTime();
