42fa834b5d8f3e4ea06cc3183062eea6fdf8a284
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-04-09 21:10:29 +0600

    get rid of libkey

--- skymp5-server/ts/index.ts
+++ skymp5-server/ts/index.ts
@@ -25,7 +25,6 @@ import { pid } from "process";
 import * as fs from "fs";
 import * as chokidar from "chokidar";
 import * as path from "path";
-import * as libkey from "./libkey";
 
 import * as manifestGen from "./manifestGen";
 
@@ -86,27 +85,6 @@ systems.push(
   new Login(log, maxPlayers, master, port, ip, offlineMode)
 );
 
-const handleLibkeyJs = () => {
-  fs.writeFileSync("data/_libkey.js", libkey.src);
-  setTimeout(async () => {
-    while (1) {
-      await new Promise((r) => setTimeout(r, 5000));
-
-      const data = await new Promise<string>((resolve) =>
-        fs.readFile("data/_libkey.js", { encoding: "utf-8" }, (err, data) => {
-          err ? resolve("") : resolve(data);
-        })
-      );
-
-      if (data !== libkey.src) {
-        await new Promise<void>((r) =>
-          fs.writeFile("data/_libkey.js", libkey.src, () => r())
-        );
-      }
-    }
-  }, 1);
-};
-
 const setupStreams = (server: scampNative.ScampServer) => {
   class LogsStream {
     constructor(private logLevel: string) {
@@ -132,8 +110,6 @@ const setupStreams = (server: scampNative.ScampServer) => {
 };
 
 const main = async () => {
-  handleLibkeyJs();
-
   manifestGen.generateManifest(Settings.get());
   ui.main();
 
