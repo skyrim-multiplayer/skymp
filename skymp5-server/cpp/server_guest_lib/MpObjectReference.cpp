19c21e7fdfefd2707aab39e4c4520d9e19659a69
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-04-09 23:19:12 +0600

    remove unused events

--- skymp5-server/cpp/server_guest_lib/MpObjectReference.cpp
+++ skymp5-server/cpp/server_guest_lib/MpObjectReference.cpp
@@ -687,8 +687,6 @@ void MpObjectReference::Subscribe(MpObjectReference* emitter,
       listener->GetChangeForm().profileId != -1) {
     emitter->pImpl->onInitEventSent = true;
     emitter->SendPapyrusEvent("OnInit");
-
-    emitter->MpApiOnInit();
   }
 
   const bool hasPrimitive = emitter->HasPrimitive();
@@ -849,8 +847,6 @@ void MpObjectReference::ApplyChangeForm(const MpChangeForm& changeForm)
       tp - std::chrono::system_clock::now());
     RequestReloot(ms);
   }
-
-  MpApiOnInit();
 }
 
 const DynamicFields& MpObjectReference::GetDynamicFields() const
@@ -1061,17 +1057,6 @@ void MpObjectReference::ProcessActivate(MpObjectReference& activationSource)
       this->occupant = nullptr;
     }
   }
-}
-
-void MpObjectReference::MpApiOnInit()
-{
-  // nice API (91% of cpu in profiler when spawning player)
-  /*if (auto wst = GetParent()) {
-    const auto id = GetFormId();
-    for (auto& listener : wst->listeners) {
-      listener->OnMpApiEvent("onInit", std::nullopt, id);
-    }
-  }*/
 }
 
 bool MpObjectReference::MpApiOnActivate(MpObjectReference& caster)
