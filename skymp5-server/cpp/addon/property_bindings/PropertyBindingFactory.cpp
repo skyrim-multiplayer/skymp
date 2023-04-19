60c482cb8e230837886ebb99ced99b74d343f2f4
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-04-19 19:24:19 +0600

    fix(skymp5-server): make profileId property available again (#1374)

--- skymp5-server/cpp/addon/property_bindings/PropertyBindingFactory.cpp
+++ skymp5-server/cpp/addon/property_bindings/PropertyBindingFactory.cpp
@@ -16,6 +16,7 @@
 #include "OnlinePlayersBinding.h"
 #include "PercentagesBinding.h"
 #include "PosBinding.h"
+#include "ProfileIdBinding.h"
 #include "SpawnPointBinding.h"
 #include "TypeBinding.h"
 #include "WorldOrCellDescBinding.h"
@@ -24,42 +25,25 @@ std::map<std::string, std::shared_ptr<PropertyBinding>>
 PropertyBindingFactory::CreateStandardPropertyBindings()
 {
   std::map<std::string, std::shared_ptr<PropertyBinding>> result;
-  result["actorNeighbors"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<ActorNeighborsBinding>());
-  result["angle"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<AngleBinding>());
-  result["appearance"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<AppearanceBinding>());
-  result["baseDesc"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<BaseDescBinding>());
-  result["equipment"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<EquipmentBinding>());
-  result["inventory"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<InventoryBinding>());
-  result["isDead"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<IsDeadBinding>());
-  result["isDisabled"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<IsDisabledBinding>());
-  result["isOnline"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<IsOnlineBinding>());
-  result["isOpen"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<IsOpenBinding>());
-  result["locationalData"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<LocationalDataBinding>());
-  result["neighbors"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<NeighborsBinding>());
-  result["onlinePlayers"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<OnlinePlayersBinding>());
-  result["percentages"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<PercentagesBinding>());
-  result["pos"] =
-    std::dynamic_pointer_cast<PropertyBinding>(std::make_shared<PosBinding>());
-  result["spawnPoint"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<SpawnPointBinding>());
-  result["type"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<TypeBinding>());
-  result["worldOrCellDesc"] = std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<WorldOrCellDescBinding>());
+  result["actorNeighbors"] = std::make_shared<ActorNeighborsBinding>();
+  result["angle"] = std::make_shared<AngleBinding>();
+  result["appearance"] = std::make_shared<AppearanceBinding>();
+  result["baseDesc"] = std::make_shared<BaseDescBinding>();
+  result["equipment"] = std::make_shared<EquipmentBinding>();
+  result["inventory"] = std::make_shared<InventoryBinding>();
+  result["isDead"] = std::make_shared<IsDeadBinding>();
+  result["isDisabled"] = std::make_shared<IsDisabledBinding>();
+  result["isOnline"] = std::make_shared<IsOnlineBinding>();
+  result["isOpen"] = std::make_shared<IsOpenBinding>();
+  result["locationalData"] = std::make_shared<LocationalDataBinding>();
+  result["neighbors"] = std::make_shared<NeighborsBinding>();
+  result["onlinePlayers"] = std::make_shared<OnlinePlayersBinding>();
+  result["percentages"] = std::make_shared<PercentagesBinding>();
+  result["pos"] = std::make_shared<PosBinding>();
+  result["profileId"] = std::make_shared<ProfileIdBinding>();
+  result["spawnPoint"] = std::make_shared<SpawnPointBinding>();
+  result["type"] = std::make_shared<TypeBinding>();
+  result["worldOrCellDesc"] = std::make_shared<WorldOrCellDescBinding>();
   return result;
 }
 
@@ -67,6 +51,5 @@ std::shared_ptr<PropertyBinding>
 PropertyBindingFactory::CreateCustomPropertyBinding(
   const std::string& propertyName)
 {
-  return std::dynamic_pointer_cast<PropertyBinding>(
-    std::make_shared<CustomPropertyBinding>(propertyName));
+  return std::make_shared<CustomPropertyBinding>(propertyName);
 }
