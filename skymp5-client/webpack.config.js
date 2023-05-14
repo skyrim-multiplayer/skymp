4637d3e660cdde74f2fba1662ce5acff60ff904a
Marko Golovko <markgolovko@gmail.com>
2023-05-14 18:06:30 +0200

    Revert "add isHarvested check"
    This reverts commit 3b1f6b619f71a1a9908e9809a868d180871855d6.

--- skymp5-client/webpack.config.js
+++ skymp5-client/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
-// const outDirPath = path.resolve(__dirname, "../build/dist/client/Data/Platform/Plugins");
-const outDirPath = "F:/Program Files (x86)/Steam/steamapps/common/Skyrim Special Edition/Data/Platform/Plugins";
+
+const outDirPath = path.resolve(__dirname, "../build/dist/client/Data/Platform/Plugins");
 const outFileName = "skymp5-client.js";
 const spFilePath = path.resolve(__dirname, '../build/dist/client/Data/Platform/Modules/skyrimPlatform.ts');
 
