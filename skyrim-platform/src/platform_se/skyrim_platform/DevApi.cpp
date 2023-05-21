a5ae4e55813578142cb615bf853c83f06bdb1ba1
Pospelove <Pospelove@users.noreply.github.com>
2023-05-21 17:41:46 +0000

    release(skyrim-platform): version 2.7

--- skyrim-platform/src/platform_se/skyrim_platform/DevApi.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/DevApi.cpp
@@ -97,7 +97,7 @@ JsValue DevApi::WritePlugin(const JsFunctionArguments& args)
 
 JsValue DevApi::GetPlatformVersion(const JsFunctionArguments& args)
 {
-  return "2.6.0";
+  return "2.7.0";
 }
 
 JsValue DevApi::GetJsMemoryUsage(const JsFunctionArguments& args)
