48e1a529c2d65ac9493a85c670041352268df6f1
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-06-05 17:12:09 +0600

    feat(skymp5-client): move UI to the client dist (#1438)

--- skymp5-client/src/features/browser.ts
+++ skymp5-client/src/features/browser.ts
@@ -113,18 +113,7 @@ export const main = (): void => {
     });
   });
 
-  const cfg = {
-    ip: settings["skymp5-client"]["server-ip"],
-    port: settings["skymp5-client"]["server-port"],
-  };
-
-  printConsole({ cfg });
-
-  const uiPort = cfg.port === 7777 ? 3000 : (cfg.port as number) + 1;
-
-  const url = `http://${cfg.ip}:${uiPort}/ui/index.html`;
-  printConsole(`loading url ${url}`);
-  browser.loadUrl(url);
+  // "file:///Data/Platform/UI/index.html" is loaded by default so we need to call it manually 
 };
 
 export const getAuthData = (): RemoteAuthGameData | null => {
