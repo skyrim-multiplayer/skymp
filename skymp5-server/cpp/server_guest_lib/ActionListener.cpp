066799d28e948a32bcf8f2573b828f00b918d932
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-06-21 18:13:06 +0600

    fix(skymp5-server): fix crafted item not adding (#1489)

--- skymp5-server/cpp/server_guest_lib/ActionListener.cpp
+++ skymp5-server/cpp/server_guest_lib/ActionListener.cpp
@@ -417,6 +417,19 @@ void UseCraftRecipe(MpActor* me, espm::COBJ::Data recipeData,
   }
   me->RemoveItems(entries);
   me->AddItem(outputFormId, recipeData.outputCount);
+
+  // A hack to fix craft items do not appear (likely related to random
+  // SendInventoryUpdate ordering in RemoveItems/AddItem)
+  auto formId = me->GetFormId();
+  if (auto worldState = me->GetParent()) {
+    worldState->SetTimer(1.f).Then([worldState, formId](Viet::Void) {
+      auto actor =
+        std::dynamic_pointer_cast<MpActor>(worldState->LookupFormById(formId));
+      if (actor) {
+        actor->SendInventoryUpdate();
+      }
+    });
+  }
 }
 
 void ActionListener::OnCraftItem(const RawMessageData& rawMsgData,
