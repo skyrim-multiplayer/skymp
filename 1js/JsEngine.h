9d19e87019ff91587c147a7eca35cb4fd6850abd
ZikkeyLS <hisaha21@yandex.ru>
2023-06-22 17:45:28 +0300

    Revert any JsEngine changes

--- 1js/JsEngine.h
+++ 1js/JsEngine.h
@@ -480,6 +480,18 @@ private:
     }
   }
 
+  static std::string GetJsExceptionMessage(const char* opName, JsErrorCode ec)
+  {
+    std::stringstream ss;
+    JsValueRef exception;
+    if (JsGetAndClearException(&exception) == JsNoError) {
+      ss << ConvertJsExceptionToString(exception);
+    } else {
+      ss << "'" << opName << "' returned error 0x" << std::hex << int(ec);
+    }
+    return ss.str();
+  }
+
   class JsFunctionArgumentsImpl : public JsFunctionArguments
   {
   public:
@@ -505,18 +517,6 @@ private:
     const size_t n;
     std::unique_ptr<JsValue> undefined;
   };
-
-  static std::string GetJsExceptionMessage(const char* opName, JsErrorCode ec)
-  {
-    std::stringstream ss;
-    JsValueRef exception;
-    if (JsGetAndClearException(&exception) == JsNoError) {
-      ss << ConvertJsExceptionToString(exception);
-    } else {
-      ss << "'" << opName << "' returned error 0x" << std::hex << int(ec);
-    }
-    return ss.str();
-  }
 
   static void* NativeFunctionImpl(void* callee, bool isConstructorCall,
                                   void** arguments,
