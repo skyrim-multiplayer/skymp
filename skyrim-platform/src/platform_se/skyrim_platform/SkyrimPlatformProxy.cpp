345a74e25a8c16cc5b2729c77ec45f58bc612f38
ZikkeyLS <hisaha21@yandex.ru>
2023-06-22 12:48:20 +0300

    Remove unrequired duplicate of CallNative
    Instead, make JsFunctionArgumentImpl public and use it to cast from vector<JsValue> to JsFunctonArguments.

--- skyrim-platform/src/platform_se/skyrim_platform/SkyrimPlatformProxy.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/SkyrimPlatformProxy.cpp
@@ -12,72 +12,14 @@ using namespace CallNativeApi;
 
 namespace {
 
-JsValue CallNative(
-  const std::vector<JsValue>& args,
-  const std::function<NativeCallRequirements()>& getNativeCallRequirements)
+JsValue::JsFunctionArgumentsImpl ArgumentsImplCast(
+  const std::vector<JsValue>& arguments)
 {
-  auto className = (std::string)args[1];
-  auto functionName = (std::string)args[2];
-  auto self = args[3];
-  constexpr int nativeArgsStart = 4;
+  auto n = arguments.size();
+  JsValueRef* args = const_cast<JsValueRef*>(
+    reinterpret_cast<const JsValueRef*>(arguments.data()));
 
-  auto requirements = getNativeCallRequirements();
-  if (!requirements.vm)
-    throw std::runtime_error('\'' + className + '.' + functionName +
-                             "' can't be called in this context");
-
-  CallNative::AnySafe nativeArgs[CallNative::g_maxArgs + 1];
-  auto n = (size_t)std::max((int)args.size() - nativeArgsStart, 0);
-
-  for (size_t i = 0; i < n; ++i)
-    nativeArgs[i] =
-      NativeValueCasts::JsValueToNativeValue(args[nativeArgsStart + i]);
-
-  static VmProvider provider;
-
-  CallNative::Arguments callNativeArgs{
-    requirements.vm,
-    requirements.stackId,
-    className,
-    functionName,
-    NativeValueCasts::JsObjectToNativeObject(self),
-    nativeArgs,
-    n,
-    provider,
-    *requirements.gameThrQ,
-    *requirements.jsThrQ,
-    nullptr
-  };
-
-  auto f = provider.GetFunctionInfo(className, functionName);
-  auto isAddOrRemove =
-    (functionName == "removeItem") || (functionName == "addItem");
-
-  if (f && f->IsLatent() && !isAddOrRemove) {
-
-    thread_local CallNative::Arguments* g_callNativeArgsPtr = nullptr;
-    g_callNativeArgsPtr = &callNativeArgs;
-
-    thread_local auto g_promiseFn =
-      JsValue::Function([](const JsFunctionArguments& args) {
-        auto resolve = std::shared_ptr<JsValue>(new JsValue(args[1]));
-        if (!g_callNativeArgsPtr)
-          throw NullPointerException("g_callNativeArgsPtr");
-        g_callNativeArgsPtr->latentCallback =
-          [resolve](const CallNative::AnySafe& v) {
-            resolve->Call({ JsValue::Undefined(),
-                            NativeValueCasts::NativeValueToJsValue(v) });
-          };
-        CallNative::CallNativeSafe(*g_callNativeArgsPtr);
-        return JsValue::Undefined();
-      });
-    return CreatePromise(g_promiseFn);
-  } else {
-    Override o;
-    auto res = NativeValueCasts::NativeValueToJsValue(
-      CallNative::CallNativeSafe(callNativeArgs));
-    return res;
-  }
+  return JsValue::JsFunctionArgumentsImpl(args, n);
 }
 
 JsValue GetProxyForClass(const std::string& className, const JsValue& skyrimPlatformExports,
@@ -161,7 +103,9 @@ JsValue GetProxyForClass(const std::string& className, const JsValue& skyrimPlat
             callNativeArgs->resize(args.GetSize() + 3);
             for (size_t i = 1; i < args.GetSize(); ++i)
               (*callNativeArgs)[i + 3] = args[i];
-            return CallNative(*callNativeArgs, getNativeCallRequirements);
+
+            return CallNativeApi::CallNative(
+                ArgumentsImplCast(*callNativeArgs), getNativeCallRequirements);
           });
         }
       }
