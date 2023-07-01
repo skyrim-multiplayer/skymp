a3308d791af94b3c418d894971fe6bcd06a54e62
Marko Golovko <markgolovko@gmail.com>
2023-07-01 23:53:08 +0200

    feat(skymp5-client): hide nickname for SweetHidePerson (#1519)

--- skymp5-client/src/view/formView.ts
+++ skymp5-client/src/view/formView.ts
@@ -1,4 +1,4 @@
-import { Actor, ActorBase, createText, destroyText, Form, FormType, Game, NetImmerse, ObjectReference, once, printConsole, setTextPos, setTextString, TESModPlatform, Utility, worldPointToScreenPoint } from "skyrimPlatform";
+import { Actor, ActorBase, createText, destroyText, Form, FormType, Game, Keyword, NetImmerse, ObjectReference, once, printConsole, setTextPos, setTextString, TESModPlatform, Utility, worldPointToScreenPoint } from "skyrimPlatform";
 import { setDefaultAnimsDisabled, applyAnimation } from "../sync/animation";
 import { Appearance, applyAppearance } from "../sync/appearance";
 import { isBadMenuShown, applyEquipment } from "../sync/equipment";
@@ -359,7 +359,10 @@ export class FormView implements View<FormModel> {
       const headPart = "NPC Head [Head]";
       const maxNicknameDrawDistance = 1000;
       const playerActor = Game.getPlayer()!;
-      const isVisibleByPlayer = !model.movement?.isSneaking && playerActor.getDistance(refr) <= maxNicknameDrawDistance && playerActor.hasLOS(refr);
+      const isVisibleByPlayer = !model.movement?.isSneaking
+                                && playerActor.getDistance(refr) <= maxNicknameDrawDistance
+                                && playerActor.hasLOS(refr)
+                                && !this.isSweetHidePerson(refr);
       if (isVisibleByPlayer) {
         const headScreenPos = worldPointToScreenPoint([
           NetImmerse.getNodeWorldPositionX(refr, headPart, false),
@@ -382,6 +385,13 @@ export class FormView implements View<FormModel> {
     } else {
       this.removeNickname();
     }
+  }
+
+  private isSweetHidePerson(refr: ObjectReference): boolean {
+    const actor = Actor.from(refr)
+    if (!actor) return false;
+    const keyword = Keyword.getKeyword('SweetHidePerson');
+    return actor.wornHasKeyword(keyword);
   }
 
   private removeNickname() {
