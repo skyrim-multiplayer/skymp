868ba72b6da35e0778906dbc4ff8e94ce1b9e144
Egor Gavrilov <ejs.defolt@gmail.com>
2023-07-08 23:02:07 +0500

    feat: extend MathUtils api

--- skymp5-server/cpp/server_guest_lib/MathUtils.cpp
+++ skymp5-server/cpp/server_guest_lib/MathUtils.cpp
@@ -3,9 +3,24 @@
 
 namespace MathUtils {
 
-bool IsNearlyEqual(float value, float target, float margin)
+bool IsNearlyEqual(float value, float target, float margin) noexcept
 {
   return std::abs(target - value) < margin;
+}
+
+float PercentToFloat(float percent) noexcept
+{
+  return percent / 100.f;
+}
+
+float PercentToMultPos(float percent) noexcept
+{
+  return 1.f + PercentToFloat(percent);
+}
+
+float PercentToMultNeg(float percent) noexcept
+{
+  return 1.f - PercentToFloat(percent);
 }
 
 }
--- skymp5-server/cpp/server_guest_lib/MathUtils.h
+++ skymp5-server/cpp/server_guest_lib/MathUtils.h
@@ -2,6 +2,10 @@
 
 namespace MathUtils {
 
-bool IsNearlyEqual(float value, float target, float margin = 1.f / 1024.f);
+bool IsNearlyEqual(float value, float target,
+                   float margin = 1.f / 1024.f) noexcept;
+float PercentToFloat(float percent) noexcept;
+float PercentToMultPos(float percent) noexcept;
+float PercentToMultNeg(float percent) noexcept;
 
 }
