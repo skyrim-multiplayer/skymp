69b9576ab7bca8422c19078b95b465e3102b947a
Egor Gavrilov <55379903+che6a@users.noreply.github.com>
2023-07-12 19:27:55 +0500

    fix(skymp5-server): fix MpChangeForm::ToTuple to respect learnedSpells #1559 (#1566)

--- skymp5-server/cpp/server_guest_lib/MpChangeForms.h
+++ skymp5-server/cpp/server_guest_lib/MpChangeForms.h
@@ -29,6 +29,21 @@ struct LearnedSpells
 
   std::vector<Data::key_type> GetLearnedSpells() const;
 
+  friend bool operator==(const LearnedSpells& lhs, const LearnedSpells& rhs)
+  {
+    return lhs._learnedSpellIds == rhs._learnedSpellIds;
+  }
+
+  friend bool operator!=(const LearnedSpells& lhs, const LearnedSpells& rhs)
+  {
+    return !(lhs == rhs);
+  }
+
+  friend bool operator<(const LearnedSpells& lhs, const LearnedSpells& rhs)
+  {
+    return lhs._learnedSpellIds < rhs._learnedSpellIds;
+  }
+
 private:
   Data _learnedSpellIds{};
 };
@@ -78,12 +93,13 @@ public:
 
   auto ToTuple() const
   {
-    return std::make_tuple(
-      recType, formDesc, baseDesc, position.x, position.y, position.z, angle.x,
-      angle.y, angle.z, worldOrCellDesc, inv.ToJson(), isHarvested, isOpen,
-      baseContainerAdded, nextRelootDatetime, isDisabled, profileId,
-      isRaceMenuOpen, isDead, appearanceDump, equipmentDump,
-      actorValues.ToTuple(), spawnPoint, dynamicFields, spawnDelay);
+    return std::make_tuple(recType, formDesc, baseDesc, position.x, position.y,
+                           position.z, angle.x, angle.y, angle.z,
+                           worldOrCellDesc, inv.ToJson(), isHarvested, isOpen,
+                           baseContainerAdded, nextRelootDatetime, isDisabled,
+                           profileId, isRaceMenuOpen, isDead, appearanceDump,
+                           equipmentDump, actorValues.ToTuple(), spawnPoint,
+                           dynamicFields, spawnDelay, learnedSpells);
   }
 
   static nlohmann::json ToJson(const MpChangeFormREFR& changeForm);
