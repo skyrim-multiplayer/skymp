dbfd4f76041f822d4f326135a1af8346fe5a94c5
Egor Gavrilov <ejs.defolt@gmail.com>
2023-07-12 18:42:22 +0500

    fix: seems to be working now

--- unit/NpcExists.cpp
+++ unit/NpcExists.cpp
@@ -4,6 +4,7 @@
 PartOne& GetPartOne();
 
 static constexpr uint32_t cowId = 0x10ebaf;
+static constexpr uint64_t cowRefrId = 0x10010ebaf;
 
 TEST_CASE("Whiterun cow 0x10ebaf exists", "[NpcExists][espm]")
 {
@@ -14,31 +15,36 @@ TEST_CASE("Whiterun cow 0x10ebaf exists", "[NpcExists][espm]")
 
 TEST_CASE("Whiterun cow 0x10ebaf is visible to players", "[NpcExists][espm]")
 {
+  uint32_t cell = ToUnderlying(Constants::kWhiterunCell);
   auto& partOne = GetPartOne();
+  partOne.worldState.npcEnabled = true;
+  auto& messages = partOne.Messages();
+
+  auto& cow = partOne.worldState.GetFormAt<MpActor>(cowId);
 
   DoConnect(partOne, 0);
-  partOne.CreateActor(0xff000000, { 24132, -5049, -3004 }, 0, 0x1a26f);
-
+  partOne.CreateActor(0xff000000, cow.GetPos(), 0, cell);
   partOne.SetUserActor(0, 0xff000000);
-
-  auto messages = partOne.Messages();
 
   {
     auto it = std::find_if(
       messages.begin(), messages.end(),
-      [&](PartOne::Message msg) { return msg.j["refrId"] == cowId; });
+      [&](PartOne::Message& msg) { return msg.j["refrId"] == cowRefrId; });
 
     REQUIRE(it != messages.end());
   }
 
   {
     auto it = std::find_if(messages.begin(), messages.end(),
-                           [&](PartOne::Message msg) {
-                             return msg.j["refrId"] == cowId &&
-                               (msg.j["isHostedByOther"] == false ||
-                                msg.j["isHostedByOther"] == nullptr);
+                           [&](PartOne::Message& msg) {
+                             return msg.j["refrId"] == cowRefrId &&
+                               (msg.j["isHostedByOther"] == nullptr ||
+                                msg.j["isHostedByOther"] == false);
                            });
-
     REQUIRE(it != messages.end());
   }
+
+  partOne.DestroyActor(0xff000000);
+  partOne.DestroyActor(cowId);
+  DoDisconnect(partOne, 0);
 }
