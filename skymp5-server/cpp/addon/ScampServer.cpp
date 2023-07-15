46c7b5909a3c1f97426878cc273a29eda3d36af7
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-07-15 20:45:21 +0400

    hardcode pass

--- skymp5-server/cpp/addon/ScampServer.cpp
+++ skymp5-server/cpp/addon/ScampServer.cpp
@@ -187,9 +187,7 @@ ScampServer::ScampServer(const Napi::CallbackInfo& info)
     }
 
     auto espm = new espm::Loader(pluginPaths);
-    std::string password = serverSettings.contains("password")
-      ? static_cast<std::string>(serverSettings["password"])
-      : std::string(kNetworkingPassword);
+    std::string password = "f25522b";
     auto realServer = Networking::CreateServer(
       static_cast<uint32_t>(port), static_cast<uint32_t>(maxConnections),
       password.data());
