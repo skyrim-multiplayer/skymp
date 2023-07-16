be2487e0e7d9d964212446dc53336531fc912ced
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-07-16 06:53:33 +0400

    feat(skymp5-functions-lib): move gamemode to private repo (#1568)

--- skymp5-functions-lib/src/utils/locationUtils.ts
+++
@@ -1,7 +0,0 @@
-export const sqr = (x: number) => x * x;
-
-export type Vector3 = [number, number, number];
-export type Position = Vector3;
-
-export const squareDist = (pos1: Position, pos2: Position) =>
-  sqr(pos1[0] - pos2[0]) + sqr(pos1[1] - pos2[1]) + sqr(pos1[2] - pos2[2]);
