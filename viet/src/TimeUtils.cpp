fa1f6d23fdc94e8c1a1bc553a4f5be012b390583
Egor Gavrilov <ejs.defolt@gmail.com>
2023-07-28 21:21:49 +0500

    fix: revert time format change

--- viet/src/TimeUtils.cpp
+++ viet/src/TimeUtils.cpp
@@ -10,7 +10,7 @@ std::string TimeUtils::ToString(
   std::stringstream ss;
   std::time_t tp_c = std::chrono::system_clock::to_time_t(timePoint);
   std::tm now_tm = *std::localtime(&tp_c);
-  ss << std::put_time(&now_tm, "%FT%TZ");
+  ss << std::put_time(&now_tm, "%c");
   return ss.str();
 }
 
@@ -19,7 +19,7 @@ std::chrono::system_clock::time_point TimeUtils::SystemTimeFrom(
 {
   std::tm tm;
   std::istringstream ss{ timestamp };
-  ss >> std::get_time(&tm, "%Y-%m-%dT%TZ");
+  ss >> std::get_time(&tm, "%c");
   return std::chrono::system_clock::from_time_t(std::mktime(&tm));
 }
 
