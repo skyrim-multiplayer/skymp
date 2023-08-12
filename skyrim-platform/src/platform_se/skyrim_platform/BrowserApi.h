fab25616e9afb84cb7fa3af82ad4275ebefa32b4
ZikkeyLS <hisaha21@yandex.ru>
2023-08-12 10:54:54 +0300

    Fix

--- skyrim-platform/src/platform_se/skyrim_platform/BrowserApi.h
+++ skyrim-platform/src/platform_se/skyrim_platform/BrowserApi.h
@@ -19,10 +19,6 @@ JsValue ExecuteJavaScript(const JsFunctionArguments& args,
 
 inline void Register(JsValue& exports, std::shared_ptr<State> state)
 {
-  auto settings = Settings::GetPlatformSettings();
-  if (settings->GetBool("Debug", "ChromiumEnabled", true) == false)
-    return;
-
   auto browser = JsValue::Object();
   browser.SetProperty("setVisible", JsValue::Function(SetVisible));
   browser.SetProperty("isVisible", JsValue::Function(IsVisible));
