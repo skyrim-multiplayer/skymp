d249d27c9560a9ff507ab7215f55b2cc5a98ea96
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-08-17 01:28:10 +0400

    fix compile

--- skymp5-client/src/modelSource/msgHandler.ts
+++ skymp5-client/src/modelSource/msgHandler.ts
@@ -1,13 +1,17 @@
 import * as msg from "../messages";
+import { ChangeValuesMessage } from "../services/messages/changeValues";
+import { UpdateAnimationMessage } from "../services/messages/updateAnimationMessage";
+import { UpdateEquipmentMessage } from "../services/messages/updateEquipmentMessage";
+import { UpdateMovementMessage } from "../services/messages/updateMovementMessage";
 
 export interface MsgHandler {
   createActor(msg: msg.CreateActorMessage): void;
   destroyActor(msg: msg.DestroyActorMessage): void;
-  UpdateMovement(msg: msg.UpdateMovementMessage): void;
-  UpdateAnimation(msg: msg.UpdateAnimationMessage): void;
+  UpdateMovement(msg: UpdateMovementMessage): void;
+  UpdateAnimation(msg: UpdateAnimationMessage): void;
   UpdateAppearance(msg: msg.UpdateAppearanceMessage): void;
-  UpdateEquipment(msg: msg.UpdateEquipmentMessage): void;
-  ChangeValues(msg: msg.ChangeValuesMessage): void;
+  UpdateEquipment(msg: UpdateEquipmentMessage): void;
+  ChangeValues(msg: ChangeValuesMessage): void;
   setRaceMenuOpen(msg: msg.SetRaceMenuOpenMessage): void;
   customPacket(msg: msg.CustomPacket): void;
   DeathStateContainer(msg: msg.DeathStateContainerMessage): void;
