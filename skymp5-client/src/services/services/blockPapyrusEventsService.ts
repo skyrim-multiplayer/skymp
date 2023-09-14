50b49f1db23415496ea61086ef138b9d3ece1559
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-09-14 23:41:45 +0600

    fix & [reformat]

--- skymp5-client/src/services/services/blockPapyrusEventsService.ts
+++ skymp5-client/src/services/services/blockPapyrusEventsService.ts
@@ -3,9 +3,16 @@ import { ClientListener, CombinedController, Sp } from "./clientListener";
 export class BlockPapyrusEventsService implements ClientListener {
     constructor(private sp: Sp, private controller: CombinedController) {
         this.controller.once("tick", () => this.onceTick());
+        this.controller.once("update", () => this.onceUpdate());
     }
 
     private onceTick() {
-        this.sp.blockPapyrusEvents(true);
+        if (typeof this.sp.blockPapyrusEvents === "function") {
+            this.sp.blockPapyrusEvents(true);
+        }
+    }
+
+    private onceUpdate() {
+        (this.sp.TESModPlatform as any).blockPapyrusEvents(true);
     }
 };
