07517be23b4a911e758bb2e140651afafb0d9c1e
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-09-18 03:55:41 +0600

    feat: add blockPapyrusEvents alias for TESModPlatform.blockPapyrusEvents (#1693)

--- skymp5-client/src/services/services/blockPapyrusEventsService.ts
+++ skymp5-client/src/services/services/blockPapyrusEventsService.ts
@@ -2,11 +2,17 @@ import { ClientListener, CombinedController, Sp } from "./clientListener";
 
 export class BlockPapyrusEventsService implements ClientListener {
     constructor(private sp: Sp, private controller: CombinedController) {
+        this.controller.once("tick", () => this.onceTick());
         this.controller.once("update", () => this.onceUpdate());
     }
 
+    private onceTick() {
+        if (typeof this.sp.blockPapyrusEvents === "function") {
+            this.sp.blockPapyrusEvents(true);
+        }
+    }
+
     private onceUpdate() {
-        // TODO: It is racing with OnInit in Papyrus, fix it
         (this.sp.TESModPlatform as any).blockPapyrusEvents(true);
     }
 };
