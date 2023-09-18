8ef2a01beb37406aec64d4b0b2b802df878ed29b
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-09-19 00:46:23 +0600

    todo: test 1-byte anims

--- skymp5-server/cpp/messages/UpdateAnimationMessage.cpp
+++ skymp5-server/cpp/messages/UpdateAnimationMessage.cpp
@@ -1,19 +1,75 @@
 #include "UpdateAnimationMessage.h"
 #include "SerializationUtil/BitStreamUtil.h"
 #include <nlohmann/json.hpp>
+#include "papyrus-vm/Utils.h" // stricmp
+
+static const char*const kAnimEventNameDictionary[] = {
+  "jumpStandingStart",
+  "jumpDirectionalStart",
+  "JumpFall",
+  "JumpLandDirectional",
+  "JumpLand",
+  "attackStart",
+  "attackStartLeftHand",
+  "AttackStartH2HRight",
+  "AttackStartH2HLeft",
+  "bowAttackStart",
+  "attackRelease",
+  "crossbowAttackStart",
+  "SneakSprintStartRoll",
+  "attackStartDualWield",
+  "attackPowerStartInPlace",
+  "attackPowerStartBackward",
+  "attackPowerStartLeft",
+  "attackPowerStartRight",
+  "attackPowerStartDualWield",
+  "attackPowerStartForward",
+  "attackPowerStart_2HWSprint",
+  "attackStartSprint",
+  "attackPowerStart_2HMSprint",
+  "sprintStart",
+  "sprintStop",
+  "blockStart",
+  "blockStop",
+  "sneakStart",
+  "sneakStop"
+};
 
 void UpdateAnimationMessage::WriteBinary(SLNet::BitStream& stream) const
 {
   SerializationUtil::WriteToBitStream(stream, idx);
   SerializationUtil::WriteToBitStream(stream, numChanges);
-  SerializationUtil::WriteToBitStream(stream, animEventName);
+
+  auto it = std::find_if(std::begin(kAnimEventNameDictionary), std::end(kAnimEventNameDictionary), [&](const char* name) {
+    return Utils::stricmp(name, animEventName.c_str()) == 0;
+  });
+
+  if (it == std::end(kAnimEventNameDictionary)) {
+    SerializationUtil::WriteToBitStream(stream, animEventName);
+  }
+  else {
+    SerializationUtil::WriteToBitStream(stream, static_cast<uint8_t>(it - std::begin(kAnimEventNameDictionary)));
+  }
 }
 
 void UpdateAnimationMessage::ReadBinary(SLNet::BitStream& stream)
 {
   SerializationUtil::ReadFromBitStream(stream, idx);
   SerializationUtil::ReadFromBitStream(stream, numChanges);
-  SerializationUtil::ReadFromBitStream(stream, animEventName);
+
+  if (stream.GetNumberOfUnreadBits() == 8) {
+    uint8_t animEventNameIdx;
+    SerializationUtil::ReadFromBitStream(stream, animEventNameIdx);
+    constexpr auto size = sizeof(kAnimEventNameDictionary) / sizeof(kAnimEventNameDictionary[0]);
+    if (animEventNameIdx >= size) {
+      animEventName = "";
+    }
+    else {
+      animEventName = kAnimEventNameDictionary[animEventNameIdx];
+    }
+  } else {
+    SerializationUtil::ReadFromBitStream(stream, animEventName);
+  }
 }
 
 void UpdateAnimationMessage::WriteJson(nlohmann::json& json) const
