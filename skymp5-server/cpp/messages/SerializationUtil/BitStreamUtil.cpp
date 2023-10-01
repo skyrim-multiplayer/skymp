50fa63ef293313515e909ccbb57d15312b051a88
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-10-01 06:46:41 +0600

    forgot to resolve merge conflict here

--- skymp5-server/cpp/messages/SerializationUtil/BitStreamUtil.cpp
+++ skymp5-server/cpp/messages/SerializationUtil/BitStreamUtil.cpp
@@ -1,8 +1,5 @@
 #include "BitStreamUtil.h"
-<<<<<<< HEAD
 #include <nlohmann/json.hpp>
-=======
->>>>>>> main
 
 void SerializationUtil::WriteToBitStream(SLNet::BitStream& stream,
                                          const std::string& str)
