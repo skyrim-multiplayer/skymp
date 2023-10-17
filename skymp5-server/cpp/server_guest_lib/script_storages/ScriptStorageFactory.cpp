d8eb55a1513374a19a1aa770d5e66b6af22558e7
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2023-10-18 03:18:56 +0600

    useless refactor

--- skymp5-server/cpp/server_guest_lib/script_storages/ScriptStorageFactory.cpp
+++ skymp5-server/cpp/server_guest_lib/script_storages/ScriptStorageFactory.cpp
@@ -2,9 +2,23 @@
 
 #include <filesystem>
 
-namespace {
-std::string ResolveArchivePath(const std::string& pathFromConfigStr,
-                               const std::string& dataDir)
+std::shared_ptr<IScriptStorage> ScriptStorageFactory::Create(
+  nlohmann::json serverSettings)
+{
+
+  std::vector<std::shared_ptr<IScriptStorage>> scriptStorages;
+
+  // TODO: ensure this order is correct
+  AddDirectory(scriptStorages, serverSettings);
+  AddBsa(scriptStorages, serverSettings);
+  AddAssets(scriptStorages, serverSettings);
+
+  return std::dynamic_pointer_cast<IScriptStorage>(
+    std::make_shared<CombinedScriptStorage>(scriptStorages));
+}
+
+std::string ScriptStorageFactory::ResolveArchivePath(
+  const std::string& pathFromConfigStr, const std::string& dataDir)
 {
   std::filesystem::path pathFromConfig = pathFromConfigStr;
 
@@ -14,31 +28,36 @@ std::string ResolveArchivePath(const std::string& pathFromConfigStr,
     return (dataDir / pathFromConfig).string();
   }
 }
-}
 
-std::shared_ptr<IScriptStorage> ScriptStorageFactory::Create(
-  nlohmann::json serverSettings)
+void ScriptStorageFactory::AddDirectory(
+  std::vector<std::shared_ptr<IScriptStorage>>& storages,
+  nlohmann::json& serverSettings)
 {
   std::string dataDir = serverSettings["dataDir"];
 
-  std::vector<std::shared_ptr<IScriptStorage>> bsaScriptStorages;
+  storages.push_back(std::make_shared<DirectoryScriptStorage>(
+    (std::filesystem::path(dataDir) / "scripts").string()));
+}
+
+void ScriptStorageFactory::AddAssets(
+  std::vector<std::shared_ptr<IScriptStorage>>& storages, nlohmann::json&)
+{
+  storages.push_back(std::make_shared<AssetsScriptStorage>());
+}
+
+void ScriptStorageFactory::AddBsa(
+  std::vector<std::shared_ptr<IScriptStorage>>& storages,
+  nlohmann::json& serverSettings)
+{
+  std::string dataDir = serverSettings["dataDir"];
+
   if (serverSettings.contains("archives") &&
       serverSettings.at("archives").is_array()) {
-    for (auto archive : serverSettings.at("archives")) {
+    for (auto& archive : serverSettings.at("archives")) {
       std::string archivePath =
         ResolveArchivePath(archive.get<std::string>(), dataDir);
-      bsaScriptStorages.push_back(
+      storages.push_back(
         std::make_shared<BsaArchiveScriptStorage>(archivePath.data()));
     }
   }
-
-  std::vector<std::shared_ptr<IScriptStorage>> scriptStorages;
-  scriptStorages.push_back(std::make_shared<DirectoryScriptStorage>(
-    (std::filesystem::path(dataDir) / "scripts").string()));
-  for (auto& scriptStorage : bsaScriptStorages) {
-    scriptStorages.push_back(scriptStorage);
-  }
-  scriptStorages.push_back(std::make_shared<AssetsScriptStorage>());
-  return std::dynamic_pointer_cast<IScriptStorage>(
-    std::make_shared<CombinedScriptStorage>(scriptStorages));
 }
--- skymp5-server/cpp/server_guest_lib/script_storages/ScriptStorageFactory.h
+++ skymp5-server/cpp/server_guest_lib/script_storages/ScriptStorageFactory.h
@@ -5,9 +5,22 @@
 #include "DirectoryScriptStorage.h"
 
 #include <nlohmann/json.hpp>
+#include <string>
 
 class ScriptStorageFactory
 {
 public:
   static std::shared_ptr<IScriptStorage> Create(nlohmann::json serverSettings);
+
+private:
+  static std::string ResolveArchivePath(const std::string& pathFromConfigStr,
+                                        const std::string& dataDir);
+
+  static void AddDirectory(
+    std::vector<std::shared_ptr<IScriptStorage>>& storages,
+    nlohmann::json &serverSettings);
+  static void AddAssets(std::vector<std::shared_ptr<IScriptStorage>>& storages,
+                        nlohmann::json &serverSettings);
+  static void AddBsa(std::vector<std::shared_ptr<IScriptStorage>>& storages,
+                     nlohmann::json &serverSettings);
 };
