2dcd124f0bbf4e1dd08aee6bf25a7dec1f4dc8f8
Leonid Pospelov <pospelovlm@yandex.ru>
2023-12-12 03:12:58 +0600

    feat(skyrim-platform): add TESModPlatform.CreateReferenceAtLocation

--- skyrim-platform/src/platform_se/skyrim_platform/PapyrusTESModPlatform.cpp
+++ skyrim-platform/src/platform_se/skyrim_platform/PapyrusTESModPlatform.cpp
@@ -4,6 +4,10 @@
 #include "ExceptionPrinter.h"
 #include "NullPointerException.h"
 
+#include <RE/B/BSPointerHandle.h>
+#include <RE/N/NiPoint3.h>
+#include <REL/Relocation.h>
+
 extern CallNativeApi::NativeCallRequirements g_nativeCallRequirements;
 
 namespace TESModPlatform {
@@ -871,6 +875,54 @@ void TESModPlatform::BlockPapyrusEvents(IVM* vm, StackID stackId,
   papyrusEventsBlocked = blocked;
 }
 
+class TESDataHandlerExtension
+{
+public:
+  RE::ObjectRefHandle CreateReferenceAtLocationImpl(
+    RE::TESBoundObject* a_base, const RE::NiPoint3& a_location,
+    const RE::NiPoint3& a_rotation, RE::TESObjectCELL* a_targetCell,
+    RE::TESWorldSpace* a_selfWorldSpace,
+    RE::TESObjectREFR* a_alreadyCreatedRef, RE::BGSPrimitive* a_primitive,
+    const RE::ObjectRefHandle& a_linkedRoomRefHandle, bool a_forcePersist,
+    bool a_arg11)
+  {
+    using func_t =
+      decltype(&TESDataHandlerExtension::CreateReferenceAtLocationImpl);
+    REL::Relocation<func_t> func{ REL::ID(
+      /*13625, SE */ 13723) }; // TODO: move to addresses file, support SE
+    return func(this, a_base, a_location, a_rotation, a_targetCell,
+                a_selfWorldSpace, a_alreadyCreatedRef, a_primitive,
+                a_linkedRoomRefHandle, a_forcePersist, a_arg11);
+  }
+};
+
+RE::TESObjectREFR* TESModPlatform::CreateReferenceAtLocation(
+  IVM* vm, StackID stackId, RE::StaticFunctionTag*, RE::TESForm* baseForm,
+  RE::TESObjectCELL* cell, RE::TESWorldSpace* world, float posX, float posY,
+  float posZ, float rotX, float rotY, float rotZ, bool persist)
+{
+  if (!baseForm || (!cell && !world)) {
+    return nullptr;
+  }
+
+  auto dataHandler = reinterpret_cast<TESDataHandlerExtension*>(
+    RE::TESDataHandler::GetSingleton());
+  if (!dataHandler) {
+    return nullptr;
+  }
+
+  constexpr bool kUnknownBool = true;
+
+  RE::ObjectRefHandle objectRefHandle =
+    dataHandler->CreateReferenceAtLocationImpl(
+      reinterpret_cast<RE::TESBoundObject*>(baseForm),
+      RE::NiPoint3{ posX, posY, posZ }, RE::NiPoint3{ rotX, rotY, rotZ }, cell,
+      world, nullptr, nullptr, RE::ObjectRefHandle(), persist, kUnknownBool);
+
+  RE::TESObjectREFR* result = objectRefHandle.get().get();
+  return result;
+}
+
 int TESModPlatform::GetWeapDrawnMode(uint32_t actorId)
 {
   std::lock_guard l(share.m);
@@ -1109,6 +1161,14 @@ bool TESModPlatform::Register(IVM* vm)
     new RE::BSScript::NativeFunction<true, decltype(BlockPapyrusEvents), void,
                                      RE::StaticFunctionTag*, bool>(
       "BlockPapyrusEvents", "TESModPlatform", BlockPapyrusEvents));
+
+  vm->BindNativeMethod(
+    new RE::BSScript::NativeFunction<
+      true, decltype(CreateReferenceAtLocation), RE::TESObjectREFR*,
+      RE::StaticFunctionTag*, RE::TESForm*, RE::TESObjectCELL*,
+      RE::TESWorldSpace*, float, float, float, float, float, float, bool>(
+      "CreateReferenceAtLocation", "TESModPlatform",
+      CreateReferenceAtLocation));
 
   static LoadGameEvent loadGameEvent;
 
