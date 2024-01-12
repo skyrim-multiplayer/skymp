73aa5d546f1cbbdcf6080cee35f80fac5e983b46
ZikkeyLS <hisaha21@yandex.ru>
2024-01-13 00:51:04 +0300

    Methods template

--- skymp5-server/cpp/server_guest_lib/Quest.h
+++ skymp5-server/cpp/server_guest_lib/Quest.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <nlohmann/json.hpp>
+#include <simdjson.h>
+#include <string>
 
 class Quest
 {
@@ -6,8 +9,21 @@ public:
   nlohmann::json ToJson() const;
 
   // Doesn't parse extra data currently
-  static Inventory FromJson(simdjson::dom::element& element);
-  static Inventory FromJson(const nlohmann::json& j);
+  static Quest FromJson(simdjson::dom::element& element);
+  static Quest FromJson(const nlohmann::json& j);
+
+  bool SetCurrentStageID(uint32_t stageID);
+  void SetActive(bool active);
+  uint32_t GetStage();
+  bool GetStageDone(uint32_t stageID);
+  void CompleteQuest();
+  void CompleteAllObjectives();
+  bool IsObjectiveCompleted(uint32_t objective);
+  void SetObjectiveCompleted(uint32_t objective, bool completed);
+  std::string GetID();
+  uint32_t GetPriority();
+
+  static Quest GetQuest(std::string editorID);
 
   // Fields
 
