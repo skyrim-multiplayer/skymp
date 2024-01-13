fce6ad75944c3fd5f3b55d374b08e53a66c55ed6
ZikkeyLS <hisaha21@yandex.ru>
2024-01-13 17:44:30 +0300

    Move to struct

--- skymp5-server/cpp/server_guest_lib/Quest.h
+++ skymp5-server/cpp/server_guest_lib/Quest.h
@@ -3,34 +3,46 @@
 #include <simdjson.h>
 #include <string>
 
-class Quest
+struct Quest
 {
-public:
-  bool SetStage(uint32_t stageID) const;
-  void SetActive(bool active);
-  uint32_t GetStage() const;
-  bool GetStageDone(uint32_t stageID) const;
-  void CompleteQuest();
-  void CompleteAllObjectives();
-  bool IsObjectiveCompleted(uint32_t objective) const;
-  void SetObjectiveCompleted(uint32_t objective, bool completed);
-  bool IsObjectiveDisplayed(uint32_t objective) const;
-  void SetObjectiveDisplayed(uint32_t objective, bool displayed);
-  bool IsObjectiveFailed(uint32_t objective) const;
-  void SetObjectiveFailed(uint32_t objective, bool failed);
-  std::string GetID() const;
-  uint32_t GetPriority() const;
+  // Move as Papyrus Commands
 
-  static Quest GetQuest(std::string editorID);
+  // void Start();
+  // void End();
+  // bool SetStage(uint32_t stageID) const;
+  // void SetActive(bool active);
+  // uint32_t GetStage() const;
+  // bool GetStageDone(uint32_t stageID) const;
+  // void CompleteQuest();
+  // void CompleteAllObjectives();
+  // bool IsObjectiveCompleted(uint32_t objective) const;
+  // void SetObjectiveCompleted(uint32_t objective, bool completed);
+  // bool IsObjectiveDisplayed(uint32_t objective) const;
+  // void SetObjectiveDisplayed(uint32_t objective, bool displayed);  
+  // bool IsObjectiveFailed(uint32_t objective) const;
+  // void SetObjectiveFailed(uint32_t objective, bool failed);
+  // std::string GetID() const;
+  // uint32_t GetPriority() const;
+  // static Quest GetQuest(std::string editorID);
 
-  // public due we need to assign this values initially
   std::string editorID;
   uint32_t priority;
 
-private:
   std::vector<uint32_t> unlockedStages;
   std::vector<uint32_t> unlockedObjectives;
+  std::vector<uint32_t> failedObjectives;
 
+  uint32_t currentStageID;
   uint32_t displayedObjective;
   bool completed;
+
+  friend bool operator==(const Quest& r, const Quest& l)
+  {
+    return r.editorID == l.editorID;
+  }
+
+  friend bool operator!=(const Quest& r, const Quest& l)
+  {
+    return !(r == l);
+  }
 };
