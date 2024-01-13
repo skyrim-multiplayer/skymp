83e592517754737077732a41bb25e593c9a785d5
Leonid Pospelov <pospelovlm@yandex.ru>
2024-01-14 01:39:08 +0600

    fix(skymp5-client): fix auth

--- skymp5-client/src/index.ts
+++ skymp5-client/src/index.ts
@@ -41,6 +41,7 @@ import { BrowserService } from "./services/services/browserService";
 import { AuthService } from "./services/services/authService";
 import { NetInfoService } from "./services/services/netInfoService";
 import { AnimDebugService } from "./services/services/animDebugService";
+import { TimersService } from "./services/services/timersService";
 
 once("update", () => {
   Utility.setINIBool("bAlwaysActive:General", true);
@@ -85,7 +86,8 @@ const main = () => {
       new BrowserService(sp, controller),
       new AuthService(sp, controller),
       new NetInfoService(sp, controller),
-      new AnimDebugService(sp, controller)
+      new AnimDebugService(sp, controller),
+      new TimersService(sp, controller)
     ];
     SpApiInteractor.setup(listeners);
   }
