0f55c06f1ad546fa56794b1b4ebdf0f1aa438e92
ZikkeyLS <hisaha21@yandex.ru>
2024-01-13 13:53:50 +0300

    Fields template
    & Methods templete complete

--- skymp5-server/cpp/server_guest_lib/Quest.h
+++ skymp5-server/cpp/server_guest_lib/Quest.h
@@ -20,12 +20,21 @@ public:
   void CompleteAllObjectives();
   bool IsObjectiveCompleted(uint32_t objective) const;
   void SetObjectiveCompleted(uint32_t objective, bool completed);
+  bool IsObjectiveDisplayed(uint32_t objective) const;
+  void SetObjectiveDisplayed(uint32_t objective, bool displayed);
+  bool IsObjectiveFailed(uint32_t objective) const;
+  void SetObjectiveFailed(uint32_t objective, bool failed);
   std::string GetID() const;
   uint32_t GetPriority() const;
 
   static Quest GetQuest(std::string editorID);
 
-  // Fields
+  // public due we need to assign this values initially
+  std::string editorID;
+  uint32_t priority;
+private:
+  std::vector<uint32_t> unlockedStages;
+  std::vector<uint32_t> unlockedObjectives;
 
-  // Operators
+  uint32_t displayedObjective;
 };
