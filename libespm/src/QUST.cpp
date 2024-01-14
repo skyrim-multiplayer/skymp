648d11a1c4f5576c67c723c7fbeb8a49c894a5af
ZikkeyLS <hisaha21@yandex.ru>
2024-01-14 14:54:19 +0300

    QUST parse preparation

---
+++ libespm/src/QUST.cpp
@@ -0,0 +1,32 @@
+#include "libespm/QUST.h"
+#include "libespm/RecordHeaderAccess.h"
+#include <cstring>
+
+namespace espm {
+
+QUST::Data QUST::GetData(
+  CompressedFieldsCache& compressedFieldsCache) const noexcept
+{
+  Data result;
+  RecordHeaderAccess::IterateFields(
+    this,
+    [&](const char* type, uint32_t dataSize, const char* data) {
+      if (!std::memcmp(type, "CNTO", 4)) {
+        result.inputObjects.push_back(
+          *reinterpret_cast<const InputObject*>(data));
+      } else if (!std::memcmp(type, "CNAM", 4)) {
+        const auto formId = *reinterpret_cast<const uint32_t*>(data);
+        result.outputObjectFormId = formId;
+      } else if (!std::memcmp(type, "BNAM", 4)) {
+        const auto formId = *reinterpret_cast<const uint32_t*>(data);
+        result.benchKeywordId = formId;
+      } else if (!std::memcmp(type, "NAM1", 4)) {
+        const auto count = *reinterpret_cast<const uint16_t*>(data);
+        result.outputCount = count;
+      }
+    },
+    compressedFieldsCache);
+  return result;
+}
+
+}
--- skymp5-server/cpp/server_guest_lib/Quest.h
+++ skymp5-server/cpp/server_guest_lib/Quest.h
@@ -5,25 +5,23 @@
 
 struct Quest
 {
-  // Move as Papyrus Commands
+  // Move to PapyrusQuest
 
-  // void Start();
-  // void End();
-  // bool SetStage(uint32_t stageID) const;
-  // void SetActive(bool active);
-  // uint32_t GetStage() const;
-  // bool GetStageDone(uint32_t stageID) const;
-  // void CompleteQuest();
-  // void CompleteAllObjectives();
-  // bool IsObjectiveCompleted(uint32_t objective) const;
-  // void SetObjectiveCompleted(uint32_t objective, bool completed);
-  // bool IsObjectiveDisplayed(uint32_t objective) const;
-  // void SetObjectiveDisplayed(uint32_t objective, bool displayed);  
-  // bool IsObjectiveFailed(uint32_t objective) const;
-  // void SetObjectiveFailed(uint32_t objective, bool failed);
-  // std::string GetID() const;
-  // uint32_t GetPriority() const;
-  // static Quest GetQuest(std::string editorID);
+  // void StartQuest(std::string editorID);
+  // void EndQuest(std::string editorID);
+  // bool SetStage(std::string editorID, uint32_t stageID) const;
+  // void SetActive(std::string editorID, bool active);
+  // uint32_t GetStage(std::string editorID) const;
+  // bool GetStageDone(std::string editorID, uint32_t stageID) const;
+  // void CompleteQuest(std::string editorID);
+  // void CompleteAllObjectives(std::string editorID);
+  // bool IsObjectiveCompleted(std::string editorID, uint32_t objective) const;
+  // void SetObjectiveCompleted(std::string editorID, uint32_t objective, bool completed);
+  // bool IsObjectiveDisplayed(std::string editorID, uint32_t objective) const;
+  // void SetObjectiveDisplayed(std::string editorID, uint32_t objective, bool displayed);
+  // bool IsObjectiveFailed(std::string editorID, uint32_t objective) const;
+  // void SetObjectiveFailed(std::string editorID, uint32_t objective, bool failed);
+  // Quest GetQuest(std::string editorID); (Function)
 
   std::string editorID;
   uint32_t priority;
@@ -41,8 +39,5 @@ struct Quest
     return r.editorID == l.editorID;
   }
 
-  friend bool operator!=(const Quest& r, const Quest& l)
-  {
-    return !(r == l);
-  }
+  friend bool operator!=(const Quest& r, const Quest& l) { return !(r == l); }
 };
