6041327ebcf6924b2e3c47f9d9bda799b4a4854b
Leonid Pospelov <pospelovlm@yandex.ru>
2024-01-23 18:45:17 +0600

    .

--- skymp5-server/cpp/server_guest_lib/MpObjectReference.cpp
+++ skymp5-server/cpp/server_guest_lib/MpObjectReference.cpp
@@ -857,7 +857,7 @@ void MpObjectReference::Subscribe(MpObjectReference* emitter,
     emitter->pImpl->onInitEventSent = true;
     emitter->SendPapyrusEvent("OnInit");
     emitter->SendPapyrusEvent("OnCellLoad");
-    emitter->SendPapyrusEvent("OnLoad");
+    //emitter->SendPapyrusEvent("OnLoad");
   }
 
   const bool hasPrimitive = emitter->HasPrimitive();
