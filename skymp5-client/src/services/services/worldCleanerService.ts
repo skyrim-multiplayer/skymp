a626437f45b6f5da8470bd242f5e2e45646b9196
Leonid Pospelov <pospelovlm@yandex.ru>
2024-03-24 15:47:15 +0500

    fix(skymp5-client): handle refs that ignore Disable

--- skymp5-client/src/services/services/worldCleanerService.ts
+++ skymp5-client/src/services/services/worldCleanerService.ts
@@ -1,19 +1,30 @@
-import { Game, Actor } from "skyrimPlatform";
 import { ClientListener, CombinedController, Sp } from "./clientListener";
+import { NiPoint3 } from "../../sync/movement";
+import { ObjectReferenceEx } from "../../extensions/objectReferenceEx";
+import { Actor } from "skyrimPlatform";
 
 export class WorldCleanerService extends ClientListener {
   constructor(private sp: Sp, private controller: CombinedController) {
     super();
     this.controller.on("update", () => this.onUpdate());
+    this.controller.emitter.on("gameLoad", () => this.onGameLoad());
   }
 
   modWcProtection(actorId: number, mod: number): void {
     const currentProtection = this.protection.get(actorId);
     this.protection.set(actorId, currentProtection ? currentProtection + mod : mod);
   }
-  
+
   getWcProtection(actorId: number): number {
     return this.protection.get(actorId) || 0;
+  }
+
+  private onGameLoad() {
+    let player = this.sp.Game.getPlayer();
+    if (!player) return;
+
+    this.initialPos = ObjectReferenceEx.getPos(player);
+    this.initialCellOrWorld = ObjectReferenceEx.getWorldOrCell(player);
   }
 
   private onUpdate() {
@@ -21,10 +32,10 @@ export class WorldCleanerService extends ClientListener {
   }
 
   private processOneActor() {
-    const pc = Game.getPlayer();
+    const pc = this.sp.Game.getPlayer();
     if (pc === null) return;
 
-    const actor = Game.findRandomActor(
+    const actor = this.sp.Game.findRandomActor(
       pc.getPositionX(),
       pc.getPositionY(),
       pc.getPositionZ(),
@@ -47,13 +58,35 @@ export class WorldCleanerService extends ClientListener {
       return;
     }
 
-    if (actor.isDead()) {
+    // Keep vanila pre-placed bodies, but delete player bodies
+    if (actor.isDead() && actorId < 0xff000000) {
       actor.blockActivation(true);
       return;
     }
 
+    const pos = ObjectReferenceEx.getPos(actor);
+    const cellOrWorld = ObjectReferenceEx.getWorldOrCell(actor);
+
+    const chickenRace = 0xa919d;
+
+    // We discovered anomaly chickens that fail to Disable if we load game near to them
+    // Refs: 106C22, 106C23
+    if (actorId < 0xff000000 && actor.getRace()?.getFormID() === chickenRace) {
+      if (this.initialPos && ObjectReferenceEx.getDistanceNoZ(pos, this.initialPos) < 4096) {
+        if (cellOrWorld === this.initialCellOrWorld) {
+          if (this.isActorInDialogue(actor)) return;
+          this.logTrace(`Deleting chicken anomaly ${actorId.toString(16)}`);
+          actor.killSilent(null);
+          actor.blockActivation(true);
+          actor.disableNoWait(false);
+          actor.setAlpha(0, false);
+          return;
+        }
+      }
+    }
+
     actor.disable(false).then(() => {
-      const ac = Actor.from(Game.getFormEx(actorId));
+      const ac = this.sp.Actor.from(this.sp.Game.getFormEx(actorId));
       if (!ac || this.isActorInDialogue(ac)) return;
       ac.delete();
     });
@@ -64,4 +97,6 @@ export class WorldCleanerService extends ClientListener {
   }
 
   private protection = new Map<number, number>();
+  private initialPos?: NiPoint3;
+  private initialCellOrWorld?: number;
 }
