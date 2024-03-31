bd94802ac0af8e393f6680a2f502b6f8358e4f59
Leonid Pospelov <pospelovlm@yandex.ru>
2024-03-31 19:50:20 +0500

    refact(skymp5-client): improve types

--- skymp5-client/src/services/messages/createActorMessage.ts
+++ skymp5-client/src/services/messages/createActorMessage.ts
@@ -4,18 +4,50 @@ import { Inventory } from "../../sync/inventory";
 import { Transform } from "../../sync/movement";
 import { Animation } from "../../sync/animation";
 
-export interface CreateActorMessage {
+export interface CreateActorMessage extends CreateActorMessageMainProps {
     type: "createActor";
     idx: number;
-    refrId?: number;
     baseRecordType: "DOOR" | undefined; // see PartOne.cpp
     transform: Transform;
     isMe: boolean;
+    props?: CreateActorMessageAdditionalProps;
+}
+
+export interface CreateActorMessageMainProps {
+    refrId?: number;
+    baseId?: number;
     appearance?: Appearance;
     equipment?: Equipment;
-    inventory?: Inventory;
-    baseId?: number;
-    isDead?: boolean;
-    props?: Record<string, unknown>;
     animation?: Animation;
+
+    inventory?: Inventory;
+    isDead?: boolean;
+}
+
+export interface CreateActorMessageAdditionalProps {
+    isOpen?: boolean;
+    isHarvested?: boolean;
+    setNodeTextureSet?: Record<string, number>;
+    disabled?: boolean;
+    lastAnimation?: string;
+    displayName?: string;
+    isHostedByOther?: boolean;
+    isRaceMenuOpen?: boolean;
+    learnedSpells?: number[];
+    healRate?: number;
+    healRateMult?: number;
+    health?: number;
+    magickaRate?: number;
+    magickaRateMult?: number;
+    magicka?: number;
+    staminaRate?: number;
+    staminaRateMult?: number;
+    stamina?: number;
+    healthPercentage?: number;
+    staminaPercentage?: number;
+    magickaPercentage?: number;
+    templateChain?: number[];
+
+    inventory?: Inventory; // TODO: take a look why doubles CreateActorMessageMainProps
+    isDead?: boolean; // TODO: take a look why doubles CreateActorMessageMainProps
 }
