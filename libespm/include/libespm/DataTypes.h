70b8cbc57f13f3e6533f20f9bd002ef202d144ba
kkEngine <78795688+kkEngine@users.noreply.github.com>
2024-04-07 15:30:38 +0300

    Fix formatting

--- libespm/include/libespm/DataTypes.h
+++ libespm/include/libespm/DataTypes.h
@@ -1,17 +1,20 @@
 #pragma once
 
-namespace espm
-{
+namespace espm {
 
 using RecordFlagsType = std::uint32_t;
 
 /// <summary>
-/// A ulong used to identify a data object. May refer to a data object from a mod or new object created in-game.
+/// A ulong used to identify a data object. May refer to a data object from a
+/// mod or new object created in-game.
 /// </summary>
 using formId = std::uint32_t;
 
 /// <summary>
-/// 'Localized string', a ulong that is used as an index to look up string data information in one of the string tables. Note that the record header for the TES4 record indicates if the file is localized or not. If not, all lstrings are zstrings.
+/// 'Localized string', a ulong that is used as an index to look up string data
+/// information in one of the string tables. Note that the record header for
+/// the TES4 record indicates if the file is localized or not. If not, all
+/// lstrings are zstrings.
 /// </summary>
 using lstring = std::uint32_t;
 
--- libespm/src/WRLD.cpp
+++ libespm/src/WRLD.cpp
@@ -33,7 +33,8 @@ WRLD::Data WRLD::GetData(CompressedFieldsCache& cache) const noexcept
         result.cloudModelFileName = reinterpret_cast<const char*>(data);
       } else if (!std::memcmp(type, "MNAM", 4)) {
         if (size == sizeof(WRLD::MapData::MapSize)) {
-          result.mapData.mapSize = *reinterpret_cast<const WRLD::MapData::MapSize*>(data);
+          result.mapData.mapSize =
+            *reinterpret_cast<const WRLD::MapData::MapSize*>(data);
         } else {
           result.mapData = *reinterpret_cast<const WRLD::MapData*>(data);
         }
@@ -42,15 +43,18 @@ WRLD::Data WRLD::GetData(CompressedFieldsCache& cache) const noexcept
       } else if (!std::memcmp(type, "DATA", 4)) {
         result.flags = *reinterpret_cast<const WRLD::Flags*>(data);
       } else if (!std::memcmp(type, "NAM0", 4)) {
-        std::copy_n(data, std::size(result.bottomLeftCoord), result.bottomLeftCoord);
+        std::copy_n(data, std::size(result.bottomLeftCoord),
+                    result.bottomLeftCoord);
       } else if (!std::memcmp(type, "NAM9", 4)) {
-        std::copy_n(data, std::size(result.topRightCoord), result.topRightCoord);
+        std::copy_n(data, std::size(result.topRightCoord),
+                    result.topRightCoord);
       } else if (!std::memcmp(type, "WNAM", 4)) {
         result.parentWorldspaceId = *reinterpret_cast<const formId*>(data);
       } else if (!std::memcmp(type, "PNAM", 4)) {
         result.useFlags = *reinterpret_cast<const WRLD::UseParentFlags*>(data);
       } else if (!std::memcmp(type, "ONAM", 4)) {
-        std::copy_n(data, std::size(result.mapMarkerData), result.mapMarkerData);
+        std::copy_n(data, std::size(result.mapMarkerData),
+                    result.mapMarkerData);
       } else if (!std::memcmp(type, "TNAM", 4)) {
         result.diffuseLODName = reinterpret_cast<const char*>(data);
       } else if (!std::memcmp(type, "UNAM", 4)) {
