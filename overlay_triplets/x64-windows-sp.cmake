f73debf9541926e2779998046c893ae47bd15e2a
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-04-12 20:36:15 +0500

    internal: start switching libraries to dynamic linkage (PoC) (#1915)

--- overlay_triplets/x64-windows-sp.cmake
+++ overlay_triplets/x64-windows-sp.cmake
@@ -3,7 +3,8 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE static)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-if(${PORT} MATCHES "chakracore")
+# Keep in sync with skyrim-platform\tools\dev_service\index.js, requiredVcpkgDlls constant
+if(${PORT} MATCHES "spdlog|fmt|chakracore")
   set(VCPKG_CRT_LINKAGE static) # VCPKG_CRT_LINKAGE should be the same for all ports
   set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
