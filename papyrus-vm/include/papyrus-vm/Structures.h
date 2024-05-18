57788d4765a9b1a72af27aaf251ace7b6f93b2e4
Leonid Pospelov <pospelovlm@yandex.ru>
2024-05-18 20:03:56 +0500

    bonus: Structures.h decompose

--- papyrus-vm/include/papyrus-vm/Structures.h
+++ papyrus-vm/include/papyrus-vm/Structures.h
@@ -15,29 +15,7 @@ struct PexScript;
 class ActivePexInstance;
 class StackIdHolder;
 
-class IGameObject
-{
-  friend class VirtualMachine;
-  friend class ActivePexInstance;
-
-public:
-  virtual ~IGameObject() = default;
-  virtual const char* GetStringID() { return "Virtual Implementation"; };
-
-  // 'Actor', 'ObjectReference' and so on. Used for dynamic casts
-  virtual const char* GetParentNativeScript() { return ""; }
-
-  virtual bool EqualsByValue(const IGameObject& obj) const { return false; }
-
-  bool HasScript(const char* name) const;
-
-protected:
-  virtual const std::vector<std::shared_ptr<ActivePexInstance>>&
-  ListActivePexInstances() const = 0;
-
-  virtual void AddScript(
-    std::shared_ptr<ActivePexInstance> sctipt) noexcept = 0;
-};
+#include "IGameObject.h"
 
 enum class FunctionType
 {
