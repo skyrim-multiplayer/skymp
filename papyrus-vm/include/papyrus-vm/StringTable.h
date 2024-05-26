69061827b6733e5d1a06894ef0ea90f37f5cd49b
Leonid Pospelov <pospelovlm@yandex.ru>
2024-05-27 01:18:52 +0500

    wip-stringtable

--- papyrus-vm/include/papyrus-vm/StringTable.h
+++ papyrus-vm/include/papyrus-vm/StringTable.h
@@ -3,20 +3,61 @@
 #include <string>
 #include <vector>
 
-class StringTable
+#include <BitStream.h>
+
+
+// class StringTable
+// {
+// public:
+//   // Do NOT mutate storage after pex loading. reallocation would make string
+//   // VarValues invalid
+//   void SetStorage(std::vector<std::string> newStorage)
+//   {
+//     storage = std::move(newStorage);
+//   }
+
+//   std::vector<std::shared_ptr<std::string>> instanceStringTable;
+
+//   const std::vector<std::string>& GetStorage() const { return storage; }
+
+// private:
+//   std::vector<std::string> storage;
+// };
+
+struct StringTableCache
+{
+  std::vector<std::string> storage;
+};
+
+class StringTableView
 {
 public:
-  // Do NOT mutate storage after pex loading. reallocation would make string
-  // VarValues invalid
-  void SetStorage(std::vector<std::string> newStorage)
+  StringTableView(const uint8_t* data_, size_t dataSize_) noexcept
   {
-    storage = std::move(newStorage);
+    data = data_;
+    dataSize = dataSize_;
   }
 
-  std::vector<std::shared_ptr<std::string>> instanceStringTable;
+  const std::string& GetNthString(size_t index,
+                                  StringTableCache& cache) const noexcept
+  {
 
-  const std::vector<std::string>& GetStorage() const { return storage; }
+    if (cache.storage.empty()) {
+      SLNet::BitStream bs(const_cast<uint8_t*>(data), dataSize, false);
+
+      uint16_t numStrings = 0;
+      bs.Read(numStrings);
+
+      std::vector<std::string> strings;
+      strings.resize(numStrings);
+
+      for (size_t i = 0; i < numStrings && bs.GetNumberOfUnreadBits() > 0; ++ i) {
+        // TODO
+      }
+    }
+  }
 
 private:
-  std::vector<std::string> storage;
+  const uint8_t* data = nullptr;
+  size_t dataSize = 0;
 };
