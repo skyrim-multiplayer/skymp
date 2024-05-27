c2c4ad67fb7769755e29728d8ac351d33289eb62
Leonid Pospelov <pospelovlm@yandex.ru>
2024-05-27 16:58:45 +0500

    feat(skymp5-server): implement Cell.IsInterior

--- skymp5-server/cpp/server_guest_lib/script_classes/PapyrusCell.cpp
+++ skymp5-server/cpp/server_guest_lib/script_classes/PapyrusCell.cpp
@@ -6,10 +6,30 @@ VarValue PapyrusCell::IsAttached(VarValue self,
   return VarValue(true); // stub
 }
 
+VarValue PapyrusCell::IsInterior(VarValue self,
+                                 const std::vector<VarValue>& arguments)
+{
+  if (auto lookupRes = GetRecordPtr(self); lookupRes.rec) {
+    espm::CompressedFieldsCache cache;
+
+    auto cell = espm::Convert<espm::CELL>(lookupRes.rec);
+    if (cell) {
+      bool isInterior = cell->GetData(cache).flags & espm::CELL::Interior;
+      return VarValue(isInterior);
+    } else {
+      spdlog::error(
+        "PapyrusCell::IsInterior: failed to convert record to CELL");
+    }
+  } else {
+    spdlog::error("PapyrusCell::IsInterior: record not found");
+  }
+}
+
 void PapyrusCell::Register(VirtualMachine& vm,
                            std::shared_ptr<IPapyrusCompatibilityPolicy> policy)
 {
   compatibilityPolicy = policy;
 
   AddMethod(vm, "IsAttached", &PapyrusCell::IsAttached);
+  AddMethod(vm, "IsInterior", &PapyrusCell::IsInterior);
 }
