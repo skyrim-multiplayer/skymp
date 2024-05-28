b95a615f69782abec5bd9f35442873da544b5a05
Leonid Pospelov <pospelovlm@yandex.ru>
2024-05-28 16:35:22 +0500

    feat(skymp5-server): de-hardcode spawnDelay property for all actors

--- skymp5-server/cpp/server_guest_lib/MpActor.cpp
+++ skymp5-server/cpp/server_guest_lib/MpActor.cpp
@@ -1157,10 +1157,13 @@ LocationalData MpActor::GetEditorLocationalData() const
 
 const float MpActor::GetRespawnTime() const
 {
-  if (!IsCreatedAsPlayer()) {
-    static const auto kNpcSpawnDelay = 2 * 60.f * 60.f;
-    return kNpcSpawnDelay;
-  }
+  // This was commented out in order to deahrcode spawnDelay gamemode property
+  // for all actors
+
+  // if (!IsCreatedAsPlayer()) {
+  //   static const auto kNpcSpawnDelay = 2 * 60.f * 60.f;
+  //   return kNpcSpawnDelay;
+  // }
   return ChangeForm().spawnDelay;
 }
 
