148bce0a5800de555d4f2564f1aaf10be942c506
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-06-09 21:12:03 +0500

    feat: move file buffers to viet (#2028)

--- viet/src/MappedBuffer.cpp
+++ viet/src/MappedBuffer.cpp
@@ -1,6 +1,7 @@
-#include "libespm/MappedBuffer.h"
+#include "MappedBuffer.h"
 
 #include <iostream>
+#include <stdexcept>
 
 #ifndef WIN32
 #  include <fcntl.h>
@@ -8,30 +9,31 @@
 #  include <unistd.h>
 #endif
 
-namespace espm {
+namespace Viet {
 
-MappedBuffer::MappedBuffer(const fs::path& path)
+MappedBuffer::MappedBuffer(const std::filesystem::path& path)
 {
-  size_ = fs::file_size(path);
+  size_ = std::filesystem::file_size(path);
 #ifdef WIN32
   fileHandle_ = CreateFileW(path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL,
                             OPEN_EXISTING, FILE_FLAG_RANDOM_ACCESS, 0);
   if (!fileHandle_) {
     throw std::system_error(GetLastError(), std::system_category(),
-                            "[espm] CreateFileW failed for " + path.string());
+                            "[MappedBuffer] CreateFileW failed for " +
+                              path.string());
   }
 
   mapHandle_ = CreateFileMapping(fileHandle_, NULL, PAGE_READONLY, 0, 0, NULL);
   if (!mapHandle_) {
     throw std::system_error(GetLastError(), std::system_category(),
-                            "[espm] CreateFileMapping failed for " +
+                            "[MappedBuffer] CreateFileMapping failed for " +
                               path.string());
   }
 
   viewPtr_ = MapViewOfFile(mapHandle_, FILE_MAP_READ, 0, 0, 0);
   if (!mapHandle_) {
     throw std::system_error(GetLastError(), std::system_category(),
-                            "[espm] CreateFileMapping failed for " +
+                            "[MappedBuffer] CreateFileMapping failed for " +
                               path.string());
   }
   data_ = static_cast<char*>(viewPtr_);
@@ -39,12 +41,12 @@ MappedBuffer::MappedBuffer(const fs::path& path)
   fd_ = open(path.c_str(), O_RDONLY);
   if (fd_ == -1) {
     throw std::system_error(errno, std::system_category(),
-                            "[espm] open failed for " + path.string());
+                            "[MappedBuffer] open failed for " + path.string());
   }
   const auto mmapResult = mmap(NULL, size_, PROT_READ, MAP_SHARED, fd_, 0);
   if (mmapResult == MAP_FAILED) {
     throw std::system_error(errno, std::system_category(),
-                            "[espm] mmap failed for " + path.string());
+                            "[MappedBuffer] mmap failed for " + path.string());
   }
   data_ = static_cast<char*>(mmapResult);
 #endif
@@ -56,7 +58,7 @@ MappedBuffer::~MappedBuffer()
   if (viewPtr_) {
     bool result = UnmapViewOfFile(viewPtr_);
     if (!result) {
-      std::cerr << "[espm] can't unmap file, error=" << GetLastError()
+      std::cerr << "[MappedBuffer] can't unmap file, error=" << GetLastError()
                 << std::endl;
       std::terminate();
     }
@@ -64,16 +66,16 @@ MappedBuffer::~MappedBuffer()
   if (mapHandle_) {
     bool result = CloseHandle(mapHandle_);
     if (!result) {
-      std::cerr << "[espm] can't close map handle, error=" << GetLastError()
-                << std::endl;
+      std::cerr << "[MappedBuffer] can't close map handle, error="
+                << GetLastError() << std::endl;
       std::terminate();
     }
   }
   if (fileHandle_) {
     bool result = CloseHandle(fileHandle_);
     if (!result) {
-      std::cerr << "[espm] can't close file handle, error=" << GetLastError()
-                << std::endl;
+      std::cerr << "[MappedBuffer] can't close file handle, error="
+                << GetLastError() << std::endl;
       std::terminate();
     }
   }
@@ -81,14 +83,16 @@ MappedBuffer::~MappedBuffer()
   if (data_) {
     int result = munmap(data_, size_);
     if (result == -1) {
-      std::cerr << "[espm] can't unmap file, errno=" << errno << std::endl;
+      std::cerr << "[MappedBuffer] can't unmap file, errno=" << errno
+                << std::endl;
       std::terminate();
     }
   }
   if (fd_ != -1) {
     int result = close(fd_);
     if (result == -1) {
-      std::cerr << "[espm] can't close file, errno=" << errno << std::endl;
+      std::cerr << "[MappedBuffer] can't close file, errno=" << errno
+                << std::endl;
       std::terminate();
     }
   }
@@ -105,4 +109,4 @@ size_t MappedBuffer::GetLength() const
   return size_;
 }
 
-} // namespace espm
+}
