08fe52e10cf8877f598654d4a021040e9934ab7c
Leonid Pospelov <pospelovlm@yandex.ru>
2024-06-15 13:37:35 +0500

    feat(skymp5-client): block nocturnal anims

---
+++ skymp5-client/src/services/services/blockedAnimationsService.ts
@@ -0,0 +1,22 @@
+import { logTrace } from "../../logging";
+import { ClientListener, Sp, CombinedController } from "./clientListener";
+
+export class BlockedAnimationsService extends ClientListener {
+    constructor(private sp: Sp, private controller: CombinedController) {
+        super();
+
+        const blockedAnims = ["IdleNocturnal*"];
+
+        const self = this;
+
+        blockedAnims.forEach(blockedAnim => {
+            this.sp.hooks.sendAnimationEvent.add({
+                enter(ctx) {
+                    logTrace(self, `blocking animation event`, ctx.animEventName);
+                    ctx.animEventName = "";
+                },
+                leave() { }
+            }, 0x14, 0x14, blockedAnim);
+        });
+    }
+};
