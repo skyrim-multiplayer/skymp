fbe66f6393d2005b24f649e341e8465f734fb2c8
ZikkeyLS <hisaha21@yandex.ru>
2024-07-02 03:09:17 +0300

    Sync client p1

--- skymp5-client/src/view/formView.ts
+++ skymp5-client/src/view/formView.ts
@@ -1,4 +1,4 @@
-import { Actor, ActorBase, createText, destroyText, Form, FormType, Game, Keyword, NetImmerse, ObjectReference, once, printConsole, setTextPos, setTextString, storage, TESModPlatform, Utility, worldPointToScreenPoint } from "skyrimPlatform";
+import { Actor, ActorBase, createText, destroyText, Faction, Form, FormType, Game, Keyword, NetImmerse, ObjectReference, once, printConsole, setTextPos, setTextString, storage, TESModPlatform, Utility, worldPointToScreenPoint } from "skyrimPlatform";
 import { setDefaultAnimsDisabled, applyAnimation } from "../sync/animation";
 import { Appearance, applyAppearance } from "../sync/appearance";
 import { isBadMenuShown, applyEquipment } from "../sync/equipment";
@@ -285,6 +285,11 @@ export class FormView implements View<FormModel> {
         if (model.appearance && model.appearance.name) {
           refr?.setDisplayName("" + model.appearance.name, true);
         }
+        if (model.factions !== undefined && model.factions.length > 0) {
+          for (let i = 0; i < model.factions.length; i++) {
+            (Actor.from(refr) as any).addToFaction(Faction.from(Game.getFormEx(model.factions[i].formDesc)));
+          }
+        }
         Actor.from(refr)?.setActorValue("attackDamageMult", 0);
       }
       this.refrId = (refr as ObjectReference).getFormID();
@@ -456,15 +461,15 @@ export class FormView implements View<FormModel> {
         }
       }
     }
-    
-    if (refr.is3DLoaded() !== undefined && refr.is3DLoaded() == true){
-      if (model.animation){
+
+    if (refr.is3DLoaded() !== undefined && refr.is3DLoaded() == true) {
+      if (model.animation) {
         //printConsole(`${model.animation?.animEventName}`);
         applyAnimation(refr, model.animation, this.animState);
-      } 
-      
+      }
+
     }
-    
+
 
     if (model.appearance) {
       const actor = Actor.from(refr);
