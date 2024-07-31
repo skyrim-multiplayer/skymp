784a0e0e9d976fdff8cc77b2cfd042bc94eb2992
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-07-31 21:46:28 +0500

    feat(skymp5-server): enforce strict changeforms processing order (#2090)

--- skymp5-server/cpp/server_guest_lib/database_drivers/MongoDatabase.h
+++ skymp5-server/cpp/server_guest_lib/database_drivers/MongoDatabase.h
@@ -13,6 +13,8 @@ public:
 private:
   int GetDocumentCount();
   void CheckErrorList(const std::vector<std::string>& errorList);
+  std::string BytesToHexString(const uint8_t* bytes, size_t length);
+  std::string Sha256(const std::string& str);
 
   struct Impl;
   std::shared_ptr<Impl> pImpl;
