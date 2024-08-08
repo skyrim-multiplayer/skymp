a49181901d524eda83519ee3b3c9c844089384dc
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-08-08 18:32:08 +0500

    fix(skymp5-client): fix inventory race condition (#2104)

--- skymp5-client/src/services/services/remoteServer.ts
+++ skymp5-client/src/services/services/remoteServer.ts
@@ -140,6 +140,8 @@ export class RemoteServer extends ClientListener {
   }
 
   private onSetInventoryMessage(event: ConnectionMessage<SetInventoryMessage>): void {
+    this.numSetInventory++;
+
     const msg = event.message;
     once('update', () => {
       setPcInventory(msg.inventory);
@@ -376,18 +378,26 @@ export class RemoteServer extends ClientListener {
     if (msg.props && msg.props.isRaceMenuOpen && msg.isMe)
       this.onSetRaceMenuOpenMessage({ message: { type: 'setRaceMenuOpen', open: true } });
 
+    const numSetInventory = this.numSetInventory;
+
     const applyPcInv = () => {
       if (msg.equipment) {
         applyEquipment(Game.getPlayer()!, msg.equipment)
       }
 
-      if (msg.props && msg.props.inventory)
+      if (numSetInventory !== this.numSetInventory) {
+        logTrace(this, 'Skipping inventory apply due to newer setInventory message');
+        return;
+      }
+
+      if (msg.props && msg.props.inventory) {
         this.onSetInventoryMessage({
           message: {
             type: 'setInventory',
             inventory: msg.props.inventory
           }
         });
+      }
     };
 
     if (msg.isMe && msg.props && msg.props.learnedSpells) {
@@ -828,4 +838,6 @@ export class RemoteServer extends ClientListener {
     // Optimization added in #1186, however it doesn't work for doors for some reason
     return msg.refrId && msg.refrId < 0xff000000 && msg.baseRecordType !== 'DOOR';
   };
+
+  private numSetInventory = 0;
 }
