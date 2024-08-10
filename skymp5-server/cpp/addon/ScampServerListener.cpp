aa0cfff99da9731e8700e713741d3a9202299f4f
Leonid Pospelov <pospelovlm@yandex.ru>
2024-08-10 07:34:22 +0500

    d

--- skymp5-server/cpp/addon/ScampServerListener.cpp
+++ skymp5-server/cpp/addon/ScampServerListener.cpp
@@ -41,6 +41,20 @@ void ScampServerListener::OnCustomPacket(Networking::UserId userId,
 bool ScampServerListener::OnMpApiEvent(const GameModeEvent& event)
 {
   const char* eventName = event.GetName();
+
+  auto& mpValue = server.self;
+
+  auto mp = mpValue.Value().As<Napi::Object>();
+
+  auto fValue = mp.Get(eventName);
+  if (!fValue.IsFunction()) {
+    // It's ok not to have a handler for an event
+    spdlog::trace("ScampServerListener::OnMpApiEvent {}: failed to get '{}' "
+                  "function from 'mp' object",
+                  event.GetDetailedNameForLogging(), eventName);
+    return true;
+  }
+
   const std::string& eventArgsJsonDump = event.GetArgumentsJsonArray();
   auto [additionalArgs, additionalArgsCount] = event.GetAdditionalArguments();
 
@@ -63,29 +77,17 @@ bool ScampServerListener::OnMpApiEvent(const GameModeEvent& event)
     return true;
   }
 
-  auto& mpValue = server.self;
-
-  auto mp = mpValue.Value().As<Napi::Object>();
-
-  auto fValue = mp.Get(eventName);
-  if (!fValue.IsFunction()) {
-    // It's ok not to have a handler for an event
-    spdlog::trace("ScampServerListener::OnMpApiEvent {}: failed to get '{}' "
-                  "function from 'mp' object",
-                  event.GetDetailedNameForLogging(), eventName);
-    return true;
-  }
   auto f = fValue.As<Napi::Function>();
 
   std::vector<Napi::Value> argumentsInNapiFormat;
+
+  auto builtinJson = server.tickEnv.Global().Get("JSON").As<Napi::Object>();
+  auto builtinParse = builtinJson.Get("parse").As<Napi::Function>();
 
   const size_t n = eventArgsJson.value().get_array().size();
   for (size_t i = 0; i < n; ++i) {
     std::string elementString =
       simdjson::minify(eventArgsJson.value().get_array().at(i));
-
-    auto builtinJson = server.tickEnv.Global().Get("JSON").As<Napi::Object>();
-    auto builtinParse = builtinJson.Get("parse").As<Napi::Function>();
     auto resultOfParsing = builtinParse.Call(
       builtinJson, { Napi::String::New(server.tickEnv, elementString) });
     argumentsInNapiFormat.push_back(resultOfParsing);
