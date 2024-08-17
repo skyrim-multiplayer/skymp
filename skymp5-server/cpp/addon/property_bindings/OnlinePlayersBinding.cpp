59dd1400ba1470bde8e976f6cbcec07749532bb4
Leonid Pospelov <pospelovlm@yandex.ru>
2024-08-17 10:09:15 +0500

    perf(skymp5-server): optimize OnlinePlayersBinding

--- skymp5-server/cpp/addon/property_bindings/OnlinePlayersBinding.cpp
+++ skymp5-server/cpp/addon/property_bindings/OnlinePlayersBinding.cpp
@@ -1,25 +1,27 @@
 #include "OnlinePlayersBinding.h"
 
+#include "Config.h"
+
 Napi::Value OnlinePlayersBinding::Get(Napi::Env env, ScampServer& scampServer,
                                       uint32_t)
 {
   auto& partOne = scampServer.GetPartOne();
 
-  auto n = partOne->serverState.userInfo.size();
-  std::vector<uint32_t> onlineActors;
-  onlineActors.reserve(n);
+  thread_local std::vector<uint32_t> g_onlineActors(kMaxPlayers);
+  size_t numOnlineActors = 0;
 
+  auto n = partOne->serverState.userInfo.size();
   for (size_t i = 0; i < n; ++i) {
     if (auto actor = partOne->serverState.ActorByUser(i)) {
-      onlineActors.push_back(actor->GetFormId());
+      g_onlineActors[numOnlineActors] = actor->GetFormId();
+      ++numOnlineActors;
     }
   }
 
-  auto arr = Napi::Array::New(env, onlineActors.size());
-  int i = 0;
-  for (auto id : onlineActors) {
-    arr.Set(i, Napi::Number::New(env, id));
-    ++i;
+  auto arr = Napi::Array::New(env, numOnlineActors);
+  for (size_t k = 0; k < numOnlineActors; ++k) {
+    auto id = g_onlineActors[k];
+    arr.Set(k, Napi::Number::New(env, id));
   }
   return arr;
 }
