abddf57b3339771b227def39b3bfabcf85aaf1bd
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-09-02 19:07:38 +0500

    internal: fix ga-sdk-cpp port to install header (#2150)

--- overlay_ports/ga-sdk-cpp/portfile.cmake
+++ overlay_ports/ga-sdk-cpp/portfile.cmake
@@ -18,3 +18,7 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
 )
 vcpkg_cmake_install()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
