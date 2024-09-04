210860a2c685849ad13b7bac85490e707d1b93aa
Leonid Pospelov <pospelovlm@yandex.ru>
2024-09-04 14:33:30 +0500

    fix

--- serialization/include/archives/BitStreamInputArchive.h
+++ serialization/include/archives/BitStreamInputArchive.h
@@ -18,7 +18,7 @@ public:
   }
 
   template <IntegralConstant T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     // Compile time constant. Do nothing
     // Maybe worth adding equality check
@@ -27,7 +27,7 @@ public:
   }
 
   template <StringLike T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     ReadFromBitStream(bs, value);
     return *this;
@@ -35,7 +35,7 @@ public:
 
   // Specialization for std::array
   template <typename T, std::size_t N>
-  JsonInputArchive& Serialize(const char* key, std::array<T, N>& value)
+  BitStreamInputArchive& Serialize(const char* key, std::array<T, N>& value)
   {
     for (size_t i = 0; i < N; ++i) {
       Serialize("element", value[i]);
@@ -44,7 +44,7 @@ public:
   }
 
   template <ContainerLike T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     uint32_t n = 0;
     Serialize("size", n);
@@ -56,7 +56,7 @@ public:
   }
 
   template <Optional T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     bool hasValue = false;
     Serialize("hasValue", hasValue);
@@ -71,14 +71,14 @@ public:
   }
 
   template <Arithmetic T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     ReadFromBitStream(bs, value);
     return *this;
   }
 
   template <NoneOfTheAbove T>
-  JsonInputArchive& Serialize(const char* key, T& value)
+  BitStreamInputArchive& Serialize(const char* key, T& value)
   {
     Serialize("child", value);
     return *this;
