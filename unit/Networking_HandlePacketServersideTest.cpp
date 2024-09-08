21746962a4f6c82c2175ce615448305aa4ebf308
Leonid Pospelov <pospelovlm@yandex.ru>
2024-09-08 21:47:03 +0500

    fixtest

--- unit/Networking_HandlePacketServersideTest.cpp
+++ unit/Networking_HandlePacketServersideTest.cpp
@@ -76,8 +76,8 @@ TEST_CASE("HandlePacketServerside", "[Networking]")
         *reinterpret_cast<bool*>(called) = true;
         REQUIRE(idManager->find(0) == RakNetGUID(222));
         REQUIRE(userId == 0);
-        REQUIRE(length == 0);
-        REQUIRE(data == nullptr);
+        REQUIRE(length == 3);
+        REQUIRE(std::string(reinterpret_cast<const char*>(data), 3) == "222");
         REQUIRE(packetType ==
                 Networking::PacketType::ServerSideUserDisconnect);
       },
