31e4845e4d648d0b5c0e3954d404b54440c1e4b8
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-09-11 01:46:35 +0500

    Update FLST.cpp

--- libespm/src/FLST.cpp
+++ libespm/src/FLST.cpp
@@ -13,11 +13,12 @@ FLST::Data FLST::GetData(
     [&](const char* type, uint32_t dataSize, const char* data) {
       if (!std::memcmp(type, "LNAM", 4)) {
         const auto formId = *reinterpret_cast<const uint32_t*>(data);
-        result.formIds.push_back(formId);
+        // push front by intention: order reversed in game files
+        // we care about indexes in PapyrusFormList
+        result.formIds.insert(result.formIds.begin(), formId);
       }
     },
     compressedFieldsCache);
-  std::reverse(result.formIds.begin(), result.formIds.end());
   return result;
 }
 
