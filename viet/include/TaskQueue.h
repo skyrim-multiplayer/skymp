0f3ebb15825ad8360bfe7eddc71d91591b310db8
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-09-23 21:03:01 +0500

    fix

--- viet/include/TaskQueue.h
+++ viet/include/TaskQueue.h
@@ -15,12 +15,44 @@ class TaskQueue
 public:
   TaskQueue();
 
-  void AddTask(const std::function<void(const State&)>& task);
-  void Update(const State &state);
-  void Clear();
+  void AddTask(const std::function<void(const State&)>& task)
+  {
+    std::lock_guard l(m);
+    tasks.push_back(task);
+  }
+
+  void Update(const State &state)
+  {
+    decltype(tasks) tasksCopy;
+    {
+      std::lock_guard l(m);
+      tasksCopy = std::move(tasks);
+      tasks.clear();
+    }
+
+    for (size_t i = 0; i < tasksCopy.size(); ++i) {
+      auto& task = tasksCopy[i];
+      try {
+        task(state);
+      } catch (const std::exception&) {
+        // Other tasks should be executed later even if one throws
+        std::lock_guard l(m);
+        for (size_t j = i + 1; j < tasksCopy.size(); ++j) {
+          tasks.push_back(tasksCopy[j]);
+        }
+        throw;
+      }
+    }
+  }
+
+  void Clear()
+  {
+    std::lock_guard l(m);
+    tasks.clear();
+  }
 
 private:
-  struct Impl;
-  std::shared_ptr<Impl> pImpl;
+  std::mutex m;
+  std::vector<std::function<void()>> tasks;
 };
 }
