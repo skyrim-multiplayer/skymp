fca12a0ff839b29ae62ab1f1b45b292d49c316f7
Leonid Pospelov <pospelovlm@yandex.ru>
2024-10-19 07:03:25 +0500

    format

--- misc/wip_sp3.js
+++ misc/wip_sp3.js
@@ -6,7 +6,7 @@
 
 function sortClassesByInheritance(classes, api) {
     let classesSorted = new Array;
-  
+
     while (classes.length > 0) {
         let stack = [classes.shift()];
         let baseClass = stack[0];
@@ -24,50 +24,50 @@ function sortClassesByInheritance(classes, api) {
         }
         classes = classes.filter(c => !stack.includes(c));
     }
-  
+
     return classesSorted;
-  }
-  
-  function createSkyrimPlatform(api) {
-  
+}
+
+function createSkyrimPlatform(api) {
+
     let sp = {};
-  
+
     //console.log(api.get)
-  
+
     let classes = sortClassesByInheritance(api._sp3ListClasses(), api);
-  
-    console.log({classes})
-  
+
+    console.log({ classes })
+
     classes.forEach(className => {
         const baseClassName = api._sp3GetBaseClass(className);
         const staticFunctions = api._sp3ListStaticFunctions(className);
         const methods = api._sp3ListMethods(className);
-  
-        console.log({className, baseClassName, staticFunctions, methods: JSON.stringify(methods)})
-  
+
+        console.log({ className, baseClassName, staticFunctions, methods: JSON.stringify(methods) })
+
         const f = eval(`(function ${className}() {})`);
-  
+
         if (baseClassName !== "") {
             f.prototype = Object.create(sp[baseClassName].prototype);
         }
-  
+
         f.prototype.constructor = f;
-  
+
         methods.forEach(method => {
             // TODO(1):
             // const impl = api._sp3GetFunctionImplementation(sp, className, method);
             // f.prototype[method] = function () {
             //   const resWithoutClass = impl(this, ...arguments);
             // };
-  
+
             f.prototype[method] = api._sp3GetFunctionImplementation(sp, className, method);
         });
-  
+
         staticFunctions.forEach(staticFunction => {
             // TODO(1):
             f[staticFunction] = api._sp3GetFunctionImplementation(sp, className, staticFunction);
         });
-  
+
         f["from"] = function (obj) {
             if (api._sp3DynamicCast(obj, className)) {
                 let res = new f();
@@ -76,43 +76,42 @@ function sortClassesByInheritance(classes, api) {
             }
             return null;
         }
-  
+
         sp[className] = f;
     });
-  
+
     return sp;
-  }
-  
-  const sp = createSkyrimPlatform(mp);
-  
-  // console.log(1);
-  
-  // console.log(sp.Game.getFormEx(0xff000000));
-  // console.log(sp.Game.getFormEx(0x7));
-  
-  // console.log(sp.Utility.randomInt(1, 100));
-  
-  // console.log("===", sp.Utility.randomInt(1, 1));
-  
-  // sp.Skymp.setDefaultActor();
-  // console.log(sp.Game.getPlayer());
-  
-  //var createdId = mp.createActor(0, [0,0,0], 0, 0x3c);
-  //console.log("createdId!", createdId);
-  
-  var form = sp.game.GetFormEx(4278190082);
-  console.log("form!", form);
-  form = { desc: "0", type:"form"};
-  console.log("form!", form);
-  
-  var actor = sp.actor.from(form);
-  console.log("actor!", actor);
-  
-  sp.skymp.SetDefaultActor(actor);
-  console.log("player!", sp.game.GetPlayer());
-  
-  var pos = [sp.game.GetPlayer().GetPositionX()];
-  console.log({pos})
-  
-  process.exit(0);
-  
+}
+
+const sp = createSkyrimPlatform(mp);
+
+// console.log(1);
+
+// console.log(sp.Game.getFormEx(0xff000000));
+// console.log(sp.Game.getFormEx(0x7));
+
+// console.log(sp.Utility.randomInt(1, 100));
+
+// console.log("===", sp.Utility.randomInt(1, 1));
+
+// sp.Skymp.setDefaultActor();
+// console.log(sp.Game.getPlayer());
+
+//var createdId = mp.createActor(0, [0,0,0], 0, 0x3c);
+//console.log("createdId!", createdId);
+
+var form = sp.game.GetFormEx(4278190082);
+console.log("form!", form);
+form = { desc: "0", type: "form" };
+console.log("form!", form);
+
+var actor = sp.actor.from(form);
+console.log("actor!", actor);
+
+sp.skymp.SetDefaultActor(actor);
+console.log("player!", sp.game.GetPlayer());
+
+var pos = [sp.game.GetPlayer().GetPositionX()];
+console.log({ pos })
+
+process.exit(0);
