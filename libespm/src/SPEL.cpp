71925a49fde207d36d379040a9ebe9e6a3d344c7
Vladyslav Merais <Darkwood9612@gmail.com>
2024-10-27 12:39:45 +0200

    Add (MGEF & SPEL) reverse data and calculation for spell demage to server side

--- libespm/src/SPEL.cpp
+++ libespm/src/SPEL.cpp
@@ -12,7 +12,11 @@ SPEL::Data SPEL::GetData(
     this,
     [&](const char* type, uint32_t dataSize, const char* data) {
       if (!std::memcmp(type, "SPIT", 4)) {
-        result.type = *reinterpret_cast<const SpellType*>(data + 8);
+        result.spellItem = reinterpret_cast<const SPITData*>(data);
+      } else if (!std::memcmp(type, "EFID", 4)) {
+        result.effects.emplace_back(*reinterpret_cast<const uint32_t*>(data));
+      } else if (!std::memcmp(type, "EFIT", 4)) {
+        result.effects.back().effectItem = reinterpret_cast<const EFIT*>(data);
       }
     },
     compressedFieldsCache);
