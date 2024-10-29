77101d75a8e4219594c66bcfac1b297bc1822415
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-10-29 17:23:49 +0500

    internal: fix and improve get-logs GitHub workflow (#2201)

--- misc/deploy/get_logs_from_server.sh
+++ misc/deploy/get_logs_from_server.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
 
-report_fail() {
-  ./misc/deploy/call_webhook.sh "Something went wrong, please see GitHub logs for details"
-  exit 1
-}
-trap report_fail ERR
-
 # cheatsheet: https://stackoverflow.com/a/44606194
 # ? without : accepts empty string
 # Ensuring all required variables are set:
 echo "${DEPLOY_TARGET_HOST:?}" > /dev/null
 echo "${DEPLOY_TARGET_USER:?}" > /dev/null
-echo "${GREP_ARG:?}" > /dev/null
+echo "${GREP_ARG?}" > /dev/null # OK to be empty
+echo "${TAIL_ARG:?}" > /dev/null
 echo "${DEPLOY_BRANCH:?}" > /dev/null
 echo "${DEPLOY_SSH_PRIVATE_KEY:?}" > /dev/null
 echo "${DEPLOY_SSH_KNOWN_HOSTS:?}" > /dev/null
@@ -39,4 +34,4 @@ remote_branch_dir="skymp-server-$DEPLOY_BRANCH"
 run_remote test -e "$remote_branch_dir" \
   || (echo "no branch on remote server" && exit 1)
 
-run_remote bash -c "docker logs --tail 100 $remote_branch_dir | grep $GREP_ARG"
+run_remote "bash -c 'docker logs --tail $TAIL_ARG \"$remote_branch_dir\" | grep \"$GREP_ARG\"'"
