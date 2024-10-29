802e67f51d93d9094c6f715c9dacec62f52aa84b
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-10-29 16:23:17 +0500

    internal: add get-logs GitHub workflow (#2198)

---
+++ misc/deploy/get_logs_from_server.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+report_fail() {
+  ./misc/deploy/call_webhook.sh "Something went wrong, please see GitHub logs for details"
+  exit 1
+}
+trap report_fail ERR
+
+# cheatsheet: https://stackoverflow.com/a/44606194
+# ? without : accepts empty string
+# Ensuring all required variables are set:
+echo "${DEPLOY_TARGET_HOST:?}" > /dev/null
+echo "${DEPLOY_TARGET_USER:?}" > /dev/null
+echo "${GREP_ARG:?}" > /dev/null
+echo "${DEPLOY_BRANCH:?}" > /dev/null
+echo "${DEPLOY_SSH_PRIVATE_KEY:?}" > /dev/null
+echo "${DEPLOY_SSH_KNOWN_HOSTS:?}" > /dev/null
+
+if [[ "$CI" != "" ]]; then
+  sudo chmod -R 777 .
+fi
+
+touch ssh_id
+touch ssh_known_hosts
+chmod 600 ssh_id
+chmod 600 ssh_known_hosts
+echo "$DEPLOY_SSH_PRIVATE_KEY" > ssh_id
+echo "$DEPLOY_SSH_KNOWN_HOSTS" > ssh_known_hosts
+
+remote_shell="ssh -i ssh_id -o UserKnownHostsFile=ssh_known_hosts"
+remote_server_connstr="$DEPLOY_TARGET_USER@$DEPLOY_TARGET_HOST"
+
+run_remote() {
+  $remote_shell "$remote_server_connstr" "$@"
+}
+
+remote_branch_dir="skymp-server-$DEPLOY_BRANCH"
+
+run_remote test -e "$remote_branch_dir" \
+  || (echo "no branch on remote server" && exit 1)
+
+run_remote bash -c "docker logs --tail 100 $remote_branch_dir | grep $GREP_ARG"
