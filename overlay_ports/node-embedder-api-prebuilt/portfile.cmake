ceeeaae692fd151fd3d7b808ce4c9b970963e266
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-12-04 00:14:11 +0500

    internal: update node-embedder-api to version with headers mismatch fixed (#2245)

--- overlay_ports/node-embedder-api-prebuilt/portfile.cmake
+++ overlay_ports/node-embedder-api-prebuilt/portfile.cmake
@@ -1,6 +1,6 @@
-set(NODE_EMBEDDER_API_URL "https://node-embedder-api-prebuilt-vcpkg-port-v2.b-cdn.net/archive.zip")
+set(NODE_EMBEDDER_API_URL "https://node-embedder-api-prebuilt-vcpkg-port-v2.b-cdn.net/archive2_1.zip")
 set(NODE_EMBEDDER_API_FILENAME "node-embedder-api-prebuilt.zip")
-set(NODE_EMBEDDER_API_SHA512 31aa1c29b0c2cf4da4f6b2fa98471d0c692928d2efbe5d547e1910124db783483e8a899333fb116030b0d93b1896cba13dcd6ddb11b96e8308b02b90a36612f0)
+set(NODE_EMBEDDER_API_SHA512 bea6dbf02783e3cafbe8a752f3ce245e85189d3946902aece5a5ff6373cec684a986aa65df279f1c79fc886be380edac0926fc3808930a29a013d7fefeec67da)
 
 vcpkg_download_distfile(ARCHIVE
     URLS ${NODE_EMBEDDER_API_URL}
