c0e8c80002cb1424eb44f4fddcc303dee757c13d
Roman Nikonov <code@nic11.xyz>
2024-12-03 16:29:46 +0100

    cleanup Equipment.cpp

--- skymp5-server/cpp/server_guest_lib/Equipment.cpp
+++ skymp5-server/cpp/server_guest_lib/Equipment.cpp
@@ -12,14 +12,11 @@ nlohmann::json Equipment::ToJson() const
 {
   JsonOutputArchive ar;
   const_cast<Equipment*>(this)->Serialize(ar);
-  return std::move(ar.Release());
+  return std::move(ar.j);
 }
 
 Equipment Equipment::FromJson(const simdjson::dom::element& element)
 {
-  // std::string minifiedDump = simdjson::minify(element);
-  // nlohmann::json j = nlohmann::json::parse(minifiedDump);
-
   SimdJsonInputArchive ar(element);
   Equipment res;
   res.Serialize(ar);
