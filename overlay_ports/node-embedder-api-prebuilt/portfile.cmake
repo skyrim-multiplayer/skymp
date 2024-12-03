c865fd38fd7fd8d2815297b849e8017b629febf5
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2024-12-03 19:09:38 +0500

    Update portfile.cmake

--- overlay_ports/node-embedder-api-prebuilt/portfile.cmake
+++ overlay_ports/node-embedder-api-prebuilt/portfile.cmake
@@ -1,6 +1,6 @@
-set(NODE_EMBEDDER_API_URL "https://node-embedder-api-prebuilt-vcpkg-port.b-cdn.net/node-embedder-api-prebuilt.zip")
+set(NODE_EMBEDDER_API_URL "https://node-embedder-api-prebuilt-vcpkg-port-v2.b-cdn.net/archive.zip")
 set(NODE_EMBEDDER_API_FILENAME "node-embedder-api-prebuilt.zip")
-set(NODE_EMBEDDER_API_SHA512 0082a1537052d2343a867cac2eb9acb9ba2da1607c3766ef5ada6fd2498e0fc3df754cabf3a47825358e441a7f594addcf8fdf3be84b05f27fa6eb8aefa1e2ed)
+set(NODE_EMBEDDER_API_SHA512 0)
 
 vcpkg_download_distfile(ARCHIVE
     URLS ${NODE_EMBEDDER_API_URL}
