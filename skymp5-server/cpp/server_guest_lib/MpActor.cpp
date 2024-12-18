0296694d2e0515144871eee3b0fdaa62bd0fef30
Egor Gavrilov <55379903+barkinlove@users.noreply.github.com>
2024-12-18 16:36:18 +0500

    fix(skymp5-server): correct stamina regeneration formula (#1852)

--- skymp5-server/cpp/server_guest_lib/MpActor.cpp
+++ skymp5-server/cpp/server_guest_lib/MpActor.cpp
@@ -1688,9 +1688,12 @@ void MpActor::ApplyMagicEffect(espm::Effects::Effect& effect, bool hasSweetpie,
           static_cast<std::underlying_type_t<espm::MGEF::EffectType>>(type));
       }
       spdlog::trace("Final multiplicator is {}", mult);
+      // TODO: proper fix (or effects system) instead of monkey-patching 4x
+      // higher mult
+      // https://github.com/skyrim-multiplayer/skymp/pull/1852
       spdlog::trace("The result of baseValue * mult is: {}*{}={}", baseValue,
-                    mult, baseValue * mult);
-      SetActorValue(av, baseValue * mult);
+                    mult, baseValue * (mult * 4));
+      SetActorValue(av, baseValue * (mult * 4));
     }
   }
 }
