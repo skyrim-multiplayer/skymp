1db0d2ef2b60a2f9f69306f6d7bee21be7b74d07
Roman Nikonov <code@nic11.xyz>
2025-01-30 22:38:46 +0100

    upd

--- misc/deploy/remote/branchctl.sh
+++ misc/deploy/remote/branchctl.sh
@@ -10,7 +10,7 @@ branch="${2:?}"
 sleep 30
 /root/announce.sh "$branch" sys Сервер будет перезапущен через 30 секунд. || true
 sleep 30
-/root/announce.sh "$branch" sys Сервер перезапускается. Пожалуйста, перезайдите примерно через минуту. || true
+/root/announce.sh "$branch" sys Сервер перезапускается. Пожалуйста, перезайдите примерно через полторы минуты. || true
 
 cd "skymp-server-$branch"
 docker stop "skymp-server-$branch" || true
