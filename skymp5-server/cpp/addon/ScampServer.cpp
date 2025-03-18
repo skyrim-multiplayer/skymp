9cdb8bd92aeb48064a110c7d445807f9c36c4cb5
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2025-03-18 23:02:14 +0500

    fix(skymp5-server): make listenHost arg optional (#2375)

--- skymp5-server/cpp/addon/ScampServer.cpp
+++ skymp5-server/cpp/addon/ScampServer.cpp
@@ -177,8 +177,9 @@ ScampServer::ScampServer(const Napi::CallbackInfo& info)
     auto serverSettings = nlohmann::json::parse(serverSettingsJson);
 
     // TODO: rework parsing with archives?
-    std::string listenHost =
-      serverSettings.at("listenHost").get<std::string>();
+    std::string listenHost = serverSettings.contains("listenHost")
+      ? serverSettings.at("listenHost").get<std::string>()
+      : std::string();
     uint32_t listenPort = serverSettings.at("port").get<uint32_t>();
     uint32_t maxPlayers = serverSettings.at("maxPlayers").get<uint32_t>();
 
@@ -321,8 +322,9 @@ ScampServer::ScampServer(const Napi::CallbackInfo& info)
       ? std::string(kNetworkingPasswordPrefix) +
         static_cast<std::string>(serverSettings["password"])
       : std::string(kNetworkingPasswordPrefix);
-    auto realServer = Networking::CreateServer(listenHost.c_str(), listenPort,
-                                               maxPlayers, password.data());
+    auto realServer = Networking::CreateServer(
+      listenHost.empty() ? nullptr : listenHost.c_str(), listenPort,
+      maxPlayers, password.data());
 
     static_assert(kMockServerIdx == 1);
     server = Networking::CreateCombinedServer({ realServer, serverMock });
