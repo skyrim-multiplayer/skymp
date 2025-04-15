f23d528bf99cf31ffdf7df1d9dbc27572a037bcd
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2025-04-15 19:22:49 +0500

    add protection

--- skymp5-server/cpp/addon/property_bindings/PercentagesBinding.cpp
+++ skymp5-server/cpp/addon/property_bindings/PercentagesBinding.cpp
@@ -26,12 +26,48 @@ void PercentagesBinding::Set(Napi::Env env, ScampServer& scampServer,
   auto newPercentages = NapiHelper::ExtractObject(newValue, "newPercentages");
 
   auto& actor = partOne->worldState.GetFormAt<MpActor>(formId);
+
   ActorValues actorValues = actor.GetActorValues();
+
   actorValues.healthPercentage = NapiHelper::ExtractFloat(
     newPercentages.Get("health"), "newPercentages.health");
   actorValues.magickaPercentage = NapiHelper::ExtractFloat(
     newPercentages.Get("magicka"), "newPercentages.magicka");
   actorValues.staminaPercentage = NapiHelper::ExtractFloat(
     newPercentages.Get("stamina"), "newPercentages.stamina");
+
+  if (actorValues.healthPercentage < 0 || actorValues.healthPercentage > 1 ||
+      isnan(actorValues.healthPercentage)) {
+    spdlog::warn(
+      "PercentagesBinding::Set - healthPercentage must be in [0, 1]");
+    if (actorValues.healthPercentage < 0) {
+      actorValues.healthPercentage = 0.f;
+    } else {
+      actorValues.healthPercentage = 1.0f;
+    }
+  }
+
+  if (actorValues.magickaPercentage < 0 || actorValues.magickaPercentage > 1 ||
+      isnan(actorValues.magickaPercentage)) {
+    spdlog::warn(
+      "PercentagesBinding::Set - magickaPercentage must be in [0, 1]");
+    if (actorValues.magickaPercentage < 0) {
+      actorValues.magickaPercentage = 0.0f;
+    } else {
+      actorValues.magickaPercentage = 1.0f;
+    }
+  }
+
+  if (actorValues.staminaPercentage < 0 || actorValues.staminaPercentage > 1 ||
+      isnan(actorValues.staminaPercentage)) {
+    spdlog::warn(
+      "PercentagesBinding::Set - staminaPercentage must be in [0, 1]");
+    if (actorValues.staminaPercentage < 0) {
+      actorValues.staminaPercentage = 0.0f;
+    } else {
+      actorValues.staminaPercentage = 1.0f;
+    }
+  }
+
   actor.NetSetPercentages(actorValues);
 }
--- skymp5-server/cpp/addon/property_bindings/RespawnPercentagesBinding.cpp
+++ skymp5-server/cpp/addon/property_bindings/RespawnPercentagesBinding.cpp
@@ -34,6 +34,39 @@ void RespawnPercentagesBinding::Set(Napi::Env env, ScampServer& scampServer,
   float staminaPercentage = NapiHelper::ExtractFloat(
     newPercentages.Get("stamina"), "newPercentages.stamina");
 
+  if (healthPercentage <= 0 || healthPercentage > 1 ||
+      isnan(healthPercentage)) {
+    spdlog::warn(
+      "RespawnPercentagesBinding::Set - healthPercentage must be in (0, 1]");
+    if (healthPercentage <= 0) {
+      healthPercentage = 0.01f;
+    } else {
+      healthPercentage = 1.0f;
+    }
+  }
+
+  if (magickaPercentage < 0 || magickaPercentage > 1 ||
+      isnan(magickaPercentage)) {
+    spdlog::warn(
+      "RespawnPercentagesBinding::Set - magickaPercentage must be in [0, 1]");
+    if (magickaPercentage < 0) {
+      magickaPercentage = 0.0f;
+    } else {
+      magickaPercentage = 1.0f;
+    }
+  }
+
+  if (staminaPercentage < 0 || staminaPercentage > 1 ||
+      isnan(staminaPercentage)) {
+    spdlog::warn(
+      "RespawnPercentagesBinding::Set - staminaPercentage must be in [0, 1]");
+    if (staminaPercentage < 0) {
+      staminaPercentage = 0.0f;
+    } else {
+      staminaPercentage = 1.0f;
+    }
+  }
+
   actor.SetHealthRespawnPercentage(healthPercentage);
   actor.SetMagickaRespawnPercentage(magickaPercentage);
   actor.SetStaminaRespawnPercentage(staminaPercentage);
