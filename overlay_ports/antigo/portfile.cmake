e34fe366b7d616f5f848a882aeff904e45efee82
Roman Nikonov <code@nic11.xyz>
2025-06-13 21:14:12 +0200

    cleanup

--- overlay_ports/antigo/portfile.cmake
+++ overlay_ports/antigo/portfile.cmake
@@ -4,8 +4,6 @@ vcpkg_from_github(
   REF da15297955935da32e455fc1ca2a12faf564caae
   SHA512 1d94bc13e05a6572fce3295d3e0c8ae8b16d2ba2b08e5d13b69a0816dba61d28ee16c3fec92ea65b375ce1753063c0f22379a075770702d938f8234d921570c3
   HEAD_REF master
-  # PATCHES
-  #   patches/tmp.patch
 )
 
 vcpkg_configure_cmake(SOURCE_PATH ${SOURCE_PATH})
