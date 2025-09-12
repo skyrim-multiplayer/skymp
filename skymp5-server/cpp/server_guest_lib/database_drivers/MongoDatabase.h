5d4bda8a48ad0b1314008a164bb145e7439f9731
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2025-09-12 17:31:06 +0000

    .

--- skymp5-server/cpp/server_guest_lib/database_drivers/MongoDatabase.h
+++ skymp5-server/cpp/server_guest_lib/database_drivers/MongoDatabase.h
@@ -16,8 +16,10 @@ private:
   int GetDocumentCount();
   std::optional<std::string> GetCombinedErrorOrNull(
     const std::vector<std::optional<std::string>>& errorList);
+
   std::string BytesToHexString(const uint8_t* bytes, size_t length);
   std::string Sha256(const std::string& str);
+
   const std::string& GetEncKeysKey() const noexcept;
   const std::string& GetEncPrefix() const noexcept;
   std::optional<std::string> SanitizeKey(const std::string& key);
