49901c635d4902b503fbd845de5366fec94bbfa7
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2025-09-14 02:55:16 +0500

    perf(skymp5-server): use AsObjectReference() in WorldState::AddForm (#2519)

--- skymp5-server/cpp/server_guest_lib/WorldState.cpp
+++ skymp5-server/cpp/server_guest_lib/WorldState.cpp
@@ -133,7 +133,7 @@ void WorldState::AddForm(std::unique_ptr<MpForm> form, uint32_t formId,
   auto it = forms.insert({ formId, std::move(form) }).first;
 
   if (optionalChangeFormToApply) {
-    auto refr = dynamic_cast<MpObjectReference*>(it->second.get());
+    auto refr = it->second->AsObjectReference();
     if (!refr) {
       forms.erase(it); // Rollback changes due to exception
       throw std::runtime_error(
