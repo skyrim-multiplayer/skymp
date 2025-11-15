7fac3fe83edc63eeced9bad99c9c4ddc0b365985
IceLeon911 <62570468+IceLeon911@users.noreply.github.com>
2025-11-15 16:49:40 +0400

    fix(skymp5-server): add GetWorn check to GetEquipped condition (#2557)
    Co-authored-by: Leonid Pospelov <leonidpospelov.dev@gmail.com>

--- skymp5-server/cpp/server_guest_lib/condition_functions/GetEquipped.cpp
+++ skymp5-server/cpp/server_guest_lib/condition_functions/GetEquipped.cpp
@@ -15,8 +15,11 @@ float ConditionFunctions::GetEquipped::Execute(
   MpActor& actor, uint32_t parameter1, [[maybe_unused]] uint32_t parameter2,
   const ConditionEvaluatorContext&)
 {
-  if (actor.GetEquipment().inv.GetItemCount(parameter1) > 0) {
-    return 1.f;
+  for (auto& entry : actor.GetEquipment().inv.entries) {
+    if (entry.baseId == parameter1 &&
+        entry.GetWorn() != Inventory::Worn::None) {
+      return 1.f;
+    }
   }
   return 0.f;
 }
