695640c4554c76c922ab117ee4b13d2b6b6f9520
Pospelove <leonidpospelov.dev@gmail.com>
2025-11-20 22:22:25 +0500

    add tips

--- skymp5-server/cpp/server_guest_lib/condition_functions/SkympWornHasKeywordCount.h
+++ skymp5-server/cpp/server_guest_lib/condition_functions/SkympWornHasKeywordCount.h
@@ -2,6 +2,34 @@
 #include "ConditionFunction.h"
 
 namespace ConditionFunctions {
+
+/*
+
+For body part flags see:
+https://en.uesp.net/wiki/Skyrim_Mod:Mod_File_Format/BOD2_Field
+
+// No mask. Calculate all worn items
+{
+  "function": "SkympWornHasKeywordCount",
+  "parameter2": "0x0",
+  ...
+},
+
+// All mask bits set. Calculate all Armor
+{
+  "function": "SkympWornHasKeywordCount",
+  "parameter2": "0b11111111111111111111111111111111",
+  ...
+},
+
+// Calculate Armor with body part flags: Head, Hair and Hands
+{
+  "function": "SkympWornHasKeywordCount",
+  "parameter2": "0b00000000000000000000000000001011",
+  ...
+}
+
+*/
 class SkympWornHasKeywordCount : public ConditionFunction
 {
 public:
