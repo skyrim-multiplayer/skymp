569f4e7b9865459ea8c091ad391a99a2f24f0a39
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2025-11-25 20:45:43 +0500

    fix(skymp5-client): revert "stick to the typescript movement sync for now" (#2570)

--- skymp5-client/src/sync/movementApplyAutoSelect.ts
+++ skymp5-client/src/sync/movementApplyAutoSelect.ts
@@ -5,8 +5,7 @@ import { applyMovementNg } from "./movementApplyNg";
 
 
 export const applyMovementAutoSelect = (refr: ObjectReference, m: Movement, isMyClone?: boolean): void => {
-    applyMovement(refr, m, isMyClone);
-    // settings["skymp5-client"]["use-old-movement"]
-    //     ? applyMovement(refr, m, isMyClone)
-    //     : applyMovementNg(refr, m, isMyClone);
+    settings["skymp5-client"]["use-old-movement"]
+        ? applyMovement(refr, m, isMyClone)
+        : applyMovementNg(refr, m, isMyClone);
 }
