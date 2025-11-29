177931903e9c1f3dbc5e9262b920c5b627ec4445
Pospelove <leonidpospelov.dev@gmail.com>
2025-11-30 02:41:36 +0500

    sp3.js

--- skyrim-platform/src/platform_se/skyrim_platform/assets/sp3.js
+++ skyrim-platform/src/platform_se/skyrim_platform/assets/sp3.js
@@ -1,6 +1,7 @@
 (api, sp) => {
+  const nodeProcess = require("node:process");
+
   function getAliasNameImpl(originalName, envName, character) {
-    const nodeProcess = require("node:process");
     if (!nodeProcess.env[envName]) {
       return null;
     }
@@ -23,28 +24,6 @@
   function getClassAliasName(className) {
     return getAliasNameImpl(className, "SP3_CLASS_ALIASES", "x");
   }
-
-
-  // function getAliasesForMethods(methods) {
-  //   const nodeProcess = require("node:process");
-  //   if (nodeProcess.env.SP3_HAS_ALIASES === "true") {
-  //     const aliases = nodeProcess.env.SP3_ALIASES.split(",");
-
-  //     const aliasesLowerCase = aliases.map(alias => alias.toLowerCase());
-  //     const methodsLowerCase = methods.map(method => method.toLowerCase());
-
-  //     const resultAliases = [];
-  //     methodsLowerCase.forEach(method => {
-  //       const indexInAliases = aliasesLowerCase.findIndex(alias => alias === method);
-  //       const aliasName = 'y' + indexInAliases;
-  //     });
-  //   }
-  //   return [];
-  // }
-
-  // function getAliasesForStatics(statics) {
-  //   return [];
-  // }
 
   function prettifyImpl(name, func) {
     let firstChar = func(name.charAt(0));
@@ -71,104 +50,71 @@
   };
 
   function sortClassesByInheritance(classes, api) {
-    let classesSorted = new Array;
+    const sorted = [];
+    const visited = new Set();
+    const recursionStack = new Set();
 
-    while (classes.length > 0) {
-      let stack = [classes.shift()];
-      let baseClass = stack[0];
-      while (baseClass) {
-        baseClass = api._sp3GetBaseClass(baseClass);
-        if (baseClass) {
-          stack.push(baseClass);
+    for (const startClass of classes) {
+      if (visited.has(startClass)) {
+        continue;
+      }
+
+      const stack = [startClass];
+      recursionStack.add(startClass);
+
+      while (stack.length > 0) {
+        const current = stack[stack.length - 1];
+        const base = api._sp3GetBaseClass(current);
+
+        if (base && !visited.has(base)) {
+          if (recursionStack.has(base)) {
+            throw new Error(`Circular dependency detected: ${current} -> ${base}`);
+          }
+          stack.push(base);
+          recursionStack.add(base);
+        } else {
+          stack.pop();
+          recursionStack.delete(current);
+          if (!visited.has(current)) {
+            visited.add(current);
+            sorted.push(current);
+          }
         }
       }
-      stack.reverse();
-      for (let i = 0; i < stack.length; i++) {
-        if (!classesSorted.includes(stack[i])) {
-          classesSorted.push(stack[i]);
-        }
-      }
-      classes = classes.filter(c => !stack.includes(c));
     }
 
-    return classesSorted;
+    return sorted;
   }
 
-  // While SP3 backend is trusted, it's better to be safe than sorry
-  function sanitizeClassNameBeforeEval(className) {
-    const alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-";
-
-    const needThrowSecurityError = className.split("").some(ch => !alphabet.includes(ch));
-
-    if (needThrowSecurityError) {
-      const classNameBase64 = Buffer.from(className).toString("base64");
-      throw new Error("sanitizeClassNameBeforeEval - Invalid class name (base64-encoded): " + classNameBase64);
-    }
-  }
-
-  // for each argument if creeationTickId is set, verify it's the same as the current tick id
-  // if not, throw an error
-  function verifyTickIds(api, args, spPrivate, className, functionName) {
-    const creationTickIds = args.map(arg => spPrivate.creationTickIds.get(arg)).filter(Boolean);
-    const currentTickId = api._sp3GetCurrentTickId();
-    if (creationTickIds.some(creationTickId => creationTickId !== currentTickId)) {
-      throw new Error(`An expired object passed to ${className}.${functionName}`);
-    }
-  }
-
-  let assign = (targetObject, sourceObject) => {
-    if (sourceObject.desc !== undefined) {
-      // Server-side/gamemode raw objects have desc and type properties
+  // Server-side/gamemode raw objects have desc and type properties
+  // Client-side objects have _skyrimPlatform_indexInPool property starting with SP3 Update in 2024
+  // In previous version, they were completely native objects, without javascript properties
+  const assign = (targetObject, sourceObject) => {
+    if (sourceObject._skyrimPlatform_indexInPool !== undefined) {
+      targetObject._skyrimPlatform_indexInPool = sourceObject._skyrimPlatform_indexInPool;
+    } else if (sourceObject.desc !== undefined) {
       targetObject.desc = sourceObject.desc;
       targetObject.type = sourceObject.type;
-
-      // Never check it in runtime again
-      assign = (targetObject, sourceObject) => {
-        targetObject.desc = sourceObject.desc;
-        targetObject.type = sourceObject.type;
-      };
-
-    } else if (sourceObject._skyrimPlatform_indexInPool !== undefined) {
-      // Client-side objects have _skyrimPlatform_indexInPool property starting with SP3 Update in 2024
-      // In previous version, they were completely native objects, without javascript properties
-      targetObject._skyrimPlatform_indexInPool = sourceObject._skyrimPlatform_indexInPool;
-
-      // Never check it in runtime again
-      assign = (targetObject, sourceObject) => {
-        targetObject._skyrimPlatform_indexInPool = sourceObject._skyrimPlatform_indexInPool;
-      };
     }
   }
 
   function createSkyrimPlatform(api, sp) {
-    // TODO: de-duplicate other wrap code from this file
-    api._sp3RegisterWrapObjectFunction((obj) => {
-      const _sp3ObjectType = obj._sp3ObjectType;
-      const ctor = sp[_sp3ObjectType];
-      if (!ctor) {
-        skyrimPlatform.printConsole("!!WRAPPER!!", _sp3ObjectType, JSON.stringify(obj));
-      }
-      spPrivate.isCtorEnabled = true;
-      const resWithClass = new ctor();
-      spPrivate.isCtorEnabled = false;
-      assign(resWithClass, obj);
-      return resWithClass;
-    });
-
     if (!sp) {
       sp = {};
     }
 
-    const spPrivate = {
-      isCtorEnabled: false,
-      creationTickIds: new WeakMap()
-    };
+    // TODO: de-duplicate other wrap code from this file
+    api._sp3RegisterWrapObjectFunction((obj) => {
+      const _sp3ObjectType = obj._sp3ObjectType;
+      const ctor = sp[_sp3ObjectType];
+      const resWithClass = Object.create(ctor.prototype);
+      assign(resWithClass, obj);
+      return resWithClass;
+    });
 
     const classes =
       sortClassesByInheritance(api._sp3ListClasses(), api)
         .map(className => prettifyUpperCase(className));
-
-    console.log({ classes })
 
     classes.forEach(className => {
       const baseClassName = prettifyUpperCase(api._sp3GetBaseClass(className));
@@ -179,23 +125,12 @@
       staticFunctions = staticFunctions.filter(f => f.toLowerCase() !== "getplayer");
       const nStaticsAfter = staticFunctions.length;
 
-      console.log({ className, baseClassName, staticFunctions, methods: JSON.stringify(methods) })
-
-      sanitizeClassNameBeforeEval(className);
-
-      const onConstruct = (resWithClass) => {
-        if (!spPrivate.isCtorEnabled) {
-          throw new Error(`Direct construction of ${className} is not allowed`);
+      // The "Computed Property Name" trick is used to create a named function dynamically
+      const f = {
+        [className]: function () {
+          throw new Error(`Direct construction of ${className} is not allowed. Use .from() or static methods.`);
         }
-
-        spPrivate.creationTickIds.set(resWithClass, api._sp3GetCurrentTickId());
-      };
-
-      const f = eval(`
-            (function ${className}() {
-              onConstruct(this);  
-            })
-          `);
+      }[className];
 
       if (baseClassName !== "") {
         f.prototype = Object.create(sp[baseClassName].prototype);
@@ -207,21 +142,13 @@
         const impl = api._sp3GetFunctionImplementation(sp, className, method);
 
         const methodFinal = function () {
-          verifyTickIds(api, [this], spPrivate, className, method);
-          verifyTickIds(api, Array.from(arguments), spPrivate, className, method);
-
           let f = (resWithoutClass) => {
             if (resWithoutClass === null || typeof resWithoutClass !== "object") {
               return resWithoutClass;
             }
             const _sp3ObjectType = resWithoutClass._sp3ObjectType;
             const ctor = sp[_sp3ObjectType];
-            if (!ctor) {
-              skyrimPlatform.printConsole("!!METHOD!" + className + "." + method, _sp3ObjectType, JSON.stringify(resWithoutClass));
-            }
-            spPrivate.isCtorEnabled = true;
-            const resWithClass = new ctor();
-            spPrivate.isCtorEnabled = false;
+            const resWithClass = Object.create(ctor.prototype);
             assign(resWithClass, resWithoutClass);
             return resWithClass;
           };
@@ -231,13 +158,13 @@
           if (resWithoutClass instanceof Promise) {
             let tmp = resWithoutClass._sp3ObjectType;
 
-            return new Promise(resolve => {
+            return new Promise((resolve, reject) => {
               resWithoutClass.then((res) => {
                 if (res !== null && typeof res === "object" && tmp) {
                   res._sp3ObjectType = tmp;
                 }
                 resolve(f(res));
-              });
+              }).catch(reject);
             });
           }
 
@@ -256,20 +183,13 @@
           const impl = api._sp3GetFunctionImplementation(sp, className, staticFunction);
 
           const staticFunctionFinal = function () {
-            verifyTickIds(api, Array.from(arguments), spPrivate, className, staticFunction);
-
             let f = (resWithoutClass) => {
               if (resWithoutClass === null || typeof resWithoutClass !== "object") {
                 return resWithoutClass;
               }
               const _sp3ObjectType = resWithoutClass._sp3ObjectType;
               const ctor = sp[_sp3ObjectType];
-              if (!ctor) {
-                skyrimPlatform.printConsole("!!STATIC!" + className + "." + staticFunction, _sp3ObjectType, JSON.stringify(resWithoutClass));
-              }
-              spPrivate.isCtorEnabled = true;
-              const resWithClass = new ctor();
-              spPrivate.isCtorEnabled = false;
+              const resWithClass = Object.create(ctor.prototype);
               assign(resWithClass, resWithoutClass);
               return resWithClass;
             };
@@ -297,11 +217,8 @@
         });
 
       f["from"] = function (obj) {
-        verifyTickIds(api, [obj], spPrivate, className, "from");
         if (api._sp3DynamicCast(obj, className)) {
-          spPrivate.isCtorEnabled = true;
-          const resWithClass = new f();
-          spPrivate.isCtorEnabled = false;
+          const resWithClass = Object.create(f.prototype);
           assign(resWithClass, obj);
           return resWithClass;
         }
