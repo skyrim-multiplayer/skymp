6e4ed75f400603a6d6d348b345f5c2f91847b2fe
Roman Nikonov <code@nic11.xyz>
2025-11-30 19:40:29 +0100

    fix(overlay_ports): properly check minor version of nirnlab ui platform

--- overlay_ports/nirnlab-ui-platform-headers/portfile.cmake
+++ overlay_ports/nirnlab-ui-platform-headers/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
   REF 302a2b6686bd1ae517bf51a1e20e6b034d77a8fd  # ver3.0PR + 1 (version was not updated yet)
   SHA512 7d5d898fa1bf7caacb881252a6f2c44c283715a5303c5c063e0fb1ba10a33d45b42eb3a0a911394dea988229da3f280cb146b075373c01b0939e9f66ee1c5c02
   HEAD_REF main
+  PATCHES
+    fix_loader_minor_version_check.patch
 )
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
