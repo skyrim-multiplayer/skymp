70ed6ae03010aed1c9005395bdd213a3463bd632
Pospelove <leonidpospelov.dev@gmail.com>
2025-12-10 20:02:39 +0500

    mgef

--- skymp5-server/cpp/server_guest_lib/condition_functions/SpellHasKeyword.cpp
+++ skymp5-server/cpp/server_guest_lib/condition_functions/SpellHasKeyword.cpp
@@ -18,8 +18,10 @@ float ConditionFunctions::SpellHasKeyword::Execute(
   MpActor& actor, uint32_t parameter1, [[maybe_unused]] uint32_t parameter2,
   const ConditionEvaluatorContext&)
 {
-  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - parameter1={}, parameter2={}", parameter1, parameter2);
-  
+  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - parameter1={}, "
+               "parameter2={}",
+               parameter1, parameter2);
+
   // not really proven this order in espm is correct
   const uint32_t castingSource = parameter1;
   const uint32_t keywordId = parameter2;
@@ -32,56 +34,106 @@ float ConditionFunctions::SpellHasKeyword::Execute(
 
   std::optional<uint32_t> spellToCheck;
 
-  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - castingSource={}, keywordId=0x{:X}", castingSource, keywordId);
+  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - "
+               "castingSource={}, keywordId={:x}",
+               castingSource, keywordId);
 
   switch (castingSource) {
     case kCastingSourceLeft:
       spellToCheck = actor.GetEquipment().leftSpell;
-      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - kCastingSourceLeft, leftSpell=0x{:X}", spellToCheck.value_or(0));
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - "
+                   "kCastingSourceLeft, leftSpell={:x}",
+                   spellToCheck.value_or(0));
       break;
     case kCastingSourceRight:
       spellToCheck = actor.GetEquipment().rightSpell;
-      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - kCastingSourceRight, rightSpell=0x{:X}", spellToCheck.value_or(0));
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - "
+                   "kCastingSourceRight, rightSpell={:x}",
+                   spellToCheck.value_or(0));
       break;
     case kCastingSourceVoice:
-      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - kCastingSourceVoice (not yet supported)");
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - "
+                   "kCastingSourceVoice (not yet supported)");
       break; // Not yet supported
     case kCastingSourceInstant:
-      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - kCastingSourceInstant (not yet supported)");
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - "
+                   "kCastingSourceInstant (not yet supported)");
       break; // Not yet supported
     default:
-      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - unknown castingSource={}", castingSource);
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - unknown "
+                   "castingSource={}",
+                   castingSource);
       break;
   }
 
   if (!spellToCheck.has_value() || *spellToCheck == 0) {
-    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - no spell to check, returning 0");
+    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - no spell to "
+                 "check, returning 0");
     return 0.f;
   }
 
   auto worldState = actor.GetParent();
+  auto& espmCache = worldState->GetEspmCache();
   auto& br = worldState->GetEspm().GetBrowser();
 
-  espm::LookupResult spell = br.LookupById(*spellToCheck);
-  if (!spell.rec) {
-    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - spell record not found for ID=0x{:X}, returning 0", *spellToCheck);
+  espm::LookupResult spellLookup = br.LookupById(*spellToCheck);
+  if (!spellLookup.rec) {
+    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - spell record "
+                 "not found for ID={:x}, returning 0",
+                 *spellToCheck);
     return 0.f;
   }
 
-  std::vector<uint32_t> keywordIds =
-    spell.rec->GetKeywordIds(worldState->GetEspmCache());
-
-  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - spell 0x{:X} has {} keywords", *spellToCheck, keywordIds.size());
-
-  if (std::any_of(keywordIds.begin(), keywordIds.end(), [&](uint32_t id) {
-        uint32_t globalId = spell.ToGlobalId(id);
-        spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - checking keyword local=0x{:X}, global=0x{:X} against target=0x{:X}", id, globalId, keywordId);
-        return globalId == keywordId;
-      })) {
-    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - keyword match found, returning 1");
-    return 1.f;
+  const espm::SPEL* spellRecord = espm::Convert<espm::SPEL>(spellLookup.rec);
+  if (!spellRecord) {
+    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - record "
+                 "{:x} is not a SPEL, returning 0",
+                 *spellToCheck);
+    return 0.f;
   }
 
-  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - no keyword match, returning 0");
+  espm::SPEL::Data spellData = spellRecord->GetData(espmCache);
+
+  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - spell {:x} "
+               "has {} effects",
+               *spellToCheck, spellData.effects.size());
+
+  // Check keywords on each magic effect of the spell
+  for (const auto& effect : spellData.effects) {
+    if (effect.effectFormId == 0) {
+      continue;
+    }
+
+    uint32_t globalEffectId = spellLookup.ToGlobalId(effect.effectFormId);
+    espm::LookupResult mgefLookup = br.LookupById(globalEffectId);
+    if (!mgefLookup.rec) {
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - MGEF "
+                   "record not found for ID={:x}",
+                   globalEffectId);
+      continue;
+    }
+
+    std::vector<uint32_t> mgefKeywordIds =
+      mgefLookup.rec->GetKeywordIds(espmCache);
+    spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - MGEF {:x} "
+                 "has {} keywords",
+                 globalEffectId, mgefKeywordIds.size());
+
+    for (uint32_t localKeywordId : mgefKeywordIds) {
+      uint32_t globalKeywordId = mgefLookup.ToGlobalId(localKeywordId);
+      spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - checking "
+                   "keyword local={:x}, global={:x} against target={:x}",
+                   localKeywordId, globalKeywordId, keywordId);
+      if (globalKeywordId == keywordId) {
+        spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - keyword "
+                     "match found on MGEF {:x}, returning 1",
+                     globalEffectId);
+        return 1.f;
+      }
+    }
+  }
+
+  spdlog::info("ConditionFunctions::SpellHasKeyword::Execute - no keyword "
+               "match on any magic effect, returning 0");
   return 0.f;
 }
