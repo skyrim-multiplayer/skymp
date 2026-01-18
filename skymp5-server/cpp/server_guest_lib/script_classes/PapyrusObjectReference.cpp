643d24bff0c1d6509ca3aa6f237577513b1f9c86
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2026-01-18 13:21:04 +0000

    fix build

--- skymp5-server/cpp/server_guest_lib/script_classes/PapyrusObjectReference.cpp
+++ skymp5-server/cpp/server_guest_lib/script_classes/PapyrusObjectReference.cpp
@@ -664,10 +664,11 @@ VarValue PapyrusObjectReference::PlayAnimationAndWait(
     auto timerPromise = selfRefr->GetParent()->SetTimer(time);
     auto resultPromise = Viet::Promise<VarValue>();
     timerPromise
-      .Then([resultPromise](Viet::Void) {
+      .Then([resultPromise](Viet::Void) mutable {
         resultPromise.Resolve(VarValue::None());
       })
-      .Catch([resultPromise](const char* e) { resultPromise.Reject(e); });
+      .Catch(
+        [resultPromise](const char* e) mutable { resultPromise.Reject(e); });
     promises.push_back(resultPromise);
 
     return VarValue(Viet::Promise<VarValue>::Race(promises));
