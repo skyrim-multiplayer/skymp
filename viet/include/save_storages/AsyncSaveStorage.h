68805e57247bd3c4df5379eef6f59593baed407f
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2026-02-02 17:57:28 +0000

    fix

--- viet/include/save_storages/AsyncSaveStorage.h
+++ viet/include/save_storages/AsyncSaveStorage.h
@@ -320,7 +320,8 @@ private:
   static void SaverThreadMain(Impl* pImpl)
   {
     while (!pImpl->destroyed) {
-      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      std::this_thread::sleep_for(
+        std::chrono::milliseconds(pImpl->sleepTimeMs));
       ProcessUpserts(pImpl);
       ProcessIterates(pImpl);
     }
