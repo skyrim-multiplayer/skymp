10c579f0b3f3059ca3f908556c03754dc6e620b4
Leonid Pospelov <leonidpospelov.dev@gmail.com>
2026-03-23 00:17:33 +0500

    internal: fix prometheus-cpp-lite build (#2668)

--- overlay_ports/prometheus-cpp-lite/portfile.cmake
+++ overlay_ports/prometheus-cpp-lite/portfile.cmake
@@ -4,6 +4,7 @@ vcpkg_from_github(
   REF e6e54d2cbc1d3650d9ecec62d75404db7c9b653b  # v2.0
   SHA512 dcf42be028f5b740ce61b7523047235c385a4bb9fcc610733196a5a066004f9390b4df878f05515595f6e959eadea1a544408d86a420abd2c6ef85cb887881cb
   HEAD_REF main
+  PATCHES fix-missing-charconv.patch
 )
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
