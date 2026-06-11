37f42141a89c43cb0a502fe153a179fc43907007
Suraru Harlan <41290202+Suraru@users.noreply.github.com>
2026-06-11 00:35:55 -0500

    build launcher script

--- skymp5-backend/build-client.bat
+++ skymp5-backend/build-client.bat
@@ -1,3 +1,4 @@
 cd C:\Users\Administrator\Desktop\skyrp\skymp5-backend
 npm run populate
 npm run merge
+pause
---
+++ skymp5-launcher/build-launcher.bat
@@ -0,0 +1,4 @@
+cd C:\Users\Administrator\Desktop\skyrp\skymp5-launcher
+npm install
+npm run build:win
+pause
