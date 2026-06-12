64690ebc104249adaad69d462571af5a958e47ef
Emily Smith <41290202+Suraru@users.noreply.github.com>
2026-06-11 23:13:22 -0500

    Update launcher dl link

--- skymp5-backend/routes/version.js
+++ skymp5-backend/routes/version.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
  * Set DOWNLOAD_URL to the installer download link (e.g. a GitHub Releases URL).
  */
 const LATEST_VERSION = '0.2.4'
-const DOWNLOAD_URL   = 'https://skyrimroleplay.co.uk/'
+const DOWNLOAD_URL   = 'https://cdn1.site-media.eu/images/0/26224023/SkyRPLauncherSetup0.2.4.exe-2aTNqgijg1wXTKMlUllUjg.zip'
 
 router.get('/', (_req, res) => {
   res.json({
