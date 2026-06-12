8a167c4daf5948f74984821cbcb46af37dd77a0f
Suraru Harlan <41290202+Suraru@users.noreply.github.com>
2026-06-12 13:53:01 -0500

    launcher download

--- skymp5-backend/routes/version.js
+++ skymp5-backend/routes/version.js
@@ -4,8 +4,8 @@ const router = require('express').Router()
  * Update LATEST_VERSION here whenever you release a new launcher build.
  * Set DOWNLOAD_URL to the installer download link (e.g. a GitHub Releases URL).
  */
-const LATEST_VERSION = '0.2.4'
-const DOWNLOAD_URL   = 'https://cdn1.site-media.eu/images/0/26224023/SkyRPLauncherSetup0.2.4.exe-2aTNqgijg1wXTKMlUllUjg.zip'
+const LATEST_VERSION = '0.2.5'
+const DOWNLOAD_URL   = 'https://cdn1.site-media.eu/images/0/26239617/SkyRPLauncherSetup0.2.5.exe-e2ZdQFTXnXgCThfmTmX2YQ.zip'
 
 router.get('/', (_req, res) => {
   res.json({
